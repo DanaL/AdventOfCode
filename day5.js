6dae375a141b2d17e5cb7b58f657f111418397bb
Dana Larose <ywg.dana@gmail.com>
2018-12-05 07:11:47 -0600

    One file for both day 5 answers. Q2 is just an extension of Q1

---
+++ day5.js
@@ -0,0 +1,33 @@
+function react(ch1, ch2) {
+    return ch1 != ch2 && ch1.toLowerCase() == ch2.toLowerCase();
+}
+
+function do_reaction(polymer, skip) {
+    const stack = [];
+    for (j = 0; j < polymer.length; j++) {
+        if (polymer[j].toLowerCase() == skip)
+            continue;
+
+        const last = stack.length - 1;
+        if (stack.length == 0 || !react(stack[last], polymer[j]))
+            stack.push(polymer[j]);
+        else
+            stack.pop();
+    }
+
+    return stack.length;
+}
+
+const fs = require("fs");
+const polymer = fs.readFileSync("polymer.txt", "utf8");
+
+console.log("Answer to q1: " + do_reaction(polymer, ""));
+
+var smallest = polymer.length + 1;
+for (let ch of "abcdefghijklmnopqrstuvwxyz") {
+    let len = do_reaction(polymer, ch);
+    if (len < smallest)
+        smallest = len;
+}
+
+console.log("Answer to q2: " + smallest);
