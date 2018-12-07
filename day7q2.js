90e6c839f9f16226e5189186d732304cc53f47ef
Dana <dana@lrsd.net>
2018-12-07 14:57:13 -0600

    Day 7 q2 completed

--- day7q2.js
+++ day7q2.js
@@ -1,6 +1,6 @@
 
-function get_available_tasks(steps) {
-    var avail = Object.keys(steps).filter(s => steps[s].length == 0);
+function get_available_tasks(tasks) {
+    var avail = Object.keys(tasks).filter(s => tasks[s].length == 0);
     return avail.sort();
 }
 
@@ -13,51 +13,58 @@ function get_available_worker(workers) {
     return null;
 }
 
-const steps = [];
+function work_remaining(tasks, workers) {
+    return Object.keys(tasks).length > 0 ||
+        workers.filter(w => w.task !== '').length > 0;
+}
+
+const tasks = [];
 var fs = require("fs");
-fs.readFileSync("instructions_sm.txt").toString().split("\n").forEach(s => {
+fs.readFileSync("instructions.txt").toString().split("\n").forEach(s => {
     let step = s.substr(s.indexOf("step") + 5, 1);
     let depends = s.substr(5, 1);
-    if (!(step in steps))
-        steps[step] = [];
-    if (!(depends in steps))
-        steps[depends] = [];
-    steps[step].push(depends);
+    if (!(step in tasks))
+        tasks[step] = [];
+    if (!(depends in tasks))
+        tasks[depends] = [];
+    tasks[step].push(depends);
 });
 
 const workers = []
-const num_of_workers = 2;
-for (let j = 0; j < 5; j++)
+const num_of_workers = 5;
+for (let j = 0; j < num_of_workers; j++)
     workers[j] = { task:'', finished_at:Number.MAX_SAFE_INTEGER };
 
 var time = 0;
-const base_cost = 0;
-while (Object.keys(steps).length > 0) {
-    const ns = get_available_tasks(steps);
-    console.log(time + ': Tasks: ' + ns);
+const base_cost = 60;
+while (work_remaining(tasks, workers)) {
+    const ns = get_available_tasks(tasks);
+    //console.log(time + ': Tasks: ' + ns);
     for (task of ns) {
         w = get_available_worker(workers);
         if (w !== null) {
-            delete steps[ns];
+            //console.log("   Assign task " + task);
+            delete tasks[task];
             w.task = task;
-            w.finished_at = time + base_cost + task.charCodeAt(0) - 65;
+            w.finished_at = time + base_cost + task.charCodeAt(0) - 64;
         }
     }
 
     ++time;
 
     workers.forEach(w => {
-        if (w.finished_at <= time) {
+        if (w.finished_at == time) {
             // Remove the completed tasks from the depencies lists
-            console.log("Finished: " + w.task);
-            for (let d in steps) {
-                steps[d] = steps[d].filter(t => t != w.task);
+            //console.log("   Finished: " + w.task);
+            for (let d in tasks) {
+                tasks[d] = tasks[d].filter(t => t != w.task);
             }
 
             w.task = '';
         }
     });
 
-    if (time > 10) break;
+    //if (time > 10) break;
 }
-console.log(workers);
+
+console.log(time);
