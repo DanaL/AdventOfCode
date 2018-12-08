6c8ea2ebfd084cbbcaa11a5614fb2daa90a2958d
Dana Larose <ywg.dana@gmail.com>
2018-12-07 22:35:34 -0600

    Whoops broke get_available_tasks()

--- day7q2.js
+++ day7q2.js
@@ -1,7 +1,6 @@
 
 function get_available_tasks(tasks) {
-    var avail = Object.keys(tasks).filter(s => tasks[s].length == 0);
-    return avail.sort();
+    return Object.keys(tasks).filter(s => tasks[s].length == 0).sort();
 }
 
 function get_available_worker(workers) {
@@ -38,12 +37,9 @@ for (let j = 0; j < num_of_workers; j++)
 var time = 0;
 const base_cost = 60;
 while (work_remaining(tasks, workers)) {
-    const ns = get_available_tasks(tasks);
-    //console.log(time + ': Tasks: ' + ns);
-    for (task of ns) {
-        w = get_available_worker(workers);
+    for (task of get_available_tasks(tasks)) {
+        const w = get_available_worker(workers);
         if (w !== null) {
-            //console.log("   Assign task " + task);
             delete tasks[task];
             w.task = task;
             w.finished_at = time + base_cost + task.charCodeAt(0) - 64;
@@ -55,7 +51,6 @@ while (work_remaining(tasks, workers)) {
     workers.forEach(w => {
         if (w.finished_at == time) {
             // Remove the completed tasks from the depencies lists
-            //console.log("   Finished: " + w.task);
             for (let d in tasks) {
                 tasks[d] = tasks[d].filter(t => t != w.task);
             }
@@ -63,8 +58,6 @@ while (work_remaining(tasks, workers)) {
             w.task = '';
         }
     });
-
-    //if (time > 10) break;
 }
 
 console.log(time);
