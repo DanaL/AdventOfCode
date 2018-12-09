6ae11638f81c3a9d4f2cdcc0e187e4d8d268ce7f
Dana Larose <ywg.dana@gmail.com>
2018-12-09 02:28:36 -0600

    Part 2 implemented with a doubly-linked list

--- day9.js
+++ day9.js
@@ -1,30 +1,53 @@
 
+const new_node = (val) =>  {
+    return { val:val, next:null, prev:null };
+};
+
+const insert = (curr, val) => {
+    curr = curr.next;
+    nn = new_node(val);
+    nn.next = curr.next;
+    nn.prev = curr;
+    curr.next = nn;
+    nn.next.prev = nn;
+
+    return nn;
+};
+
+const find_and_cut = (curr) => {
+    curr = curr.prev;
+    curr = curr.prev;
+    curr = curr.prev;
+    curr = curr.prev;
+    curr = curr.prev;
+    curr = curr.prev;
+    curr = curr.prev;
+
+    curr.prev.next = curr.next;
+    curr.next.prev = curr.prev;
+
+    return { cut:curr, new_pos:curr.next };
+};
+
 const num_of_elves = parseInt(process.argv[2]);
 const last_marble = parseInt(process.argv[3]);
-
 const scores = Array(num_of_elves).fill(0);
-const ring = [0];
 var curr_marble = 1;
-var pos = 0;
 
-while (curr_marble <= last_marble) {
-    for (let p = 0; p < num_of_elves && curr_marble <= last_marble; p++) {
-        if (curr_marble % 23 == 0) {
-            scores[p] += curr_marble;
-            pos -= 7;
-            if (pos < 0)
-                pos += ring.length;
-            else
-                pos %= ring.length;
-            scores[p] += ring[pos];
-            ring.splice(pos, 1);
-        }
-        else {
-            pos = (pos + 1) % ring.length + 1;
-            ring.splice(pos, 0, curr_marble);
-        }
+const head = new_node(0);
+head.next = head;
+head.prev = head;
+var curr = head;
 
-        ++curr_marble;
+for (let curr_marble = 1; curr_marble <= last_marble; curr_marble++) {
+    p = curr_marble % num_of_elves;
+    if (curr_marble % 23 == 0) {
+        var result = find_and_cut(curr);
+        scores[p] += curr_marble + result.cut.val;
+        curr = result.new_pos;
+    }
+    else {
+        curr = insert(curr, curr_marble);
     }
 }
 
