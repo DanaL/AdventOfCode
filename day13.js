a8e0cca8ead3ad6486476198659764d8a2a42037
Dana <dana@lrsd.net>
2018-12-13 14:10:40 -0600

    Basic loop track working

--- day13.js
+++ day13.js
@@ -1,19 +1,144 @@
 
-const map = [];
-
-var fs = require("fs");
-const lines = fs.readFileSync("tracks.txt").toString().split("\n").map(l => {
-    var sqs = l.split("");
-    map.push(sqs);
-});
-
-for (let j = 0; j < 50; j++) {
-    let s = "";
-    for (let k = 0; k < 100; k++) {
-	s += map[j][k];
+var cart_factory = {
+    cart_id:0,
+    make: function(tile) {
+        var id = this.cart_id++;
+        return { tile:tile, id:id, turns:0 }
     }
-    console.log(s);
-}
-	    
-	
+};
 
+/* Quick and dirty priority queue */
+const add_cart_to_queue = (q, cart, row, col) => {
+    for (var spot = 0; spot < q.length; spot++) {
+        if (row < q[spot].row || (row == q[spot].row && col < q[spot].col))
+            break;
+    }
+
+    cart.row = row;
+    cart.col = col;
+    q.splice(spot, 0, cart);
+};
+
+const dump_map = (map) => {
+    for (let j = 0; j < map.length; j++) {
+        let s = "";
+        for (let k = 0; k < map[j].length; k++) {
+    	       s += map[j][k].cart == "" ? map[j][k].track : map[j][k].cart.tile;
+        }
+        console.log(s);
+    }
+};
+
+const do_turn = (map, cart, sq) => {
+    switch (sq.track) {
+        case "/":
+            if (cart.tile == "^")
+                cart.tile = ">";
+            else if (cart.tile == ">")
+                cart.tile = "^";
+            else if (cart.tile == "v")
+                cart.tile = "<";
+            else
+                cart.tile = "v";
+            break;
+        case "\\":
+            if (cart.tile == "^")
+                cart.tile = "<";
+            else if (cart.tile == "<")
+                cart.tile = "^";
+            else if (cart.tile == "v")
+                cart.tile = ">";
+            else
+                cart.tile = "v";
+            break;
+        case "+":
+    }
+}
+
+const is_cart = (ch) => {
+    switch (ch) {
+        case "^":
+        case "v":
+        case ">":
+        case "<":
+            return true;
+        default:
+            return false;
+    }
+}
+
+const do_tick = (world) => {
+    var next_q = [];
+    for (let cart of world.carts) {
+        console.log(cart.id);
+        world.map[cart.row][cart.col].cart = "";
+        switch (cart.tile) {
+            case "^":
+                cart.row -= 1;
+                break;
+            case "v":
+                cart.row += 1;
+                break;
+            case ">":
+                cart.col += 1;
+                break;
+            case "<":
+                cart.col -= 1;
+                break;
+        }
+        var next_sq = world.map[cart.row][cart.col];
+
+        if (!(next_sq == "|" || next_sq == "-"))
+            do_turn(world.map, cart, next_sq);
+        world.map[cart.row][cart.col].cart = cart;
+        add_cart_to_queue(next_q, cart, cart.row, cart.col);
+        world.carts = next_q;
+    }
+}
+
+var carts = [];
+const map = [];
+var fs = require("fs");
+const lines = fs.readFileSync("tracks_sm.txt").toString().split("\n");
+for (let r = 0; r < lines.length; r++) {
+    var sqs = lines[r].split("");
+    var row = [];
+    for (let c = 0; c < sqs.length; c++) {
+        let cart = "";
+        switch (sqs[c]) {
+            case "^":
+            case "v":
+                cart = cart_factory.make(sqs[c]);
+                add_cart_to_queue(carts, cart, r, c);
+                row.push({ track:"|", cart: cart});
+                break;
+            case ">":
+            case "<":
+                cart = cart_factory.make(sqs[c]);
+                add_cart_to_queue(carts, cart, r, c);
+                row.push({ track:"-", cart:cart });
+                break;
+            default:
+                row.push({ track:sqs[c], cart:cart} );
+                break;
+        }
+    }
+    map.push(row);
+}
+
+const world = { map:map, carts:carts };
+
+dump_map(map);
+
+const rl = require('readline');
+rl.emitKeypressEvents(process.stdin);
+process.stdin.setRawMode(true);
+process.stdin.on("keypress", (str, key) => {
+    if (key.name === "q") {
+        process.exit();
+    }
+    else {
+        do_tick(world);
+        dump_map(map);
+    }
+});
