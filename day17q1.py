416aef72170dd3a882472f82d96ce0ab8f17b986
Dana Larose <ywg.dana@gmail.com>
2018-12-18 01:34:43 -0600

    OMG day 17 q1 finally fucking done

--- day17q1.py
+++ day17q1.py
@@ -1,54 +1,85 @@
 
 def dump_scan(scan):
-    for row in scan:
-        print(row)
+    for r in range(len(scan)):
+        print(str(r).rjust(5, ' '), scan[r])
 
 def replace_ch_at_index(s, ch, index):
     return s[:index] + ch + s[index+1:]
 
-def flow_sideways(scan, r, source_col, left, bottom):
-    spill = False
-    #scan[r] = replace_ch_at_index(scan[r], "|", source_col - left)
+def count_water(scan):
+    total = 0
+    for j in range(5, len(scan)):
+        total += sum([1 for ch in scan[j] if ch in ("~", "|")])
 
-    for j in range(source_col - left + 1, len(scan[r])):
-        if scan[r][j] in ("#", "~"): break
-        scan[r] = replace_ch_at_index(scan[r], "~", j)
-        if r + 1 <= bottom and scan[r+1][j] == ".":
-            spill = True
-            just_add_water(scan, r + 1, j + left, left, bottom)
-            break
-    for j in range(source_col-left - 1, -1, -1):
-        if scan[r][j] in ("#", "~"): break
-        scan[r] = replace_ch_at_index(scan[r], "~", j)
-        if r + 1 <= bottom and scan[r+1][j] in ("."):
-            spill = True
-            just_add_water(scan, r + 1, j + left, left, bottom)
-            break
-    return spill
+    return total
 
-def just_add_water(scan, r, source_col, left, bottom):
-    # 1. Add the flowing water char to the row
-    scan[r] = replace_ch_at_index(scan[r], "|", source_col-left)
+def in_resevoir(scan, r, c):
+    sand = 0
+    right = False
+    for j in range(c, len(scan[r])):
+        if scan[r][j] == ".": sand += 1
+        if scan[r+1][j] == ".": break
+        if scan[r][j] == "#":
+            right = True
+            break;
+    left = False
+    for j in range(c, -1, -1):
+        if scan[r][j] == ".": sand += 1
+        if scan[r+1][j] == ".": break
+        if scan[r][j] == "#":
+            left = True
+            break;
 
-    # 2. If we can keep flowing downward, flow downward
-    if r == bottom: return True
-    if scan[r+1][source_col-left] in ("."):
-        spill = just_add_water(scan, r + 1, source_col, left, bottom)
-        if not spill:
-            return flow_sideways(scan, r, source_col, left, bottom)
-    elif scan[r+1][source_col-left] in ("#"):
-        return flow_sideways(scan, r, source_col, left, bottom)
-    elif scan[r+1][source_col-left] in ("~"):
-        return flow_sideways(scan, r + 1, source_col, left, bottom)
+    return right and left and sand > 0
 
-    return True
+def fill_resevoir(scan, r, source_col, left, bottom, spill_points):
+    while in_resevoir(scan, r, source_col - left):
+        spill_points.extend(flow_sideways(scan, r, source_col, left, bottom))
+        if len(spill_points) > 0: break
+        r -= 1
+    return r
 
+def pump_water(scan, r, source_col, left, bottom):
+    while r <= bottom and scan[r][source_col-left] in (".", "|"):
+        scan[r] = replace_ch_at_index(scan[r], "|", source_col-left)
+        r += 1
+
+    if r > bottom: return
+    
+    col = source_col - left
+    sq = scan[r][col]
+    if sq == "#" or (sq == "~" and in_resevoir(scan, r - 1, col)):
+        while True:
+            r -= 1
+            overflow = False
+            # flow right
+            for j in range(col, len(scan[r])):
+                if scan[r][j] == "#": break
+                if scan[r+1][j] in (".", "|"):
+                    scan[r] = replace_ch_at_index(scan[r], "|", j)
+                    pump_water(scan, r + 1, j + left, left, bottom)
+                    overflow = True
+                    break
+                else:
+                    scan[r] = replace_ch_at_index(scan[r], "~", j)
+            # flow left
+            for j in range(col, -1, -1):
+                if scan[r][j] == "#": break
+                if scan[r+1][j] in (".", "|"):
+                    scan[r] = replace_ch_at_index(scan[r], "|", j)
+                    pump_water(scan, r + 1, j + left, left, bottom)
+                    overflow = True
+                    break
+                else:
+                    scan[r] = replace_ch_at_index(scan[r], "~", j)
+            if overflow: return
+        
 clay = set()
 top = 10_000
 bottom = 0
 left = 10_000
 right = 0
-with open("clay_scan_sm2.txt", "r") as file:
+with open("clay_scan.txt", "r") as file:
     for line in file.readlines():
         l, r = line.strip().split(",")
         rng_s = int(r[r.find("=")+1:r.find(".")])
@@ -81,12 +112,8 @@ scan[0] = replace_ch_at_index(scan[0], "+", col_of_spring - left)
 for sq in clay:
     scan[sq[1]] = replace_ch_at_index(scan[sq[1]], "#", sq[0] - left)
 
-try:
-    just_add_water(scan, 1, col_of_spring, left, bottom)
-except IndexError:
-    pass
-#just_add_water(scan, 1, col_of_spring, left, bottom)
-#just_add_water(scan, 1, col_of_spring, left, bottom)
-#just_add_water(scan, 1, col_of_spring, left, bottom)
-#just_add_water(scan, 1, col_of_spring, left, bottom)
-dump_scan(scan)
+pump_water(scan, 1, col_of_spring, left, bottom)
+
+#dump_scan(scan)
+print(top, bottom)
+print(count_water(scan))
