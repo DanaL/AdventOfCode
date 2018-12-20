2093eb25673cbdcba29fe570e38ed86472a8d2ec
Dana Larose <ywg.dana@gmail.com>
2018-12-20 14:39:58 -0600

    Day 20: Q1 and Q2 finished

--- day20.py
+++ day20.py
@@ -3,7 +3,8 @@ class Node:
         self.row = 0
         self.col = 0
         self.exits = { "N":None, "S":None, "E":None, "W":None }
-
+        self.distance = 0
+        
 
 def dump_map(nodes):
     coords = [k for k in nodes.keys()]
@@ -16,26 +17,16 @@ def dump_map(nodes):
     carte = "#" * (width) + "\n"
     carte *= ((bottom - top + 1) * 2 + 1)
 
-    #                 111111 11112222 22222233 33333333
-    #      01234567 89012345 67890123 45678901 23456789
-    # s = "#######\n#.#.#.#\n#######\n#.#.#.#\n#######\n
     for coord in coords:
         row = (coord[0] - top + 1) * 2 - 1
         col = (coord[1] - left + 1) * 2 -1
         index = row * (width + 1) + col
-        carte = carte[:index] + "." + carte[index+1:]
+        sq = "X" if coord[0] == 0 and coord[1] == 0 else "."
+        carte = carte[:index] + sq + carte[index+1:]
         n = nodes[coord]
-        #if n.exits["N"] != None: carte = carte[:index-width+1] + "-" + carte[index-width+2:]
         if n.exits["E"] != None: carte =carte[:index+1] + "|" + carte[index+2:]
         if n.exits["S"] != None: carte =carte[:index+width+1] + "-" + carte[index+width+2:]
     print(carte)
-
-# (-1, 0) = 9  = 1 * 8 + 1 
-# (-1, 1) = 11 = 1 * 8 + 3
-# (-1, 2) = 13 = 1 * 8 + 5
-# ( 0, 0) = 25 = 3 * 8 + 1
-# ( 0, 1) = 27 = 3 * 8 + 3
-# ( 0, 2) = 29 = 3 * 8 + 5
 
 def opposite_dir(d):
     if d == "N": return "S"
@@ -48,30 +39,39 @@ def build_graph(re):
     start = Node()
     nodes = { (0,0):start }
     curr = start
-
+    junctions = []
+    
     for c in re[1:]:
         if c in "NESW":
             next_row, next_col = curr.row + deltas[c][0], curr.col + deltas[c][1]
             coord = (next_row, next_col)
-            nn = nodes[coord] if coord in nodes else Node()
+            if coord in nodes:
+                nn = nodes[coord]
+            else:
+                nn = Node()
+                nn.distance = curr.distance + 1
             nn.row = next_row
             nn.col = next_col
             curr.exits[c] = nn
             nn.exits[opposite_dir(c)] = curr
             nodes[coord] = nn
             curr = nn
+        elif c == "(":
+            junctions.append((curr.row, curr.col))
+        elif c == "|":
+            curr = nodes[junctions[-1]]
+        elif c == ")":
+            junctions.pop()
         elif c == "$":
             break
 
     dump_map(nodes)
     
-    return start
+    return nodes
 
+with open("floorplan.txt", "r") as f:
+    s = f.read().strip()
 
-
-test_re0 = "^EENWW$"
-test_re1 = "^ESSWWN(E|NNENN(EESS(WNSE|)SSS|WWWSSSSE(SW|NNNE)))$"
-test_re2 = "^WNE$"
-test_re3 = "^NNNNNESSSSSENNNNN$"
-
-build_graph(test_re3)
+nodes = build_graph(s)
+print("Q1:", max([nodes[k].distance for k in nodes]))
+print("Q2:", len([k for k in nodes if nodes[k].distance >= 1000]))
