3c011809b336c377d9569acafc952b7f53c184e9
Dana Larose <ywg.dana@gmail.com>
2018-12-23 14:11:07 -0600

    Day 23 q1 code formatting

--- day23.py
+++ day23.py
@@ -1,22 +1,26 @@
-def manhattan(a, b):
-    return abs(a[0] - b[0]) + abs(a[1] - b[1]) + abs(a[2] - b[2])
 
-widest = 0
-strongest = None
+def q1(nanobots):
+    widest = 0
+    strongest = None
+
+    for x, y, z, r in nanobots:
+        if r > widest:
+            widest = r
+            strongest = [x, y, z, r]
+            
+    bx, by, bz, br = strongest
+    in_range = 0
+    for x, y, z, _ in nanobots:
+        if abs(x - bx) + abs(y - by) + abs(z - bz) <= widest:
+            in_range += 1
+    
+    print(in_range)
+
 nanobots = []
 with open("nanobots.txt", "r") as f:
     for line in f.readlines():
         coords = [int(n) for n in line[line.find("<")+1:line.find(">")].split(",")]
-        r = int(line[line.find("r=")+2:].strip())
+        coords.append(int(line[line.find("r=")+2:].strip()))
         nanobots.append(coords)
-        if r > widest:
-            strongest = coords
-            widest = r
-
-bx, by, bz = strongest
-in_range = 0
-for x, y, z in nanobots:
-    if abs(x - bx) + abs(y - by) + abs(z - bz) <= widest:
-        in_range += 1
-    
-print(in_range)
+        
+q1(nanobots)
