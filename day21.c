b51fb7c71ba8cb9e865dd1aa61719f208ed86df0
Dana Larose <ywg.dana@gmail.com>
2018-12-23 14:10:35 -0600

    C version of day 21 elf interpreter

--- day21.c
+++ day21.c
@@ -21,7 +21,7 @@
 
 typedef struct instr {
     int opcode;
-    int regs[3];
+    int a, b, c;
 } instr;
 
 typedef struct machine {
@@ -31,50 +31,128 @@ typedef struct machine {
 } machine;
 
 struct instr parse_instr(char *line) {
-    char i[5];
-    i[4] = '\0';
+    int a, b, c;
+    char op[10];
     instr in;
 
-    memcpy(i, line, 4);
-
-    if (strcmp(i, "addi") == 0)
+    sscanf(line, "%s %d %d %d", op, &in.a, &in.b, &in.c);
+    if (strcmp(op, "addi") == 0)
         in.opcode = ADDI;
-    else if (strcmp(i, "addr") == 0)
+    else if (strcmp(op, "addr") == 0)
         in.opcode = ADDR;
-    else if (strcmp(i, "bani") == 0)
+    else if (strcmp(op, "bani") == 0)
         in.opcode = BANI;
-    else if (strcmp(i, "banr") == 0)
+    else if (strcmp(op, "banr") == 0)
         in.opcode = BANR;
-    else if (strcmp(i, "bori") == 0)
+    else if (strcmp(op, "bori") == 0)
         in.opcode = BORI;
-    else if (strcmp(i, "borr") == 0)
+    else if (strcmp(op, "borr") == 0)
         in.opcode = BORR;
-    else if (strcmp(i, "eqir") == 0)
+    else if (strcmp(op, "eqir") == 0)
         in.opcode = EQIR;
-    else if (strcmp(i, "eqri") == 0)
+    else if (strcmp(op, "eqri") == 0)
         in.opcode = EQRI;
-    else if (strcmp(i, "eqrr") == 0)
+    else if (strcmp(op, "eqrr") == 0)
         in.opcode = EQRR;
-    else if (strcmp(i, "gtir") == 0)
+    else if (strcmp(op, "gtir") == 0)
         in.opcode = GTIR;
-    else if (strcmp(i, "gtri") == 0)
+    else if (strcmp(op, "gtri") == 0)
         in.opcode = GTRI;
-    else if (strcmp(i, "gtrr") == 0)
+    else if (strcmp(op, "gtrr") == 0)
         in.opcode = GTRR;
-    else if (strcmp(i, "muli") == 0)
+    else if (strcmp(op, "muli") == 0)
         in.opcode = MULI;
-    else if (strcmp(i, "mulr") == 0)
+    else if (strcmp(op, "mulr") == 0)
         in.opcode = MULR;
-    else if (strcmp(i, "seti") == 0)
+    else if (strcmp(op, "seti") == 0)
         in.opcode = SETI;
-    else if (strcmp(i, "setr") == 0)
+    else if (strcmp(op, "setr") == 0)
         in.opcode = SETR;
     else {
-        printf("Unknown opcode: %s\n", i);
+        printf("Unknown opcode: %s\n", op);
         exit(0);
     }
 
     return in;
+}
+
+void execute_instr(machine *m, instr in) {
+    switch (in.opcode) {
+        case ADDI:
+            m->regs[in.c] = m->regs[in.a] + in.b;
+            break;
+        case ADDR:
+            m->regs[in.c] = m->regs[in.a] + m->regs[in.b];
+            break;
+        case MULR:
+            m->regs[in.c] = m->regs[in.a] * m->regs[in.b];
+            break;
+        case MULI:
+            m->regs[in.c] = m->regs[in.a] * in.b;
+            break;
+        case BANR:
+            m->regs[in.c] = m->regs[in.a] & m->regs[in.b];
+            break;
+        case BANI:
+            m->regs[in.c] = m->regs[in.a] & in.b;
+            break;
+        case BORR:
+            m->regs[in.c] = m->regs[in.a] | m->regs[in.b];
+            break;
+        case BORI:
+            m->regs[in.c] = m->regs[in.a] | in.b;
+            break;
+        case SETR:
+            m->regs[in.c] = m->regs[in.a];
+            break;
+        case SETI:
+            m->regs[in.c] = in.a;
+            break;
+        case GTIR:
+            m->regs[in.c] = in.a > m->regs[in.b] ? 1 : 0;
+            break;
+        case GTRI:
+            m->regs[in.c] = m->regs[in.a] > in.b ? 1 : 0;
+            break;
+        case GTRR:
+            m->regs[in.c] = m->regs[in.a] > m->regs[in.b] ? 1 : 0;
+            break;
+        case EQIR:
+            m->regs[in.c] = in.a == m->regs[in.b] ? 1 : 0;
+            break;
+        case EQRI:
+            m->regs[in.c] = m->regs[in.a] == in.b ? 1 : 0;
+            break;
+        case EQRR:
+            m->regs[in.c] = m->regs[in.a] == m->regs[in.b] ? 1 : 0;
+            break;
+        default:
+            printf("Unknown opcode: %d\n", in.opcode);
+            exit(-1);
+    }
+}
+
+void execute_program(machine *m, const instr prog[], int prog_length, int verbose) {
+    unsigned int executed = 0;
+    instr curr;
+    while (m->instr_val < prog_length) {
+        curr = prog[m->instr_val];
+        if (m->instr_val == 28) {
+            printf("Instr 28: %d\n", m->regs[3]);
+            printf("   %d %d %d %d\n", curr.opcode, curr.a, curr.b, curr.c);
+        }
+
+        m->regs[m->instr_ptr] = m->instr_val;
+        execute_instr(m, curr);
+        m->instr_val = m->regs[m->instr_ptr];
+        ++m->instr_val;
+        ++executed;
+        if (verbose && executed % 100000) {
+            printf("Curr state:\n");
+            printf("   %d %d %d %d %d %d\n", m->regs[0], m->regs[1], m->regs[2], m->regs[3],
+                m->regs[4], m->regs[5]);
+        }
+    }
 }
 
 int main(int argc, char **argv) {
@@ -109,7 +187,9 @@ int main(int argc, char **argv) {
         }
     }
 
-    for (int j = 0; j < prog_length; j++)
-        printf("%d\n", prog[j].opcode);
+    m.regs[0] = 3909249;
+    execute_program(&m, prog, prog_length, 0);
+    printf("%d\n", m.regs[0]);
+
     return 0;
 }
