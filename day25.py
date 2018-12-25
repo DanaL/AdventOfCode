bdea16e123efec5a31cc0d5b4264c5d0cae85720
Dana Larose <ywg.dana@gmail.com>
2018-12-25 00:32:12 -0600

    Day 25 done. It's pretty much Disjoint Set code I stole from crashRun

---
+++ day25.py
@@ -0,0 +1,51 @@
+class DSNode:
+    def __init__(self, value):
+        self.parent = self
+        self.rank = 0
+        self.value = value
+ 
+def find(node):
+    if node.parent == node:
+        return node
+    else:
+        _n = node.parent
+        while _n.parent != _n:
+            _n = _n.parent
+        node.parent = _n
+        return _n
+        
+def union(n1, n2):
+    _root1 = find(n1)
+    _root2 = find(n2)
+    if _root1.rank > _root2.rank:
+        _root2.parent = _root1
+    elif _root1.rank < _root2.rank:
+        _root1.parent = _root2
+    else:
+        _root2.parent = _root1
+        _root1.rank += 1
+
+def manhattan(pt1, pt2):
+    return abs(pt1[0] - pt2[0]) + abs(pt1[1] - pt2[1]) + abs(pt1[2] - pt2[2]) + abs(pt1[3] - pt2[3])
+
+pts = set()
+with open("4d_pts.txt", "r") as f:
+    for line in f.readlines():
+        p = tuple([int(n) for n in line.strip().split(",")])
+        pts.add(DSNode(p))
+
+
+for pt1 in pts:
+    for pt2 in pts:
+        if pt1.value != pt2.value and manhattan(pt1.value, pt2.value) <= 3:
+            union(pt1, pt2)
+
+constellations = set()
+for pt in pts:
+    v = find(pt).value
+    if not v in constellations:
+        constellations.add(v)
+print(len(constellations))
+
+
+
