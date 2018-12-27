04860d694dcdf2c11a9007f36a869fa7fc2b589b
Dana Larose <ywg.dana@gmail.com>
2018-12-27 00:20:33 -0600

    Day 23 q2 workingrm nanobots_sm.txt

--- day23.c
+++ day23.c
@@ -3,33 +3,96 @@
 #include <stdlib.h>
 #include <time.h>
 
-/* Mucking about with a heapq implementation, ripped straight out of my
+#define NUM_OF_BOTS 1000
+
+typedef struct coord {
+    long x, y, z, r;
+} coord;
+
+
+typedef struct searchbox {
+	long x, y, z;
+	long side_length;
+	int points_covered;
+} searchbox;
+
+long manhattan(long x0, long y0, long z0, long x1, long y1, long z1) {
+    return labs(x0 - x1) + labs(y0 - y1) + labs(z0 - z1);
+}
+
+long distance_range(int a, int lo, int hi) {
+	if (a < lo) return lo - a;
+	if (a > hi) return a - hi;
+	return 0;
+}
+
+void bots_covered_by_box(searchbox *box, coord bots[], int num_of_bots) {
+	int covered = 0;
+	long x0 = box->x, x1 = box->x + box->side_length;	
+	long y0 = box->y, y1 = box->y + box->side_length;	
+	long z0 = box->z, z1 = box->z + box->side_length;
+
+	for (int j = 0; j < num_of_bots; j++) {
+		long d = distance_range(bots[j].x, x0, x1);
+		d += distance_range(bots[j].y, y0, y1);
+		d += distance_range(bots[j].z, z0, z1);
+		if (d <= bots[j].r)
+			++covered;	
+	}
+
+	box->points_covered = covered;	
+}
+
+/* Simple priority queue with a heapq implementation, ripped straight out of my
  * university algorithms text */
-#define HEAP_LENGTH 16384
+#define HEAP_LENGTH NUM_OF_BOTS * NUM_OF_BOTS
 typedef struct heapq {
-	int heap[HEAP_LENGTH];
+	searchbox *heap[HEAP_LENGTH];
 	int heap_size;
 } heapq;
 
-void heap_push(heapq *q, int val) {
+/* Comparison function for the heap operations. A searchbox has higher priority
+	if it covers more bots. In the event of a tie, the box that is closer to the origin
+	wins */
+int cmp_searchboxes(searchbox *a, searchbox *b) {
+	if (a->points_covered > b->points_covered)
+		return 1;
+	else if (a->points_covered == b->points_covered) {
+		long ma = manhattan(a->x, a->y, a->z, 0, 0, 0);
+		long mb = manhattan(b->x, b->y, b->z, 0, 0, 0);
+	
+		return ma < mb;	
+	}
+
+	return 0;
+}
+
+void heap_push(heapq *q, searchbox *box) {
 	/* need to check if the heap is full */
+	if (q->heap_size >= HEAP_LENGTH) {
+		puts("Oops...heap exceeded max size");
+		exit(0);
+	}
+
 	int hole = ++q->heap_size;
 	
 	/* Percolate up */
-	for (; hole > 1 && val < q->heap[hole/2]; hole /= 2)
+	for (; hole > 1 && cmp_searchboxes(box, q->heap[hole/2]); hole /= 2)
 		q->heap[hole] = q->heap[hole/2];
-	q->heap[hole] = val;	
+
+	q->heap[hole] = box;	
 }
 
 void heap_percolate_down(heapq *q, int hole) {
 	int child;
-	int tmp = q->heap[hole];
+	searchbox *tmp = q->heap[hole];
 
 	for (; hole * 2 <= q->heap_size; hole = child) {
 		child = hole * 2;
-		if (child != q->heap_size && q->heap[child+1] < q->heap[child])
-			++child;
-		if (q->heap[child] < tmp)
+		if (child != q->heap_size && cmp_searchboxes(q->heap[child+1],q->heap[child]))
+			++child;	
+		
+		if (cmp_searchboxes(q->heap[child], tmp))
 			q->heap[hole] = q->heap[child];
 		else
 			break;
@@ -38,9 +101,9 @@ void heap_percolate_down(heapq *q, int hole) {
 	q->heap[hole] = tmp;
 }
 
-int heap_pop(heapq *q) {
+searchbox* heap_pop(heapq *q) {
 	/* should check if the heap is empty */
-	int result = q->heap[1];
+	searchbox *result = q->heap[1];
 	q->heap[1] = q->heap[q->heap_size--];
 	heap_percolate_down(q, 1);
 
@@ -61,119 +124,11 @@ typedef struct candidate {
     int best;
 } candidate;
 
-typedef struct coord {
-    long x, y, z, r;
-} coord;
-
 long rand_coord(long min, long max) {
     /* Straight out of the comp.lang.c faq */
     long r = min + rand() / (RAND_MAX / (max - min + 1) + 1);
 
     return r;
-}
-
-long manhattan(coord a, long x, long y, long z) {
-    return labs(a.x - x) + labs(a.y - y) + labs(a.z - z);
-}
-
-/* How many bots is this point (x, y, z) within range of? */
-int reachable_from(long x, long y, long z, coord bots[], int num_of_bots) {
-    int count = 0;
-
-    for (int j = 0; j < num_of_bots; j++) {
-        if (manhattan(bots[j], x, y, z) <= bots[j].r)
-            ++count;
-    }
-
-    return count;
-}
-
-void check_candidates(candidate cs[], int num_of_c, coord bots[], int num_of_bots) {
-    for (int j = 0; j < num_of_c; j++) {
-        cs[j].prev = cs[j].nearby;
-        cs[j].nearby = reachable_from(cs[j].x, cs[j].y, cs[j].z, bots, num_of_bots);
-        if (cs[j].nearby > cs[j].best)
-            cs[j].best = cs[j].nearby;
-    }
-}
-
-void search_by_neightbours(coord bots[], int num_of_bots) {
-    int best = 0, count, best_id;
-    long bx, by, bz;
-    coord curr;
-
-    for (int j = 0; j < num_of_bots; j++) {
-        curr = bots[j];
-        count = 0;
-        for (int k = 0; k < num_of_bots; k++) {
-            /* This calculates how many other bots' curr is within range of
-                (as opposed to how many bots are within curr's range) */
-            if (j != k && manhattan(bots[k], curr.x, curr.y, curr.z) <= bots[k].r) {
-                ++count;
-            }
-        }
-        if (count > best) {
-            if (count > 900) printf(">900 %d\n", j);
-            best = count;
-            best_id = j;
-            bx = curr.x;
-            by = curr.y;
-            bz = curr.z;
-        }
-        if (count == 0)
-            printf("%d has no neighbours :o\n", j);
-    }
-
-    printf("Best %d had %d neighbours\n", best_id, best);
-    printf("   %ld %ld %ld\n", bx, by, bz);
-    printf("   distance from origin: %ld\n", manhattan(bots[best_id], 0, 0, 0));
-    printf("   15972003, 44657553, 29285970\n");
-    // 89,915,526
-    // 76,468,059
-                                    //10000000  10000000  10000000
-    printf("--> %d\n", reachable_from(15972003, 44657553, 29285970, bots, num_of_bots));
-    printf("--> %d\n", reachable_from(15972003, 44657553, 29285970, bots, num_of_bots));
-    int deltas[] = { -50000000, 0, 100000000};
-    for (int dx = 0; dx < 3; dx++) {
-        for (int dy = 0; dy < 3; dy++) {
-            for (int dz = 0; dz < 3; dz++) {
-                count = reachable_from(bx+dx, by+dy, bz+dz, bots, num_of_bots);
-                if (count > 831)
-                    printf("%d %d %d %d\n", dx, dy, dz, count);
-            }
-        }
-    }
-}
-
-void pure_random_guessing(coord bots[], int num_of_bots, ranges r) {
-    int num_of_candidates = 9;
-    candidate candidates[num_of_candidates];
-
-    srand(time(0));
-    rand_coord(-1000, 1000);
-    for (int j = 0; j < num_of_candidates; j++) {
-        candidates[j].x = rand_coord(r.min_x, r.max_x) / 2;
-        candidates[j].y = rand_coord(r.min_y, r.max_y) / 2;
-        candidates[j].z = rand_coord(r.min_z, r.max_z) / 2;
-        candidates[j].nearby = 0;
-        candidates[j].best = 0;
-    }
-
-    check_candidates(candidates, num_of_candidates, bots, num_of_bots);
-
-    for (int n = 0; n < 1000; n++) {
-        for (int j = 0; j < num_of_candidates; j++) {
-            candidates[j].x += rand_coord(-10000000, 10000000);
-            candidates[j].y += rand_coord(-10000000, 10000000);
-            candidates[j].z += rand_coord(-10000000, 10000000);
-        }
-
-        check_candidates(candidates, num_of_candidates, bots, num_of_bots);
-    }
-
-    for (int j = 0; j < num_of_candidates; j++) {
-        printf("Best result for %d: %d\n", j, candidates[j].best);
-    }
 }
 
 void q2(coord bots[], int num_of_bots) {
@@ -197,8 +152,109 @@ void q2(coord bots[], int num_of_bots) {
             r.max_z = bots[j].z;
     }
 
-    //pure_random_guessing(bots, num_of_bots, r);
-    search_by_neightbours(bots, num_of_bots);
+	// And now, the search boxes. I need to start with a cube big enough to cover the full
+	// set of points, then split it into smaller and smaller boxes until I find a box whose
+	// side length is 1 and which covers the most bots. Search boxes are stored as an (x, y)
+	// point and a side-length. The (x, y) point are the "bottom-left" corner of the box.
+
+	/* Initial, giant box */ 
+	searchbox *box = malloc(sizeof(searchbox));
+	box->x = r.min_x;
+	box->y = r.min_y;
+	box->z = r.min_z;
+	long xrange = labs(r.max_x - r.min_x);
+	long yrange = labs(r.max_y - r.min_y);
+
+	/* We need the side length to be a power of 2 in order to ensure we can always evenly divide
+		the searchbox */	
+	box->side_length = 1;
+	while (box->x + box->side_length < r.max_x || box->y + box->side_length < r.max_y || box->z + box->side_length < r.max_z)
+		box->side_length *= 2;
+	bots_covered_by_box(box, bots, num_of_bots);
+	
+	heapq q;
+	q.heap_size = 0;
+	heap_push(&q, box);
+
+	while (q.heap_size > 0) {
+		box = heap_pop(&q);
+
+		if (box->side_length == 1) {
+			/* We are done! By definition, the first box we pop off the heap that
+				has side length of 1 is the point which covers the most bots */
+			printf("Q2: Bots covered: %d (%ld %ld %ld)\n", box->points_covered, box->x, box->y, box->z);
+			printf("        Distance from origin: %ld\n", manhattan(box->x, box->y, box->z, 0, 0, 0));
+			free(box);
+			break;
+		}
+
+		long new_side_length = box->side_length / 2;
+
+		/* Now create the eight sub-boxes */
+		searchbox *nb;
+
+		nb = malloc(sizeof(searchbox));
+		nb->x = box->x; nb->y = box->y; nb->z = box->z;
+		nb->side_length = new_side_length;
+		bots_covered_by_box(nb, bots, num_of_bots);
+		heap_push(&q, nb);
+
+		nb = malloc(sizeof(searchbox));
+		nb->x = box->x + new_side_length - 1; nb->y = box->y; nb->z = box->z;
+		nb->side_length = new_side_length;
+		bots_covered_by_box(nb, bots, num_of_bots);
+		heap_push(&q, nb);
+
+		nb = malloc(sizeof(searchbox));
+		nb->x = box->x; nb->y = box->y + new_side_length - 1; nb->z = box->z;
+		nb->side_length = new_side_length;
+		bots_covered_by_box(nb, bots, num_of_bots);
+		heap_push(&q, nb);
+
+		nb = malloc(sizeof(searchbox));
+		nb->x = box->x + new_side_length - 1; nb->y = box->y + new_side_length - 1; nb->z = box->z;
+		nb->side_length = new_side_length;
+		bots_covered_by_box(nb, bots, num_of_bots);
+		heap_push(&q, nb);
+
+		nb = malloc(sizeof(searchbox));
+		nb->x = box->x; nb->y = box->y; nb->z = box->z + new_side_length - 1;
+		nb->side_length = new_side_length;
+		bots_covered_by_box(nb, bots, num_of_bots);
+		heap_push(&q, nb);
+
+		nb = malloc(sizeof(searchbox));
+		nb->x = box->x + new_side_length - 1; nb->y = box->y; nb->z = box->z + new_side_length - 1;
+		nb->side_length = new_side_length;
+		bots_covered_by_box(nb, bots, num_of_bots);
+		heap_push(&q, nb);
+
+		nb = malloc(sizeof(searchbox));
+		nb->x = box->x; nb->y = box->y + new_side_length - 1; nb->z = box->z + new_side_length - 1;
+		nb->side_length = new_side_length;
+		bots_covered_by_box(nb, bots, num_of_bots);
+		heap_push(&q, nb);
+
+		nb = malloc(sizeof(searchbox));
+		nb->x = box->x + new_side_length - 1; nb->y = box->y + new_side_length - 1; nb->z = box->z + new_side_length - 1;
+		nb->side_length = new_side_length;
+		bots_covered_by_box(nb, bots, num_of_bots);
+		heap_push(&q, nb);
+
+		free(box);
+	}
+
+	/*
+	puts("Remaining:\n");
+	while (q.heap_size > 0) {
+		searchbox *b = heap_pop(&q);
+		if (b->points_covered == 977) {
+			printf("Points covered: %d (%ld %ld %ld)\n", b->points_covered, b->x, b->y, b->z);
+			printf("        Distance from origin: %ld\n", manhattan(b->x, b->y, b->z, 0, 0, 0));
+		}
+	}
+	*/
+	/* Free any remaining boxes on the heap */
 }
 
 void q1(coord bots[], int num_of_bots) {
@@ -215,7 +271,8 @@ void q1(coord bots[], int num_of_bots) {
     long mx = strongest.x, my = strongest.y, mz = strongest.z;
     int in_range = 0;
     for (int j = 0; j < num_of_bots; j++) {
-        if (manhattan(bots[j], mx, my, mz) <= widest)
+		coord b = bots[j];
+        if (manhattan(b.x, b.y, b.z, mx, my, mz) <= widest)
             ++in_range;
     }
 
@@ -223,7 +280,7 @@ void q1(coord bots[], int num_of_bots) {
 }
 
 int main() {
-    coord bots[1000];
+    coord bots[NUM_OF_BOTS];
     FILE *f = fopen("nanobots.txt", "r");
 
     if (f) {
@@ -236,19 +293,8 @@ int main() {
     }
     fclose(f);
 
-    //q1(bots, 1000);
-    //q2(bots, 1000);
-	heapq q;
-	q.heap_size = 0;
-	
-	srand(time(0));
-	for (int j = 0; j < 100; j++) {
-		heap_push(&q, rand() % 10000);
-	}
-
-	while (q.heap_size > 0) {
-		printf("%d\n", heap_pop(&q));
-	}	
+	q1(bots, NUM_OF_BOTS);
+	q2(bots, NUM_OF_BOTS);
 
     return 0;
 }
