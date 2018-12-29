f3e23a8f66c9eef6189907f53c83e25a5fd58781
Dana Larose <ywg.dana@gmail.com>
2018-12-29 16:16:28 -0600

    2017 Day 8: ditched my clunky switch statement to use eval() instead

--- 2017/day8.js
+++ 2017/day8.js
@@ -1,65 +1,31 @@
 const fs = require('fs').promises;
 
-let readInput = async () => {
-    let stuff = await fs.readFile('instructions.txt');
-    let lines = stuff.toString().split('\n');
-	lines = lines.filter(l => l.length > 0);
+let readInput = async (f) => {
+    let stuff = await fs.readFile(f);
+    let lines = stuff.toString().split('\n').filter(l => l.length > 0);
 
     return lines;
 }
 
 let parse_line = (line, regs) => {
 	const i = line.match(/([a-z]+) ([a-z]+) (-?\d+) if ([a-z]+) ([<>=!]+) (-?\d+)/);
-	const instr = { out_reg:i[1], delta:i[2], val:parseInt(i[3]), cmp_reg:i[4],
-						cmp:i[5], cmp_val:parseInt(i[6]) }; 
+	const instr = { out_reg:i[1], val: parseInt(i[3]),
+				expr:`regs["${i[4]}"] ${i[5]} ${parseInt(i[6])}` };
 
+	if (i[2] === "dec")
+		instr.val *= -1;
 	if (!(instr.out_reg in regs))
 		regs[instr.out_reg] = 0;	
-	if (!(instr.cmg_reg in regs))
-		regs[instr.out_reg] = 0;	
-
-	/* Then later I don't need to check if it was inc or dec. I can just add val */
-	if (instr.delta === "dec")
-		instr.val *= -1;
+	if (!(i[4] in regs))
+		regs[i[4]] = 0;	
 
 	return instr;
 }
 
-let eval = (instr, regs) => {
-	switch (instr.cmp) {
-		case "==":
-			if (regs[instr.cmp_reg] === instr.cmp_val)
-				regs[instr.out_reg] += instr.val;
-			break;
-		case "!=":
-			if (regs[instr.cmp_reg] !== instr.cmp_val)
-				regs[instr.out_reg] += instr.val;
-			break;
-		case ">=":
-			if (regs[instr.cmp_reg] >= instr.cmp_val)
-				regs[instr.out_reg] += instr.val;
-			break;
-		case ">":
-			if (regs[instr.cmp_reg] > instr.cmp_val)
-				regs[instr.out_reg] += instr.val;
-			break;
-		case "<=":
-			if (regs[instr.cmp_reg] <= instr.cmp_val)
-				regs[instr.out_reg] += instr.val;
-			break;
-		case "<":
-			if (regs[instr.cmp_reg] < instr.cmp_val)
-				regs[instr.out_reg] += instr.val;
-			break;
-		default:
-			console.log("Unknown op: " + instr.cmp);
-			break;
-	}
-}
-
 let q1 = (instructions, regs) => {
 	for (instr of instructions) {
-		eval(instr, regs);
+		if (eval(instr.expr))
+			regs[instr.out_reg] += instr.val;
 	}
 
 	console.log("Q1: " + Math.max(...Object.values(regs)));
@@ -68,7 +34,8 @@ let q1 = (instructions, regs) => {
 let q2 = (instructions, regs) => {
 	let hi = 0;
 	for (instr of instructions) {
-		eval(instr, regs);
+		if (eval(instr.expr))
+			regs[instr.out_reg] += instr.val;
 		let m = Math.max(...Object.values(regs));
 		if (m > hi)
 			hi = m;
@@ -78,10 +45,10 @@ let q2 = (instructions, regs) => {
 }
 
 let main = async () => {
-    const lines = await readInput();
+    const lines = await readInput("instructions.txt");
 	const regs = { };
 	const instructions = lines.map(line => parse_line(line, regs));
-
+	
 	q1(instructions, JSON.parse(JSON.stringify(regs)));	
 	q2(instructions, JSON.parse(JSON.stringify(regs)));	
 }
