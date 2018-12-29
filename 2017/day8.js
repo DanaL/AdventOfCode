619a5f6ba5d2cb21b9802414be96ea0002749c21
Dana Larose <ywg.dana@gmail.com>
2018-12-29 16:36:41 -0600

    2017 Day 8: if I'm going to use eval(), may as well go all the way

--- 2017/day8.js
+++ 2017/day8.js
@@ -9,33 +9,25 @@ let readInput = async (f) => {
 
 let parse_line = (line, regs) => {
 	const i = line.match(/([a-z]+) ([a-z]+) (-?\d+) if ([a-z]+) ([<>=!]+) (-?\d+)/);
-	const instr = { out_reg:i[1], val: parseInt(i[3]),
-				expr:`regs["${i[4]}"] ${i[5]} ${parseInt(i[6])}` };
-
-	if (i[2] === "dec")
-		instr.val *= -1;
-	if (!(instr.out_reg in regs))
-		regs[instr.out_reg] = 0;	
-	if (!(i[4] in regs))
-		regs[i[4]] = 0;	
+	let instr = `if (regs["${i[4]}"] ${i[5]} ${parseInt(i[6])}) regs["${i[1]}"] `;
+	instr += i[2] === "inc" ? "+= " : "-= ";
+	instr += parseInt(i[3]); 
+		
+	regs[i[1]] = 0;	
+	regs[i[4]] = 0;	
 
 	return instr;
 }
 
 let q1 = (instructions, regs) => {
-	for (instr of instructions) {
-		if (eval(instr.expr))
-			regs[instr.out_reg] += instr.val;
-	}
-
+	instructions.map(i => eval(i));
 	console.log("Q1: " + Math.max(...Object.values(regs)));
 }
 
 let q2 = (instructions, regs) => {
 	let hi = 0;
 	for (instr of instructions) {
-		if (eval(instr.expr))
-			regs[instr.out_reg] += instr.val;
+		eval(instr);
 		let m = Math.max(...Object.values(regs));
 		if (m > hi)
 			hi = m;
