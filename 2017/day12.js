829fcdd3639dadde1e516513eaa63e2e89793d27
Dana <dana@lrsd.net>
2019-01-02 15:23:39 -0600

    2017 Day 12 q1 finished

--- 2017/day12.js
+++ 2017/day12.js
@@ -1,4 +1,5 @@
 const fs = require('fs').promises;
+require("../DisjointSet");
 
 let readInput = async (f) => {
     let stuff = await fs.readFile(f);
@@ -12,11 +13,36 @@ let parse_line = (line) => {
     return { prog_id:parseInt(m[0]), connected:m[1].split(", ").map(Number) };
 }
 
+let q1 = (progs) => {
+    let nodes = {};
+
+    /* Use Union/Find to divide the programs into sets that are connected by
+        their communication pipes */
+    for (p of progs) {
+        if (!(p.prog_id in nodes))
+            nodes[p.prog_id] = DSNode(p.prog_id);
+        for (n of p.connected) {
+            if (!(n in nodes))
+                nodes[n] = DSNode(n);
+            nodes[p.prog_id].union(nodes[n]);
+        }
+    }
+
+    let count = 0;
+    let p0 = nodes[0].find();
+    for (n in nodes) {
+        if (p0 === nodes[n].find())
+            ++count;
+    }
+
+    console.log("Q1: " + count);
+}
+
 let main = async () => {
     const lines = await readInput("day12input.txt");
 	const progs = lines.map(parse_line);
 
-    console.log(progs);
+    q1(progs);
 }
 
 main();
