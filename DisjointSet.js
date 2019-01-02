1c5e2faa5c793e3ac3f09a1686ad86e6f5cc47c1
Dana <dana@lrsd.net>
2019-01-02 13:17:59 -0600

    Added a javscript implementation of DisjointSet, which will probably be useful in more than one, and future, problems

---
+++ DisjointSet.js
@@ -0,0 +1,32 @@
+DSNode = (val) => {
+    return {
+        parent:null,
+        rank:0,
+        value:val,
+
+        find() {
+            if (this.parent === null)
+                return this;
+            else {
+                let n = this.parent;
+                while (n.parent != null)
+                    n = n.parent;
+                this.parent = n;
+                return n;
+            }
+        },
+
+        union(n) {
+            let root1 = this.find();
+            let root2 = n.find();
+
+            if (root1.rank > root2.rank)
+                root2.parent = root1;
+            else if (root1.rank < root2.rank)
+                root1.parent = root2;
+            else
+                root2.parent = root1;
+                ++root1.rank;
+        }
+    }
+}
