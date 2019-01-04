039fbc9e86dc12e8ce230ce54047c42740f42c35
Dana <dana@lrsd.net>
2019-01-04 11:14:08 -0600

    Created a 'common' file as a place to keep functions that'll apparently be reused throughout the puzzles

---
+++ 2017/common.js
@@ -0,0 +1,56 @@
+let reverse = (arr, lo, hi) => {
+	while (lo < hi) {
+		let actual_lo = lo % arr.length;
+		let actual_hi = hi % arr.length;
+		let tmp = arr[actual_lo];
+		arr[actual_lo] = arr[actual_hi];
+		arr[actual_hi] = tmp;
+		++lo;
+		--hi;
+	}
+
+	return arr;
+}
+
+let knot_encrypt = (cypher_text, range, iterations) => {
+	let pos = 0;
+	let skip = 0;
+	let arr = [];
+	for (let j = 0; j < range; j++)
+		arr.push(j);
+
+	for (let j = 0; j < iterations; j++) {	
+		for (l of cypher_text) {
+			arr = reverse(arr, pos, pos + l - 1);
+			pos = (pos + l + skip++) % arr.length;
+		}
+	}
+
+	return arr;
+}
+
+let dense_hash = (arr, start, end) => {
+	let result = arr[start];
+	for (let j = start + 1; j < end; j++)
+		result ^= arr[j];
+
+	return result;
+}
+
+module.exports = {
+	knot_hash: ascii_str => {
+		// convert each character to its ascii value (0..255)
+		let lengths = ascii_str.split("").map(c => c.charCodeAt(0));
+		lengths = lengths.concat([17, 31, 73, 47, 23]);
+		let arr = knot_encrypt(lengths, 256, 64);
+
+		let dh = [];
+		for (j = 0; j < 16; j++) {
+			let start = 0 + (j * 16);
+			let end = start + 16;
+			dh.push(dense_hash(arr, start, end));
+		}
+
+		return dh.map(d => d < 16 ? "0" + d.toString(16) : d.toString(16)).join("");
+	}
+}
