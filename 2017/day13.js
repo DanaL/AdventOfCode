ead33b86324a2c30a5793e0c94ce51d3741b3660
Dana Larose <ywg.dana@gmail.com>
2019-01-04 07:46:03 -0600

    Working on 2017 Day 13 q2

--- 2017/day13.js
+++ 2017/day13.js
@@ -13,8 +13,7 @@ let parse_line = (line) => {
 	return { layer:pieces[0], depth:pieces[1], scanner:0, travel:1};
 }
 
-let print_firewall = (firewall, max_layer, max_depth, at_picosecond) => {
-	console.log("Picosecond: " + at_picosecond);
+let print_firewall = (firewall, max_layer, max_depth, packet, at_picosecond) => {
 	let header = "";
 	for (let j = 0; j <= max_layer; j++) {
 		header += " " + j + "  ";
@@ -24,47 +23,71 @@ let print_firewall = (firewall, max_layer, max_depth, at_picosecond) => {
 	for (let d = 0; d < max_depth; d++) {
 		let row = "";
 		for (let l = 0; l <= max_layer; l++) {
-			if (!(l in firewall) || d >= firewall[l].depth)
-				row += "    ";
+			if (!(l in firewall) || d >= firewall[l].depth) {
+				if (d === 0 && packet === l)
+					row += "( ) ";
+				else
+					row += "    ";
+			}
 			else if (d == firewall[l].scanner)
-				row += d == 0 && l == at_picosecond ? "(S) " : "[S] ";    	
+				row += d == 0 && l == packet ? "(S) " : "[S] ";    	
 			else
-				row += d == 0 && l == at_picosecond ? "( ) " : "[ ] ";    	
+				row += d == 0 && l == packet ? "( ) " : "[ ] ";    	
 		}
 		console.log(row);
 	}
 }
 
-let q1 = (firewall, max_layer, max_depth, verbose) => {
+let run = (firewall, max_layer, max_depth, verbose, delay) => {
 	let packet = 0;
 	let penalty = 0;
+	let picosecond = 0;
 
 	while (packet <= max_layer) {
-		if (verbose)
-				print_firewall(firewall, max_layer, max_depth, packet);
-		if (packet in firewall && firewall[packet].scanner == 0) {
-			penalty += firewall[packet].layer * firewall[packet].depth;
+		if (verbose) {
+			console.log("Picosecond: " + picosecond);
+			console.log("Packet at: " + packet);
+			if (picosecond >= delay)
+				print_firewall(firewall, max_layer, max_depth, packet, picosecond);
+			else
+				print_firewall(firewall, max_layer, max_depth, -1, picosecond);
+		}
+
+		if (picosecond >= delay) {
+			if (packet in firewall && firewall[packet].scanner == 0) 
+				penalty += firewall[packet].layer * firewall[packet].depth;
 		}
 
 		for (let l in firewall) {
 			firewall[l].scanner += firewall[l].travel;
-
 			if (firewall[l].scanner == 0 || firewall[l].scanner == (firewall[l].depth - 1))
 				firewall[l].travel *= -1;
 		}
+
+		if (verbose) {
+			console.log("");
+			if (picosecond >= delay)
+				print_firewall(firewall, max_layer, max_depth, packet, picosecond);
+			else
+				print_firewall(firewall, max_layer, max_depth, -1, picosecond);
+		}
 	
-		++packet;
+		if (picosecond >= delay)
+			++packet;
+	
+		++picosecond;	
 	}
 
-	console.log("Q1: " + penalty);
+	return penalty;
 }
 
 let main = async () => {
     const lines = await readInput("firewall.txt");
+	const test_lines = [ "0: 3", "1: 2", "4: 4", "6: 4"];
 	const firewall = {};
 	let max_layer = 0;
 	let max_depth = 0;
-	for (let line of lines) {
+	for (let line of test_lines) {
 		let layer = parse_line(line);
 		firewall[layer.layer] = layer;
 		if (layer.layer > max_layer)
@@ -73,7 +96,32 @@ let main = async () => {
 			max_depth = layer.depth;
 	}
 
-	q1(firewall, max_layer, max_depth, false);
+	let fw = JSON.parse(JSON.stringify(firewall));
+	console.log("Q1: " + run(fw, max_layer, max_depth, false, 0));
+
+	
+	fw = JSON.parse(JSON.stringify(firewall));
+	console.log(run(fw, max_layer, max_depth, false, 1));
+
+	fw = JSON.parse(JSON.stringify(firewall));
+	console.log(run(fw, max_layer, max_depth, false, 2));
+
+	fw = JSON.parse(JSON.stringify(firewall));
+	console.log(run(fw, max_layer, max_depth, true, 4));
+	
+
+	/* For Q2 we want to find the lowest delay that'll have penalty 0 */
+	/*
+	for (let delay = 8; ; delay++) {
+		fw = JSON.parse(JSON.stringify(firewall));
+		penalty = run(fw, max_layer, max_depth, false, delay);
+		//console.log(delay + " " + penalty);
+		if (penalty === 0) {
+			console.log("Q2: " + delay);
+			break;
+		}
+	}
+	*/
 }
 
 main();
