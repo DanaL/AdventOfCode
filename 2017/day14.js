a81e599b9afd6d67788f4296fe2f8cfcc9fcd46e
Dana <dana@lrsd.net>
2019-01-04 12:48:23 -0600

    2017 Day 14 q2: generating the grid in a separate function since I also need it for q2

--- 2017/day14.js
+++ 2017/day14.js
@@ -1,24 +1,36 @@
 const util = require("./common.js");
+const ds = require("../DisjointSet.js");
 
 let c_to_bit_str = (c) => {
 	return parseInt(c, 16).toString(2).padStart(4, "0");
 }
 
-let q1 = (seed) => {
-	let total_used = 0;
+let gen_grid = (seed) => {
+	const grid = [];
 	for (let j = 0; j < 128; j++) {
 		let hash = util.knot_hash(seed + "-" + j);
-		let bits = hash.split("").map(c_to_bit_str).join("");
-		total_used += bits.split("").filter(b => b == "1").length;
+		grid.push(hash.split("").map(c_to_bit_str).join(""));
 	}
+
+	return grid;
+}
+
+let q1 = (grid) => {
+	let total_used = 0;
+	for (let row of grid)
+		total_used += row.split("").filter(b => b == "1").length;
 	
 	console.log("Q1: " + total_used);
 }
 
+let q2 = (seed) => {
+}
+
 let main = () => {
 	const seed = "wenycdww";
-
-	q1(seed);
+	const grid = gen_grid("flqrgnkx");
+	
+	q1(grid);
 }
 
 main();
