e1319a8ab129907a1c4f444ad28570fa2c4f9ed0
Dana <dana@lrsd.net>
2019-01-04 12:47:47 -0600

    Made my DisjointSet file a properly exported module

--- DisjointSet.js
+++ DisjointSet.js
@@ -1,32 +1,34 @@
-DSNode = (val) => {
-    return {
-        parent:null,
-        rank:0,
-        value:val,
+module.exports = {
+    DSNode : (val) => {
+        return {
+            parent:null,
+            rank:0,
+            value:val,
 
-        find() {
-            if (this.parent === null || this.parent === this)
-                return this;
-            else {
-                let n = this.parent;
-                while (n.parent !== null && n.parent !== n)
-                    n = n.parent;
-                this.parent = n;
-                return n;
+            find() {
+                if (this.parent === null || this.parent === this)
+                    return this;
+                else {
+                    let n = this.parent;
+                    while (n.parent !== null && n.parent !== n)
+                        n = n.parent;
+                    this.parent = n;
+                    return n;
+                }
+            },
+
+            union(n) {
+                let root1 = this.find();
+                let root2 = n.find();
+
+                if (root1.rank > root2.rank)
+                    root2.parent = root1;
+                else if (root1.rank < root2.rank)
+                    root1.parent = root2;
+                else
+                    root2.parent = root1;
+                    ++root1.rank;
             }
-        },
-
-        union(n) {
-            let root1 = this.find();
-            let root2 = n.find();
-
-            if (root1.rank > root2.rank)
-                root2.parent = root1;
-            else if (root1.rank < root2.rank)
-                root1.parent = root2;
-            else
-                root2.parent = root1;
-                ++root1.rank;
         }
     }
 }
