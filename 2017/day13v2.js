523018a5118c368ff59c047e531ae9a1014f8314
Dana <dana@lrsd.net>
2019-01-04 09:59:26 -0600

    2017 Day 13 q2 by calculation -- was missing that we didn't care about severity, any time you are in a firewall layer when the scanner is there, it is a failure

--- 2017/day13v2.js
+++ 2017/day13v2.js
@@ -11,10 +11,16 @@ let main = async () => {
 	const lines = await readInput("firewall.txt");
 	const test_lines = [ "0: 3", "1: 2", "4: 4", "6: 4"];
 
-	const fw = test_lines.map(l => l.split(": ").map(Number));
+	/* Q1 by straight calculation */
+	const fw = lines.map(l => l.split(": ").map(Number));
 	let result = 0;
 	fw.filter(layer => layer[0] % ((layer[1] - 1) * 2 ) === 0).forEach(res => result += res[0] * res[1]);
-	console.log("Q2: " + result);
+	console.log("Q1: " + result);
+
+	let delay = 1;
+	while (fw.filter(layer => (layer[0] + delay) % ((layer[1] - 1) * 2 ) === 0).length > 0)
+		++delay;
+	console.log("Q2: " + delay);
 }
 
 main();
