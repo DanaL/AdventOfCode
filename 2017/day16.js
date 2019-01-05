41816b4ac2701ad5d12646bec555c36614b93834
Dana Larose <ywg.dana@gmail.com>
2019-01-04 22:30:07 -0600

    2017 Day 16 q1

---
+++ 2017/day16.js
@@ -0,0 +1,56 @@
+const fs = require('fs').promises;
+
+let readInput = async (f) => {
+    let stuff = await fs.readFile(f);
+    let lines = stuff.toString().split('\n').filter(l => l.length > 0);
+
+    return lines;
+}
+
+let rotate = (dancers, n) => {
+	tmp = [];
+	for (let j = 0; j < n; j++)
+		tmp.push(dancers.pop());
+	tmp.reverse();
+	return tmp.concat(dancers);
+}
+
+let dance = (dancers, steps) => {
+	for (step of steps) {
+		switch (step[0]) {
+			case "s":
+				var m = step.match(/s(\d+)/).map(Number);
+				dancers = rotate(dancers, m[1]);
+				break;
+			case "x":
+				var m = step.match(/x(\d+)\/(\d+)/).map(Number);
+				var tmp = dancers[m[1]];
+				dancers[m[1]] = dancers[m[2]];
+				dancers[m[2]] = tmp;
+				break;
+			case "p":
+				var l1 = step[1];
+				var l2 = step[3];
+				for (var a=0; dancers[a] != l1; a++);
+				for (var b=0; dancers[b] != l2; b++);
+				var tmp = dancers[a];
+				dancers[a] = dancers[b];
+				dancers[b] = tmp;
+				break;
+		}
+	}
+
+	return dancers;
+}
+
+let main = async () => {
+    const lines = await readInput("dance_steps.txt");
+	//let steps = "s1,x3/4,pe/b".split(",");
+	let dancers = Array.from("abcdefghijklmnop");
+	const steps = lines[0].split(",");
+
+	dancers = dance(dancers, steps);
+	console.log("Q1: " + dancers.join(""));
+}
+
+main();
