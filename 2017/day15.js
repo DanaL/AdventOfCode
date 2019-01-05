1fc0118a67c0fa3330023a181e97284b7b9ac50f
Dana Larose <ywg.dana@gmail.com>
2019-01-04 19:02:59 -0600

    2017 Day 15 Q2 done. I'm a bit shocked brute force worked :o

--- 2017/day15.js
+++ 2017/day15.js
@@ -1,9 +1,12 @@
 
+let next_a = (a) => (a * 16807) % 2147483647;
+let next_b = (b) => (b * 48271) % 2147483647;
+
 let q1 = (a, b) => {
 	let matches = 0;
 	for (let j = 0; j < 40000000; j++) {
-		a = (a * 16807) % 2147483647;
-		b = (b * 48271) % 2147483647;
+		a = next_a(a);
+		b = next_b(b);
 
 		if ((a & 65535) === (b & 65535)) ++matches;
 	}
@@ -12,27 +15,21 @@ let q1 = (a, b) => {
 }
 
 let q2 = (a, b) => {
-	let qa = [];
-	let qb = [];
+	let matches = 0;
+	for (let cmps = 0; cmps <= 5000000; cmps++) {
+		do { a = next_a(a) } while (a % 4 !== 0);
+		do { b = next_b(b) } while (b % 8 !== 0);
 		
-	for (let j = 0; j < 100; j++) { 
-		a = (a * 16807) % 2147483647;
-		b = (b * 48271) % 2147483647;
-
-		if (a % 4 == 0) qa.push(a);
-		if (b % 8 == 0) qb.push(b);
-
-		if (b % 8 == 0)
-			console.log(b);
-		//console.log(a.toString().padStart(15, " ") + b.toString().padStart(15, " "));
-		//console.log((a & 65535).toString().padStart(15, " ") + (b & 65535).toString().padStart(15, " "));
-		//console.log("");
+		if ((a & 65535) === (b & 65535)) 
+			++matches;
 	}
+
+	console.log("Q2: " + matches);
 }
 
 let main = () => {
 	q1(873, 583);
-	q2(65, 8921);
+	q2(873, 583);
 }
 
 main();
