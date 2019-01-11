aa2a6cebe14a0af0f7f7e484b3f46dd4c8a24331
Dana Larose <ywg.dana@gmail.com>
2019-01-10 21:27:48 -0600

    2017 day 22 q1 done

---
+++ 2017/day22.js
@@ -0,0 +1,87 @@
+const fs = require("fs").promises;
+const util = require("./common.js");
+
+let readInput = async (f) => {
+	let stuff = await fs.readFile(f);
+	let lines = stuff.toString().split("\n").map(l => l.trim()).filter(l => l.length > 0);
+
+	return lines;
+}
+
+let turn = (dir, turn) => {
+	if ((dir === "n" && turn === "left") || (dir === "s" && turn === "right"))
+		return "w";
+	else if ((dir === "n" && turn === "right") || (dir === "s" && turn === "left"))
+		return "e";
+	else if ((dir === "e" && turn === "right") || (dir === "w" && turn === "left"))
+		return "s";
+	else if ((dir === "e" && turn === "left") || (dir === "w" && turn === "right"))
+		return "n";
+}
+
+let dump = (coords, virus) => {
+	for (let r = -5; r < 5; r++) {
+		let row = "";
+		for (let c = -5; c < 5; c++) {
+			let p = [r, c];
+			if (p[0] === virus.co[0] && p[1] === virus.co[1])
+				row += "*";
+			else if (coords.has(p))
+				row += "#";
+			else
+				row += ".";
+		}
+		console.log(row);
+	}
+}
+
+let q1 = (coords) => {
+	const virus = { co:[0,0], dir:"n" };	
+	let infections = 0;
+	for (let j = 0; j < 10000; j++ ) {
+		if (coords.has(virus.co)) {
+			virus.dir = turn(virus.dir, "right");
+			coords.delete(virus.co);
+		}
+		else {
+			virus.dir = turn(virus.dir, "left");
+			coords.add(virus.co, true);
+			++infections;
+		}
+
+		if (virus.dir === "n")
+			virus.co[0] -= 1;
+		else if (virus.dir === "s")
+			virus.co[0] += 1;
+		else if (virus.dir === "w")
+			virus.co[1] -= 1;
+		else if (virus.dir === "e")
+			virus.co[1] += 1;
+	}
+	
+	console.log("Q1: " + infections);
+}
+
+let parse = (lines) => {
+	let lo_row = 0 - Math.floor(lines.length / 2);
+	let lo_col = 0 - Math.floor(lines[0].length / 2);
+	let coords = util.dict();
+
+	for (let r = 0; r < lines.length; r++) {
+		for (let c = 0; c < lines.length; c++) {
+			if (lines[r][c] == "#") 
+				coords.add([r + lo_row, c + lo_col], true);			
+		}
+	}
+	
+	return coords;
+}
+
+let main = async () => {
+	const lines = await readInput("day22_grid.txt");
+	//const lines = [ "..#", "#..", "..." ];
+	const coords = parse(lines);
+	q1(coords);
+}
+
+main();
