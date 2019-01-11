5a0b39850d2815bc573cf41b30f572893e8ae12b
Dana Larose <ywg.dana@gmail.com>
2019-01-11 07:06:38 -0600

    2017 day 23 q1 done

---
+++ 2017/day23.js
@@ -0,0 +1,72 @@
+const fs = require('fs').promises;
+
+const EXECUTING = 0;
+const WAITING = 1;
+const TERMINATED = 2;
+
+let readInput = async (f) => {
+    let stuff = await fs.readFile(f);
+    let lines = stuff.toString().split('\n').filter(l => l.length > 0);
+
+    return lines;
+}
+
+let execute = (vm, line) => {
+	const bits = line.split(" ");
+	const op = bits[0];
+	const reg = bits[1];
+	
+	/* The value of the operation can be either a numeric constant or
+		a register letter */
+	if (bits.length === 3)
+		var val = isNaN(bits[2]) ? vm.registers[bits[2]] : parseInt(bits[2]);
+	
+	if (!(reg in vm.registers))
+		vm.registers[reg] = 0;
+
+	switch (op) {
+		case "set":
+			vm.registers[reg] = val;
+			break;
+		case "sub":
+			vm.registers[reg] -= val;
+			break;
+		case "mul":
+			vm.registers[reg] *= val;
+			++vm.mults;
+			break;
+		case "jnz":
+			/* jump instructions can take the form jnz a b, jnz a -1,
+				jnz 1 3, or (although I didn't have an exanple of this) jnz 4, a */
+			let a = isNaN(reg) ? vm.registers[reg] : parseInt(reg);
+			if (a !== 0) {
+				vm.pc += val;
+				return;
+			}
+			break;
+		default:
+			console.log("Op " + bits[0] + " is unknown.");
+			return;
+	}
+	
+	++vm.pc;
+}
+
+let q1 = (lines) => {
+	const vm0 = { registers: { p:0 }, pc:0, mults:0, state:EXECUTING, }; 
+
+	while (vm0.state === EXECUTING) {
+		//console.log("vm0 " + lines[vm0.pc]);
+		execute(vm0, lines[vm0.pc], true);
+		if (vm0.pc < 0 || vm0.pc >= lines.length) vm0.state = TERMINATED;
+	}
+	
+	console.log("Q1: " + vm0.mults);
+}
+
+let main = async () => {
+    const lines = await readInput("day23_prog.txt");
+	q1(lines);
+}
+
+main();
