522b0d168e622d44be5614dcee5326d671c2e59f
Dana Larose <ywg.dana@gmail.com>
2019-01-13 16:54:48 -0600

    2017 day 24 q2 done

--- 2017/day24.js
+++ 2017/day24.js
@@ -8,41 +8,56 @@ let readInput = async (f) => {
     return lines;
 }
 
-let best_path = (components, connector) => {
-	const options = components.filter(c => c.a === connector || c.b === connector);
-	
-	if (options.length === 0)
-		return 0;
-
-	const results = [];
-	for (let op of options) {
-		results.push(op.total + best_path(
-			components.filter(c => c.a !== op.a || c.b !== op.b),
-				op.a === connector ? op.b : op.a));
-	}
-
-	return Math.max(...results);
-}
-
-let q1 = (components) => {
-	const results = []
-	for (let start of components.filter(c => c.a === 0 || c.b === 0)) {
-		results.push(start.total + 
-			best_path(components.filter(c=>c.a !== start.a || c.b !== start.b), 
-				start.a === 0 ? start.b : start.a));
-	}
-
-	console.log("Q1: " + Math.max(...results));
-}
-
 let parse = (line) => {
 	const m = line.match(/^(\d+)\/(\d+)$/).map(Number);
 	return { a:m[1], b:m[2], total: m[1] + m[2] }
 }
 
-let main = async () => {
-    const lines = await readInput("components.txt");
-	q1(lines.map(parse));
+let port_eq = (p0, p1) => p0.a === p1.a && p0.b === p1.b;
+
+let gen_bridges = (end, unused, total, depth) => {
+	bridges.push([total, depth]);
+	for (part of unused) {
+		if (part.a === end || part.b === end) {
+			let leftover = unused.filter(p => !port_eq(part, p));
+			let next_end = part.a === end ? part.b : part.a;
+			gen_bridges(next_end, leftover, total+part.total, depth+1);
+		}
+	}
 }
 
+let q1 = () => {
+	let strongest = 0;
+	for (bridge of bridges) {
+		if (bridge[0] > strongest) strongest = bridge[0];
+	}
+	console.log("Q1: " + strongest);
+}
+
+let q2 = () => {
+	let longest = 0;
+	let strongest = 0;
+	for (bridge of bridges) {
+		if (bridge[1] > longest) {
+			longest = bridge[1];
+			strongest = bridge[0];
+		}
+		else if (bridge[1] == longest && bridge[0] > strongest) 
+			strongest = bridge[0];		
+	}
+	console.log(`Q2: ${strongest} (${longest})`);
+}
+
+let main = async () => {
+    const lines = await readInput("components.txt");
+	const parts = lines.map(parse);
+	for (p of parts.filter(p => p.a === 0 || p.b === 0)) {
+		const unused = parts.filter(pt => !port_eq(p, pt));
+		gen_bridges(p.a === 0 ? p.b : p.a, unused, p.total, 0);	
+	}
+	q1();
+	q2();
+}
+
+let bridges = [];
 main();
