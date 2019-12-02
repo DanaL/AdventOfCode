a0893551513c2e31f8093ca2668f2e67af970d65
Dana Larose <ywg.dana@gmail.com>
2019-12-02 00:30:20 -0600

    Day 2 q1 done

--- 2019/src/day_two.rs
+++ 2019/src/day_two.rs
@@ -1,9 +1,32 @@
 pub fn solve(input_str: &str) {
-	let mut prog: Vec<i32> = input_str.trim().split(",")
-		.map(|a| a.parse::<i32>().unwrap()).collect();
-	//let prog: Vec<&str> = fs::read_to_string(filename).unwrap()
-	//	.trim().split(",").iter().map
+	let mut prog: Vec<usize> = input_str.trim().split(",")
+		.map(|a| a.parse::<usize>().unwrap()).collect();
 
-	println!("{:?}", prog[0]);
-	println!("{:?}", prog[1]);
+	// instructions say before we run we need to replace pos 1 with 12
+	// and replace pos 2 with 2
+	prog[1] = 12;
+	prog[2] = 2;
+
+	let mut ptr = 0;
+	loop {
+		match prog[ptr] {
+			// add and write back
+			1  => {
+				let wi = prog[ptr+3];
+				prog[wi] = prog[prog[ptr+1]] + prog[prog[ptr+2]];
+			},
+			// multiply and write back
+			2  => {
+				let wi = prog[ptr+3];
+				prog[wi] = prog[prog[ptr+1]] * prog[prog[ptr+2]];
+			},
+			// halt!
+			99 => break,
+			// I don't think this should ever happen with our input?
+			_  => panic!("Hmm this shouldn't happen..."),
+		}
+		ptr += 4;
+	}
+
+	println!("Pos zero: {}", prog[0]);
 }
--- 2019/src/main.rs
+++ 2019/src/main.rs
@@ -5,4 +5,5 @@ mod day_two;
 fn main() {
 	let day2_input = fs::read_to_string("./inputs/day2.txt").unwrap();
 	day_two::solve(day2_input.trim());
+	//day_two::solve("1,9,10,3,2,3,11,0,99,30,40,50");
 }
