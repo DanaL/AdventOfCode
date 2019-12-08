42ddb731992789960f84c03e5f69aa801815a3b7
Dana Larose <ywg.dana@gmail.com>
2019-12-08 11:12:56 -0600

    Drastically cleaned up Day 8 Q1 after learning about chunks() and min_by_key() functions for arrays

--- 2019/src/day_eight.rs
+++ 2019/src/day_eight.rs
@@ -1,32 +1,14 @@
 use std::fs;
 
+fn count_ch(v: &[char], t: char) -> u32 {
+	v.iter().filter(|&&ch| ch == t).count() as u32
+}
+
 pub fn solve_q1() {
-	let img = fs::read_to_string("./inputs/day8.txt").unwrap().trim().to_string();
-
-	let mut count = 0;
-	let mut fewest_zeroes = u32::max_value();
-	let (mut zeroes, mut ones, mut twos) = (0, 0, 0);
-	let mut calc = 0;
-	for ch in img.chars() {
-		count += 1;
-		match ch  {
-			'0' => zeroes += 1,
-			'1' => ones += 1,
-			'2' => twos += 1,
-			_ => panic!("This shouldn't happen."),
-		}
-		if count % 150 == 0 {
-			if zeroes < fewest_zeroes {
-				calc = ones * twos;
-				fewest_zeroes = zeroes;
-			}
-			zeroes = 0;
-			ones = 0;
-			twos = 0;
-		}
-	}
-
-	println!("Q1: {}", calc);	
+	let pixels = fs::read_to_string("./inputs/day8.txt").unwrap().trim().chars().collect::<Vec<_>>();
+	let layers: Vec<_> = pixels.chunks(25 * 6).collect();
+	let fewest_zeroes = layers.iter().min_by_key(|l| count_ch(&l, '0')).unwrap();
+	println!("Q1: {:?}", count_ch(&fewest_zeroes, '1') * count_ch(&fewest_zeroes, '2'));
 }
 
 pub fn solve_q2() {
@@ -42,7 +24,7 @@ pub fn solve_q2() {
 			pixels[i] = ch;
 		}
 		i += 1;
-		if i % 150 == 0 {
+		if i == 150 {
 			i = 0;
 		}	
 	}
