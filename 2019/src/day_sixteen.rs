23ebbfec3a542c2a9e886fe5aab23c09323ffdc4
Dana Larose <ywg.dana@gmail.com>
2019-12-16 23:45:53 -0600

    Day 16 part 2 done

--- 2019/src/day_sixteen.rs
+++ 2019/src/day_sixteen.rs
@@ -47,17 +47,48 @@ pub fn solve_q1() {
 	println!("{:?}", signal);
 }
 
+// So we know from what part of the signal we want that we
+// are going to be multiplying our vector by a matrix that looks
+// like:
+//
+//                 1 1 1 1 1 1 1 1
+//                 0 1 1 1 1 1 1 1
+//                 0 0 1 1 1 1 1 1
+//                 0 0 0 1 1 1 1 1
+//                 0 0 0 0 1 1 1 1
+//                 0 0 0 0 0 1 1 1
+//                 0 0 0 0 0 0 1 1
+//                 0 0 0 0 0 0 0 1
+//
+// So we can start with the final digit, mod it and write it to the
+// array. Then the 2nd last digit is the previous digit plus itself, % 10.
+// Third last digit is the running sum % 10. So we can process our signal
+// (for this particular problem at least) with a single loop
+fn do_fft_q2(v: Vec<u8>, num_of_digits: usize) -> Vec<u8> {
+	let mut nv: Vec<u8> = vec![0;num_of_digits];
+	
+	let mut sum: u64 = 0;
+	for j in (0..num_of_digits).rev() {
+		sum += v[j] as u64;
+		nv[j] = (sum % 10) as u8;
+	}
+
+	nv
+}
+
 pub fn solve_q2() {
 	let signal_txt = fs::read_to_string("./inputs/day16.txt").unwrap();
 	let mut signal: Vec<u8> = signal_txt.trim()
 		.chars()
 		.map(|d| d.to_digit(10).unwrap() as u8)
 		.collect();
-	let offset = 5_979_191;
 
-	let mut v: Vec<u8> = vec![0;520809];
+	let num_of_digits = 520809;
 
-	let mut j = 520808;
+	// I want the last 520809 digits so copy them into a big ol' array
+	// in reverse order. 
+	let mut v: Vec<u8> = vec![0;num_of_digits];
+	let mut j = num_of_digits-1;
 	'outer: loop {
 		for x in (0..650).rev() {
 			v[j] = signal[x];
@@ -67,7 +98,10 @@ pub fn solve_q2() {
 	}
 	v[0] = 7; // sigh....
 
-	for j in 0..25 {
-		println!("{}", v[j]);
+	for j in 0..100 {	
+		v = do_fft_q2(v, num_of_digits);
 	}
+
+	println!("{} {} {} {} {} {} {} {}",
+		v[0], v[1], v[2], v[3], v[4], v[5], v[6], v[7]);	
 }
