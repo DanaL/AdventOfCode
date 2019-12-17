174943a26a869d515f4e623924072338a66b9a96
Dana Larose <dana@lrsd.net>
2019-12-17 17:07:55 -0600

    Day 17 Q2: began poking at the 'program' for the bot's moves

--- 2019/src/day_seventeen.rs
+++ 2019/src/day_seventeen.rs
@@ -56,3 +56,72 @@ pub fn solve_q1() {
     dump_grid(&grid);
     count_intersections(&grid);
 }
+
+pub fn solve_q2() {
+    let prog_txt = fs::read_to_string("./inputs/day17.txt").unwrap();
+	let mut vm = intcode_vm::IntcodeVM::new();
+	vm.load(prog_txt.trim());
+    vm.write(0, 2);
+
+    // Set up program
+    // A,B,C
+    vm.write_to_buff(65);
+    vm.write_to_buff(44);
+    vm.write_to_buff(66);
+    vm.write_to_buff(44);
+    vm.write_to_buff(67);
+    vm.write_to_buff(10);
+
+    // A= L12, R4, R4, L6
+    vm.write_to_buff(76);
+    vm.write_to_buff(44);
+    vm.write_to_buff(49);
+    vm.write_to_buff(50);
+    vm.write_to_buff(44);
+    vm.write_to_buff(82);
+    vm.write_to_buff(44);
+    vm.write_to_buff(52);
+    vm.write_to_buff(44);
+    vm.write_to_buff(82);
+    vm.write_to_buff(44);
+    vm.write_to_buff(52);
+    vm.write_to_buff(44);
+    vm.write_to_buff(76);
+    vm.write_to_buff(44);
+    vm.write_to_buff(54);
+
+
+    vm.write_to_buff(10);
+
+
+    // B= R,12
+    vm.write_to_buff(82);
+    //vm.write_to_buff(44);
+    //vm.write_to_buff(12);
+    vm.write_to_buff(10);
+
+    // C= L,4
+    vm.write_to_buff(76);
+    //vm.write_to_buff(44);
+    //vm.write_to_buff(4);
+    vm.write_to_buff(10);
+
+    // continuous display
+    //vm.write_to_buff(121);
+    vm.write_to_buff(110);
+    vm.write_to_buff(10);
+
+    let mut buffer = String::from("");
+    while vm.state != intcode_vm::VMState::Halted {
+        vm.run();
+        if vm.output_buffer == 10 {
+            println!("{}", buffer);
+            buffer = String::from("");
+        }
+        else {
+            buffer.push(vm.output_buffer as u8 as char);
+        }
+        //println!("{:?}, {}", vm.state, vm.output_buffer);
+        //break;
+    }
+}
--- 2019/src/main.rs
+++ 2019/src/main.rs
@@ -2,5 +2,6 @@ mod day_seventeen;
 mod intcode_vm;
 
 fn main() {
-	day_seventeen::solve_q1();
+	//day_seventeen::solve_q1();
+	day_seventeen::solve_q2();
 }
