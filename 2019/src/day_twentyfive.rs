5797e3cab8d5a0ee4c87fe75ad92b963c18932b3
Dana Larose <ywg.dana@gmail.com>
2019-12-26 10:19:36 -0600

    Day 25 done

--- 2019/src/day_twentyfive.rs
+++ 2019/src/day_twentyfive.rs
@@ -16,50 +16,13 @@ fn display_msg(vm: &mut intcode_vm::IntcodeVM) {
 	println!("{}", msg);
 }
 
-fn do_north(vm: &mut intcode_vm::IntcodeVM) {
-	vm.write_to_buff(110);
-	vm.write_to_buff(111);
-	vm.write_to_buff(114);
-	vm.write_to_buff(116);
-	vm.write_to_buff(104);
+fn send_cmd(cmd: &str, vm: &mut intcode_vm::IntcodeVM) {
+	for ch in cmd.chars() {
+		vm.write_to_buff(ch as i64);
+	}
 	vm.write_to_buff(10);
 	vm.run();
-}
 
-fn do_south(vm: &mut intcode_vm::IntcodeVM) {
-	vm.write_to_buff(115);
-	vm.write_to_buff(111);
-	vm.write_to_buff(117);
-	vm.write_to_buff(116);
-	vm.write_to_buff(104);
-	vm.write_to_buff(10);
-	vm.run();
-}
-
-fn do_east(vm: &mut intcode_vm::IntcodeVM) {
-	vm.write_to_buff(101);
-	vm.write_to_buff(97);
-	vm.write_to_buff(115);
-	vm.write_to_buff(116);
-	vm.write_to_buff(10);
-	vm.run();
-}
-
-fn do_west(vm: &mut intcode_vm::IntcodeVM) {
-	vm.write_to_buff(119);
-	vm.write_to_buff(101);
-	vm.write_to_buff(115);
-	vm.write_to_buff(116);
-	vm.write_to_buff(10);
-	vm.run();
-}
-
-fn do_inv(vm: &mut intcode_vm::IntcodeVM) {
-	vm.write_to_buff(105);
-	vm.write_to_buff(110);
-	vm.write_to_buff(118);
-	vm.write_to_buff(10);
-	vm.run();
 }
 
 pub fn solve() {
@@ -75,15 +38,11 @@ pub fn solve() {
 		let mut cmd = String::new();
 		io::stdin().read_line(&mut cmd)
 			.expect("Failed to read line");
-		match cmd.trim() {
-			"quit" => break,
-			"north" => do_north(&mut vm),
-			"south" => do_south(&mut vm),
-			"east" => do_east(&mut vm),
-			"west" => do_west(&mut vm),
-			"inv" => do_inv(&mut vm),
-			_ => println!("Unknown cmd: {}", cmd.trim()),
-		}	
+
+		if cmd.trim() == "quit" {
+			break;
+		}
+		send_cmd(cmd.trim(), &mut vm);
 		display_msg(&mut vm);
 	}	
 }
