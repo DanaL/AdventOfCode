72471e33020f05f2da53c5efdd90b4b8b7e696fb
Dana Larose <ywg.dana@gmail.com>
2019-12-26 23:43:52 -0600

    Day 24 part 2 done

--- 2019/src/day_twentyfour.rs
+++ 2019/src/day_twentyfour.rs
@@ -89,9 +89,8 @@ fn get_empty_grid() -> [[bool; 5]; 5] {
 	grid
 }
 
-fn check_q2(layer_num: usize, r: usize, c: usize, layers: &mut VecDeque<[[bool; 5]; 5]>) {
+fn check_q2(layer_num: usize, r: usize, c: usize, layers: &VecDeque<[[bool; 5]; 5]>) -> bool {
 	let mut bug_count = 0;
-	let layer = &mut layers[layer_num];	
 
 	if layer_num > 0 {
 		// If we are above layer 0, the first row of this grid is adjacent to the middle column 
@@ -101,9 +100,9 @@ fn check_q2(layer_num: usize, r: usize, c: usize, layers: &mut VecDeque<[[bool; 
 		if r == 4 && layers[layer_num - 1][3][2] { bug_count += 1 };
 		// Similarly for the encompassing columns
 		if c == 0 && layers[layer_num - 1][2][1] { bug_count += 1 };
-		if c == 0 && layers[layer_num - 1][2][3] { bug_count += 1 };
+		if c == 4 && layers[layer_num - 1][2][3] { bug_count += 1 };
 	}
-
+	
 	// if we are not at the last layer in the queue, the squares surround the centre (2, 2)
 	// are adjacent to the squares in the recursed layer
 	if layer_num < layers.len() - 1 {
@@ -128,30 +127,77 @@ fn check_q2(layer_num: usize, r: usize, c: usize, layers: &mut VecDeque<[[bool; 
 			}
 		} 
 	}
-
+	
 	// Okay, after potentially checking the layers encompassing and 
 	// encompassed by this later, later's consider the the point's neighbours
 	// on this later (which is basically the same as q1)
+	let layer = &layers[layer_num];	
+	if r > 0 && layer[r - 1][c] { bug_count += 1 }
+	if r < layer.len() - 1 && layer[r + 1][c] { bug_count += 1 }
+	if c > 0 && layer[r][c - 1] { bug_count += 1 }
+	if c < layer[0].len() - 1 && layer[r][c + 1] { bug_count += 1 }
+
+	if layer[r][c] && bug_count != 1 {
+		return false
+	}
+
+	if !layer[r][c] && (bug_count == 1 || bug_count == 2) {
+		return true
+	}
+
+	layer[r][c]
+}
+
+fn iterate_q2(layer_num: usize, layers: &VecDeque<[[bool; 5]; 5]>) -> [[bool; 5]; 5] {
+	let mut ng = layers[layer_num].clone();
+	for r in 0..ng.len() {
+		for c in 0..ng.len() {
+			// If I understand the problem, the centre square doesn't really exist
+			// anymore
+			if r == 2 && c == 2 { continue } 
+
+			ng[r][c] = check_q2(layer_num, r, c, layers);
+		}
+	}
+
+	ng
+}
+
+fn count_bugs(layers: &VecDeque<[[bool; 5]; 5]>) -> u32 {
+	let mut sum = 0;
+	for layer in layers {
+		for row in layer {
+			for col in row {
+				if *col { sum += 1 }
+			}
+		}
+	}
+
+	sum
 }
 
 pub fn solve_q2() {
-	let mut state: [[bool; 5]; 5] = [
-		[false, false, false, false, true],
-		[true, false, false, true, false],
-		[true, false, false, true, true],
-		[false, false, true, false, false],
-		[true, false, false, false, false]];
+	let state: [[bool; 5]; 5] = [
+		[true, false, true, false, false],
+		[false, true, false, true, false],
+		[true, false, false, false, true],
+		[false, true, false, false, true],
+		[true, true, false, true, false]];
 	
 	let mut layers = VecDeque::new();
 	layers.push_back(state);
 	
-	for _ in 0..10 {
+	for _ in 0..200 {
 		// Since I'm always going to have to add a blank grid to the beginning and end
 		// of the queue of layers. (I think...)
 		layers.push_front(get_empty_grid());
 		layers.push_back(get_empty_grid());
-		check_q2(0, 0, 0, &mut layers);
-		dump_q2(&layers);
-		break;
+		let mut next_layers = layers.clone();
+		for j in 0..layers.len() {
+			next_layers[j] = iterate_q2(j, &layers);
+		}
+		layers = next_layers;	
 	}	
+
+	println!("Q2: {}", count_bugs(&layers));
 }
