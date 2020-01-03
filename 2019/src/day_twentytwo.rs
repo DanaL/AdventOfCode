c021160e68d70d2569f97e1b571531702191f959
Dana Larose <dana@lrsd.net>
2020-01-03 16:06:23 -0600

    Math-y version of Q1. Q2 acts like it is working, but it is lying :(

--- 2019/src/day_twentytwo.rs
+++ 2019/src/day_twentytwo.rs
@@ -1,92 +1,4 @@
-use std::collections::VecDeque;
 use std::fs;
-use std::i32;
-
-struct Deck {
-	cards: VecDeque<usize>,
-	size: usize,
-}
-
-impl Deck {
-	fn new(size: usize) -> Deck {
-		Deck { size, cards: (0..size).collect() }
-	}
-
-	fn deal(&mut self) {
-		let mut stack = VecDeque::with_capacity(self.size);
-
-		for j in 0..self.size {
-			let x = self.cards.pop_back().unwrap();
-			stack.push_back(x);
-		}
-
-		self.cards = stack;
-	}
-
-	fn cut(&mut self, count: i32) {
-		if count > 0 {
-			self.cards.rotate_left(count as usize);
-		} else if count < 0 {
-			self.cards.rotate_right(i32::abs(count) as usize);
-		}
-	}
-
-	fn deal_with_inc(&mut self, inc: usize) {
-		let mut stack: VecDeque<usize> = (0..self.size).map(|_| 0).collect();
-		let mut pos = 0;
-
-		while self.cards.len() > 0 {
-			let a = self.cards.pop_front().unwrap();
-			if let Some(b) = stack.get_mut(pos) {
-				*b = a;
-			}
-			pos = (pos + inc) % self.size;
-		}
-
-		self.cards = stack;
-	}
-}
-
-pub fn solve_q1() {
-	let input = fs::read_to_string("./inputs/day22.txt").unwrap();
-	let mut deck = Deck::new(10007);
-
-	for line in input.trim().split('\n') {
-		let pieces: Vec<&str> = line.split(' ').collect();
-		if pieces[0] == "cut" {
-			let count = pieces[1].trim().parse::<i32>().unwrap();
-			deck.cut(count);
-		} else if pieces[1] == "with" {
-			let inc = pieces[3].trim().parse::<usize>().unwrap();
-			deck.deal_with_inc(inc);
-		} else {
-			deck.deal();
-		}
-	}
-
-	for j in 0..10007 {
-		if deck.cards[j] == 2019 {
-			println!("Q1: {}", j);
-		}
-	}
-}
-
-// From https://en.wikipedia.org/wiki/Modular_exponentiation#Right-to-left_binary_method
-fn modular_pow(base: i128, exp_val: i128, modulus: i128) -> i128 {
-	if modulus == 1 { return 0 }
-	let mut result = 1;
-	let mut b = base % modulus;
-	let mut exp = exp_val;
-	while exp > 0 {
-		if (exp % 2 == 1) {
-			result = (result * b) % modulus;
-		}
-		exp >>= 1;
-		b = (b * b) % modulus;
-	}
-
-	result
-}
 
 // For Part 2, I followed this blog's tutorial for how to approach simplifying the giant
 // values they want us to deal with: https://codeforces.com/blog/entry/72593
@@ -118,25 +30,86 @@ fn compose_lgc(a: i128, b: i128, m: i128, line: String) -> (i128, i128) {
 	(coeff1, coeff2)
 }
 
-pub fn solve_q2() {
+fn modular_pow(base: i128, exponent: i128, modulus: i128) -> i128 {
+	if modulus == 1 {
+		return 0;
+	}
+
+	let mut result = 1;
+	let mut base_new = base.rem_euclid(modulus);
+	let mut exp = exponent;
+	while exp > 0 {
+		if exp % 2 == 1 {
+			result = (result * base_new).rem_euclid(modulus);
+		}
+		exp = exp >> 1;
+		base_new = (base_new * base_new).rem_euclid(modulus);
+	}
+
+	result
+}
+
+// Calcualte the sum of the geomatric series:
+// a^k * x + (a ^ k-1 + a ^ k-2 + ... + a + 1) * b mod m, or:
+//
+// F^k(x) = (a^k * x + b(1 - a^k) / 1 - a) mod m
+//
+// But we need to use modular inverse for division because this is
+// all modula m, so p / q = p * q^-1 and q^-1 is done by modular_pow(q, m - 2 , m),
+// which apparently works because m is a prime???
+fn sum_series(x: i128, a: i128, b: i128, k: i128, m: i128) -> i128 {
+	let ak = modular_pow(a, k, m);
+	let p = b * (1 - ak);
+	let q = 1 - a;
+
+	// p * q^-1, calcualte by modular inverse
+	let p_div_q = (p * modular_pow(q, m - 2, m)).rem_euclid(m);
+
+	let f = ak * x + p_div_q;
+
+	f.rem_euclid(m)
+}
+
+fn invert_f(x: i128, a: i128, b: i128, k: i128, m: i128) -> i128 {
+	let ak = modular_pow(a, k, m);
+	let p = b * (1 - ak);
+	let q = 1 - a;
+
+	// p * q^-1, calcualte by modular inverse
+	let p_div_q = (p * modular_pow(q, m - 2, m)).rem_euclid(m);
+
+	// using modular inverse again to calculate F^-k(x), or
+	// x - B / A mod m, where B = b * (1 - a^k) / 1 - a and a = a^k
+	let inv = ((x - p_div_q) * modular_pow(ak, m - 2, m)).rem_euclid(m);
+
+	inv
+}
+
+pub fn solve_q1_mathy() {
 	let input = fs::read_to_string("./inputs/day22.txt").unwrap();
 	let mut coeffs = (1, 0);
-	let m = 119_315_717_514_047;
+	let m = 10007;
 	for line in input.trim().split('\n') {
 		coeffs = compose_lgc(coeffs.0, coeffs.1, m, line.trim().to_string());
 	}
 
-	println!("{:?}", coeffs);
+	let k = 1;
+	let res = sum_series(2019, coeffs.0, coeffs.1, k, m);
+	println!("Loc of {}: {}", 2019, res);
+	println!("Test inversion: {}", invert_f(res, coeffs.0, coeffs.1, k, m));
+	println!("(ie., where was the card in {} before the shuffle?)\n", res);
+}
 
-	let k: i128 = 101_741_582_076_661;
+pub fn solve_q2() {
+	let input = fs::read_to_string("./inputs/day22.txt").unwrap();
+	let mut coeffs = (1, 0);
+	let m = 119_315_717_514_047;
+	let k = 101_741_582_076_661;
+	for line in input.trim().split('\n') {
+		coeffs = compose_lgc(coeffs.0, coeffs.1, m, line.trim().to_string());
+	}
 
-	// Now, we are shuffling our deck of m cards many trillion (k) times
-	let a: i128 = coeffs.0;
-	let b: i128 = coeffs.1;
-	let ap = modular_pow(a, k, m);
-	let v = (ap * 2020    ); //+ b * (ap + m - 1) * (modular_pow(a - 1, m - 2, m)) + m) % m;
-
-	let ff = modular_pow(a - 1, m -2 , m);
-	println!("{}", (b * (ap + m - 1)) % m ) * ff;
-	println!("{}", v);
+	let res = invert_f(2020, coeffs.0, coeffs.1, k, m);
+	println!("Q2: {}", res);
+	println!("   -> {}", sum_series(res, coeffs.0, coeffs.1, k, m))
 }
--- 2019/src/main.rs
+++ 2019/src/main.rs
@@ -1,6 +1,6 @@
 mod day_twentytwo;
 
 fn main() {
-	day_twentytwo::solve_q1();
+	day_twentytwo::solve_q1_mathy();
 	day_twentytwo::solve_q2();
 }
