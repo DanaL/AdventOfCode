f23550882971b0ce72acbc5960199323b51be65d
Dana Larose <ywg.dana@gmail.com>
2020-11-29 15:00:50 -0600

    Added a Util class and the Taxi Cab distance function we'll inevitably need

--- 2020/Program.cs
+++ 2020/Program.cs
@@ -1,12 +1,26 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
 
-namespace _2020
+using _2020;
+
+using TextReader _tr = new StreamReader("inputs/day1.txt");
+
+List<int> _vals = new();
+
+while (_tr.Peek() != -1)
 {
-    class Program
-    {
-        static void Main(string[] args)
-        {
-            Console.WriteLine("Hello World!");
-        }
-    }
+    _vals.Add(int.Parse(_tr.ReadLine()));
 }
+
+var _numbers = _vals.ToArray();
+var _subset = _numbers[1..4];
+var _subset2 = _numbers[^4..^0];
+
+Console.WriteLine(_subset.Sum());
+
+foreach (var j in _subset2)
+    Console.WriteLine(j);
+
+Console.WriteLine($"d: {Util.TaxiDistance(4, 16, 7, 8)}");
