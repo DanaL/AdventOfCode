d6fa806759d8260e4a3df9d0ec59ec9dd236a85c
Dana Larose <ywg.dana@gmail.com>
2020-11-30 23:39:09 -0600

    Day 1 Part 1 solved

---
+++ 2020/Day1.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+
+namespace _2020
+{
+    public class Day1
+    {
+        private List<int> _expenses;
+
+        public Day1()
+        {
+            this.fetchInput();
+        }
+
+        private void fetchInput()
+        {
+            using TextReader _tr = new StreamReader("inputs/day1.txt");
+
+            this._expenses = new List<int>();
+            while (_tr.Peek() != -1)
+            {
+                var _line = _tr.ReadLine();
+                _expenses.Add(int.Parse(_line));
+            }
+        }
+
+        public void SolvePart1()
+        {            
+            for (int j = 0; j < _expenses.Count; j++)
+            {
+                int _lookingFor = 2020 - _expenses[j];
+
+                for (int k = j + 1; k < _expenses.Count; k++)
+                {
+                    if (_expenses[k] == _lookingFor)
+                    {
+                        int _res = _expenses[j] * _expenses[k];
+                        Console.WriteLine($"Answer: {_res}");
+                        goto Done;
+                    }
+                }
+            }
+
+        Done:
+            ;
+        }        
+    }
+}
--- 2020/Program.cs
+++ 2020/Program.cs
@@ -1,26 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.IO;
-using System.Linq;
+﻿using _2020;
 
-using _2020;
-
-using TextReader _tr = new StreamReader("inputs/day1.txt");
-
-List<int> _vals = new();
-
-while (_tr.Peek() != -1)
-{
-    _vals.Add(int.Parse(_tr.ReadLine()));
-}
-
-var _numbers = _vals.ToArray();
-var _subset = _numbers[1..4];
-var _subset2 = _numbers[^4..^0];
-
-Console.WriteLine(_subset.Sum());
-
-foreach (var j in _subset2)
-    Console.WriteLine(j);
-
-Console.WriteLine($"d: {Util.TaxiDistance(4, 16, 7, 8)}");
+Day1 _d1 = new Day1();
+_d1.SolvePart1();
