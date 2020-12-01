5fe28de19787222ba9915fd0f14d43939d34259d
Dana Larose <ywg.dana@gmail.com>
2020-11-30 23:58:25 -0600

    Day 2 Part 2 complete, albeit with pure brute force

--- 2020/Day1.cs
+++ 2020/Day1.cs
@@ -36,7 +36,7 @@ namespace _2020
                     if (_expenses[k] == _lookingFor)
                     {
                         int _res = _expenses[j] * _expenses[k];
-                        Console.WriteLine($"Answer: {_res}");
+                        Console.WriteLine($"P1: {_res}");
                         goto Done;
                     }
                 }
@@ -44,6 +44,35 @@ namespace _2020
 
         Done:
             ;
-        }        
+        }
+
+        public void SolvePart2()
+        {
+            // sample/test input
+            //_expenses = new List<int>() { 1721, 979, 366, 299, 675, 1456 };
+
+            // Time to brute force this shit! Puzzle input is 200 lines, so all possible combos of 3
+            // numbers is less than 8 million sets of three I think??
+            List<int[]> _combos = new List<int[]>();
+
+            for (int a = 0; a < _expenses.Count - 2; a++)
+            {
+                for (int b = a + 1; b < _expenses.Count - 1; b++)
+                {
+                    for (int c = b + 1; c < _expenses.Count; c++)
+                    {
+                        int _sum = _expenses[a] + _expenses[b] + _expenses[c];
+                        if (_sum == 2020)
+                        {
+                            Console.WriteLine($"P2: {_expenses[a] * _expenses[b] * _expenses[c]}");
+                            goto Done;
+                        }                        
+                    }
+                }
+            }
+
+        Done:
+            ;
+        }
     }
 }
--- 2020/Program.cs
+++ 2020/Program.cs
@@ -2,3 +2,4 @@
 
 Day1 _d1 = new Day1();
 _d1.SolvePart1();
+_d1.SolvePart2();
