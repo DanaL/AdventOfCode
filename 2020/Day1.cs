c70639dd25c848ec959a240911c001b3cdc92d59
Dana Larose <ywg.dana@gmail.com>
2020-12-01 21:32:03 -0600

    Actually, with tracking #s we've seen in the last we need only a single loop for part 1 and one nested loop for part 2

--- 2020/Day1.cs
+++ 2020/Day1.cs
@@ -17,7 +17,7 @@ namespace _2020
         {
             using TextReader _tr = new StreamReader("inputs/day1.txt");
 
-            this._expenses = new List<int>();
+            this._expenses = new List<int>();         
             while (_tr.Peek() != -1)
             {
                 var _line = _tr.ReadLine();
@@ -27,31 +27,22 @@ namespace _2020
 
         public void SolvePart1()
         {
-            int _res = 0;
-
-            // Keep track of numbers we've seen in the input so on each iteration
-            // we can find out what number will give us 2020 and check the hashset
-            // to see if it is in the list and save ourselves a bit of looping
+            // Keep track of numbers we've seen in the input so far so that we
+            // don't actually have to do two loops like my original naive
+            // implementation. And then I can use a break statement instead of
+            // a goto
             HashSet<int> _seen = new HashSet<int>();
             for (int j = 0; j < _expenses.Count; j++)
             {
                 _seen.Add(_expenses[j]);
                 int _lookingFor = 2020 - _expenses[j];
 
-                for (int k = j + 1; k < _expenses.Count; k++)
+                if (_seen.Contains(_lookingFor))
                 {
-                    _seen.Add(_expenses[k]);
-
-                    if (_seen.Contains(_lookingFor))
-                    {
-                        _res = _expenses[j] * _lookingFor;
-                        goto Done;
-                    }                    
+                    Console.WriteLine($"P1: {_expenses[j] * _lookingFor}");
+                    return;
                 }
             }
-
-        Done:
-            Console.WriteLine($"P1: {_res}");
         }
 
         public void SolvePart2()
@@ -59,34 +50,26 @@ namespace _2020
             // sample/test input
             //_expenses = new List<int>() { 1721, 979, 366, 299, 675, 1456 };
 
-            // Time to brute force this shit! Puzzle input is 200 lines, so all possible combos of 3
-            // numbers is less than 8 million sets of three I think??
-
-            // Use a hashset to hopefully save us some time, similar to part 1
+            // Similar to part one, use a hashset to track values we've seen so
+            // that we only need one nested loop O(n^2) instead of O(n^3)
             HashSet<int> _seen = new HashSet<int>();
             int _res = 0;
-            for (int a = 0; a < _expenses.Count - 2; a++)
+            for (int a = 0; a < _expenses.Count - 1; a++)
             {
                 _seen.Add(_expenses[a]);
-                for (int b = a + 1; b < _expenses.Count - 1; b++)
+                for (int b = a + 1; b < _expenses.Count; b++)
                 {
-                    _seen.Add(_expenses[a]);
-                    for (int c = b + 1; c < _expenses.Count; c++)
-                    {
-                        _seen.Add(_expenses[c]);
+                    _seen.Add(_expenses[b]);
 
-                        int _lookingFor = 2020 - _expenses[a] - _expenses[b];
-                        if (_seen.Contains(_lookingFor))
-                        {
-                            _res = _expenses[a] * _expenses[b] * _lookingFor;
-                            goto Done;
-                        }                                   
+                    int _lookingFor = 2020 - _expenses[a] - _expenses[b];
+                    if (_seen.Contains(_lookingFor))
+                    {
+                        _res = _expenses[a] * _expenses[b] * _lookingFor;
+                        Console.WriteLine($"P2: {_res}");
+                        return;
                     }
                 }
             }
-
-        Done:
-            Console.WriteLine($"P2: {_res}");            
         }
     }
 }
