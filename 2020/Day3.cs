a6eb9a2aae26fd6afc5af0c44c3ac621cff977fd
Dana Larose <ywg.dana@gmail.com>
2020-12-03 09:15:22 -0600

    Day 3 Part 1 finished

--- 2020/Day3.cs
+++ 2020/Day3.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.IO;
 
 namespace _2020
 {
@@ -8,7 +9,31 @@ namespace _2020
 
         public void SolvePt1()
         {
-            Console.WriteLine("Hello, world?");
+            using TextReader _tr = new StreamReader("inputs/day3.txt");
+
+            string _txt = _tr.ReadToEnd();
+            int _width = _txt.IndexOf('\n');
+            int _length = _txt.Length / (_width + 1);
+            _txt = _txt.Replace("\n", "");
+            char[] _map = _txt.ToCharArray();
+
+            // Okay, so the map repeats itself over and over as you move to the right.
+            // So, just loop, incrementing row and column as per the puzzle instructions
+            // (3 right, 1 down in part 1), while using % to simulate this being a 2D array
+            // that is square instead of a narrow rectangle.            
+            int _row = 0;
+            int _col = 0;
+            int _trees = 0;
+            while (_row < _length)
+            {
+                int _pos = _col + (_row * _width);
+                if (_map[_pos] == '#')
+                    ++_trees;
+                ++_row;
+                _col = (_col + 3) % _width;                
+            }
+
+            Console.WriteLine($"P1: {_trees}");            
         }
     }
 }
