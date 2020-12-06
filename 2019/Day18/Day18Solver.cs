21ebe2dd1685cbf52c01a842dd7355bfee239ab2
Dana Larose <ywg.dana@gmail.com>
2020-12-05 22:33:27 -0600

    Began floodfill initial search through maze; created FloodFillNode class to use to track info during the search; tracking doors on a path via a uint bitmask

--- 2019/Day18/Day18Solver.cs
+++ 2019/Day18/Day18Solver.cs
@@ -9,34 +9,107 @@ namespace Day18
     {
         public int Length { get; set; }
         public HashSet<char> KeysNeeded { get; set; }
+        public char Start { get; set; }
+        public char End { get; set; }
 
         public Path() { }
+    }
+
+    public class FloodFillNode
+    {
+        public int Distance { get; set; }
+        public uint Doors { get; set; }
+        public int Row { get; set; }
+        public int Col { get; set; }
+
+        public FloodFillNode() { }
+
+        public FloodFillNode(int d, int r, int c, uint doors)
+        {
+            this.Distance = d;
+            this.Row = r;
+            this.Col = c;
+            this.Doors = doors;
+        }
     }
 
     public class Day18Solver
     {
         public Day18Solver() { }
 
+        private (int, int)[] _dirs = { (-1, 0), (0, -1), (0, 1), (1, 0) };
+
+        private uint letterToNum(char c)
+        {
+            uint exp = (uint)Char.ToLower(c) - (uint)'a';
+            
+            return (uint)Math.Pow(2, exp);
+        }
+
+        private void floodfill(int start_r, int start_c, string[] map)
+        {
+            char start = map[start_r][start_c];
+            HashSet<(int, int)> visited = new HashSet<(int, int)>();
+            Queue<FloodFillNode> nodes = new Queue<FloodFillNode>();
+            nodes.Enqueue(new FloodFillNode(0, start_r, start_c, 0));
+
+            while (nodes.Count() > 0)
+            {
+                var node = nodes.Dequeue();
+                visited.Add((node.Row, node.Col));
+
+                foreach (var d in this._dirs)
+                {
+                    int row = node.Row + d.Item1;
+                    int col = node.Col + d.Item2;
+
+                    if (visited.Contains((row, col)))
+                        continue;
+
+                    char ch = map[row][col];
+                    if (ch == '#')
+                        continue;
+
+                    var new_node = new FloodFillNode(node.Distance + 1, row, col, node.Doors);
+                    
+                    if (ch == '.')
+                        nodes.Enqueue(new_node);
+                    else if (ch >= 'A' && ch <= 'Z')
+                    {
+                        new_node.Doors = node.Doors | this.letterToNum(ch);                        
+                        nodes.Enqueue(new_node);
+                    }
+                    else if (ch >= 'a' && ch <= 'z')
+                    {
+                        Console.WriteLine($"Key {ch} found! D: {new_node.Distance} Doors passed: {new_node.Doors} ");
+                        // What we really need to do is store the path from start to the key, which is a vertex of our
+                        // graph and then keep going
+                    }
+                }
+            }
+        }
+
         public void Solve()
         {
             string file = "/Users/dana/Development/AdventOfCode/2019/inputs/day18_test.txt";
-
             using TextReader tr = new StreamReader(file);
-            List<char[]> grid = new List<char[]>();
-            HashSet<char> keys = new HashSet<char>();
-            while (tr.Peek() != -1)
+
+            // Scan through the map and find all the keys and their locations
+            Dictionary<char, (int Row, int Col)> keys = new Dictionary<char, (int,  int)>();
+            var lines = tr.ReadToEnd().Trim().Split('\n');
+            for (int r = 0; r < lines.Length; r ++)
             {
-                var row = tr.ReadLine().ToCharArray();
-                foreach (char ch in row.Where(ch => ch == '@' || (char.IsLetter(ch) && char.IsLower(ch))))
-                    keys.Add(ch);                   
-                grid.Add(row);                
+                for (int c = 0; c < lines[0].Length; c++)
+                {
+                    if (lines[r][c] == '@' || (char.IsLetter(lines[r][c]) && char.IsLower(lines[r][c])))
+                        keys.Add(lines[r][c], (Row: r, Col: c));
+                }
             }
 
-            //Console.WriteLine(keys.ToString());
-            // So first, lets build a set of how far each key is from each other key, and the
-            // doors required to each it.
+            // Breadthfirst search to calculate the distances from each key to each other key, including
+            // which doors must be passed through
 
-            Console.WriteLine(grid[3][6]);
+            this.floodfill(keys['c'].Row, keys['c'].Col, lines);
         }
     }
 }
