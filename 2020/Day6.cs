8304c505001a6ba1218436c406b52879ca44a1ad
Dana Larose <ywg.dana@gmail.com>
2020-12-05 23:43:43 -0600

    Day parts 1 and 2 complete

--- 2020/Day6.cs
+++ 2020/Day6.cs
@@ -1,15 +1,40 @@
 ﻿using System;
+using System.IO;
+using System.Linq;
+
 namespace _2020
 {
     public class Day6
     {
-        public Day6()
-        {
-        }
+        public Day6() { }
 
         public void Solve()
         {
-            Console.WriteLine("Hello, world?");
+            using TextReader tr = new StreamReader("inputs/day6.txt");
+            var groups = tr.ReadToEnd().Split("\n\n");
+            
+            var sum = 0;
+            foreach (var g in groups.Select(g => g.Replace("\n", "")))
+            {
+                sum += g.ToCharArray().Distinct().Count();
+            }
+            Console.WriteLine($"P1: {sum}");
+
+            var totalYes = 0;
+            foreach (var grp in groups)
+            {
+                var answers = grp.ToCharArray()
+                                .GroupBy(ch => ch)
+                                .ToDictionary(g => g.Key, g => g.ToList().Count);
+
+                var group_size = answers.ContainsKey('\n') ? answers['\n'] + 1 : 1;
+                foreach (var k in answers.Keys)
+                {
+                    if (answers[k] == group_size)
+                        totalYes += 1;
+                }
+            }
+            Console.WriteLine($"P2: {totalYes}");
         }
     }
 }
