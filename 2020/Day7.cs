055d85ad843f656cbd9d491534ee55c23625dc8a
Dana Larose <ywg.dana@gmail.com>
2020-12-07 07:30:46 -0600

    Day 7 -- parsing the file complete

--- 2020/Day7.cs
+++ 2020/Day7.cs
@@ -1,19 +1,52 @@
 ﻿using System;
+using System.Collections.Generic;
 using System.IO;
+using System.Linq;
 
 namespace _2020
 {
+    class Bag
+    {
+        public string Name { get; set; }
+        public Dictionary<string, int> Contains { get; set; }
+
+        public Bag()
+        {
+            this.Contains = new Dictionary<string, int>();
+        }
+    }
+
     public class Day7
     {
         public Day7() { }
 
+        private Bag parse(string line)
+        {
+            var pieces = line.Split(" ");
+
+            Bag bag = new Bag() { Name = $"{pieces[0]} {pieces[1]}" };
+
+            if (pieces.Length > 7)
+            {
+                for (int j = 4; j < pieces.Length; j += 4)
+                {
+                    var count = int.Parse(pieces[j]);
+                    bag.Contains.Add($"{pieces[j + 1]} {pieces[j + 2]}", count);
+                }
+            }
+
+            return bag;
+        }
+
         public void Solve()
         {
-            using TextReader tr = new StreamReader("inputs/day3.txt");
+            using TextReader tr = new StreamReader("inputs/day7.txt");
 
             var txt = tr.ReadToEnd().Trim().Split("\n");
 
-            Console.WriteLine("Hello, world?");
+            var bags = txt.Select(b => parse(b));
+            Console.WriteLine($"{bags.Count()}");
+            
         }
     }
 }
