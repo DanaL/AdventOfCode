588db508a95ffa9adb4ba669cbc942dbd7b943c3
Dana Larose <ywg.dana@gmail.com>
2020-12-12 13:47:35 -0600

    Rewrote Part 1 to look like Part 2. Could allllmost merge them

--- 2020/Day12.cs
+++ 2020/Day12.cs
@@ -6,11 +6,6 @@ namespace _2020
 {
     public class Day12
     {
-        private static readonly (int, int) NORTH = (-1, 0);
-        private static readonly (int, int) SOUTH = (1, 0);
-        private static readonly (int, int) EAST = (0, 1);
-        private static readonly (int, int) WEST = (0, -1);
-
         private (char, int)[] _instructions;
 
         public Day12()
@@ -20,41 +15,13 @@ namespace _2020
             _instructions = lines.Select(d => (d[..1][0], int.Parse(d[1..]))).ToArray();
         }
 
-        private void part1()
-        {            
-            (int, int, (int, int)) pos = (0, 0, EAST);
-
-            foreach (var instr in _instructions)
-            {
-                if (instr.Item1 == 'L' || instr.Item1 == 'R')
-                {
-                    pos.Item3 = rotate(pos.Item3, instr.Item1, instr.Item2);
-                }
-                else
-                {
-                    var dir = instr.Item1 switch
-                    {
-                        'N' => NORTH,
-                        'S' => SOUTH,
-                        'E' => EAST,
-                        'W' => WEST,
-                        _ => pos.Item3
-                    };
-
-                    pos = (pos.Item1 + dir.Item1 * instr.Item2, pos.Item2 + dir.Item2 * instr.Item2, pos.Item3);
-                }
-            }
-
-            Console.WriteLine($"P1: {Util.TaxiDistance(0, 0, pos.Item1, pos.Item2)}");
-        }
-        
         private (int, int) rotate((int, int) wp, char dir, int degree)
         {
             // Okay, so clockwise rotations are just the inversion of counterclockwise rotations (ie.,
             // R90 is the same as L270) so we'll just worry about the conterclockwise rotations
             if (dir == 'R')
                 degree = degree == 180 ? degree : (degree + 180) % 360;
-            
+
             // Rotating a vector is a matrix multiplcation but they boil down to swapping
             // x and y and sometimes inverting the signs. Ie., 90' rotation counterclockwise
             // is multiplying (x, y) by [-1 1] to give you (-y, x)
@@ -68,6 +35,41 @@ namespace _2020
             return wp;
         }
 
+        private void part1()
+        {
+            (int, int) ship = (0, 0);
+            (int, int) dir = (0, 1); // East
+            
+            foreach (var instr in _instructions)
+            {
+                switch (instr.Item1)
+                {
+                    case 'N':
+                        ship.Item1 -= instr.Item2;
+                        break;
+                    case 'S':
+                        ship.Item1 += instr.Item2;
+                        break;
+                    case 'E':
+                        ship.Item2 += instr.Item2;
+                        break;
+                    case 'W':
+                        ship.Item2 -= instr.Item2;
+                        break;
+                    case 'F':
+                        ship.Item1 += dir.Item1 * instr.Item2;
+                        ship.Item2 += dir.Item2 * instr.Item2;
+                        break;
+                    case 'R':
+                    case 'L':
+                        dir = rotate(dir, instr.Item1, instr.Item2);
+                        break;
+                }
+            }
+
+            Console.WriteLine($"P1: {Util.TaxiDistance(0, 0, ship.Item1, ship.Item2)}");
+        }
+        
         private void part2()
         {
             (int, int) ship = (0, 0);
