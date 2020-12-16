331ae212cb749c9e792f5a8a2d3d988ab6f941bd
Dana Larose <ywg.dana@gmail.com>
2020-12-16 13:42:31 -0600

    Day 16 part 2 finished but my code is kind of clunky and gross

--- 2020/Day16.cs
+++ 2020/Day16.cs
@@ -8,15 +8,15 @@ namespace _2020
 {
     public class Day16
     {
-        HashSet<int> _myTicket;
-        List<HashSet<int>> _nearbyTickets;
+        List<int> _myTicket;
+        List<List<int>> _nearbyTickets;
         Dictionary<string, List<int>> _fields;
 
         public Day16()
         {
             bool otherTickets = false;
             string pattern = @"(?<cat>[a-z ]+): (?<a>\d+)-(?<b>\d+) or (?<c>\d+)-(?<d>\d+)";
-            _nearbyTickets = new List<HashSet<int>>();
+            _nearbyTickets = new List<List<int>>();
             _fields = new Dictionary<string, List<int>>();
             using TextReader tr = new StreamReader("inputs/day16.txt");
 
@@ -26,11 +26,11 @@ namespace _2020
                 if (line == "")
                     continue;
                 else if (line == "your ticket:")
-                    _myTicket = new HashSet<int>(tr.ReadLine().Split(',').Select(n => int.Parse(n)));
+                    _myTicket = new List<int>(tr.ReadLine().Split(',').Select(n => int.Parse(n)));
                 else if (line == "nearby tickets:")
                     otherTickets = true;
                 else if (otherTickets)
-                    _nearbyTickets.Add(new HashSet<int>(line.Split(',').Select(n => int.Parse(n))));
+                    _nearbyTickets.Add(new List<int>(line.Split(',').Select(n => int.Parse(n))));
                 else
                 {
                     var matches = Regex.Match(line, pattern);
@@ -57,21 +57,87 @@ namespace _2020
             return false;
         }
 
-        private List<int> invalidValues(HashSet<int> ticket)
+        private List<int> invalidValues(List<int> ticket)
         {
             return ticket.Where(v => !checkAllFields(v)).ToList();
         }
 
-        private bool validTicket(HashSet<int> ticket)
+        private bool validTicket(List<int> ticket)
         {
-            var valid = ticket.Where(v => checkAllFields(v));
+            return ticket.Where(v => checkAllFields(v)).Count() == ticket.Count;
+        }
 
-            return valid.Count() == ticket.Count;
+        private List<string> findPossibleFields(int val)
+        {
+            return _fields.Keys.Where(k => checkField(val, _fields[k])).ToList();
+        }
+
+        private void removeOption(Dictionary<int, HashSet<string>> possibilities, int keeperIndex, string toRemove)
+        {
+            foreach (int k in possibilities.Keys.Where(n => n != keeperIndex))
+                possibilities[k].Remove(toRemove);            
+        }
+
+        private void filterPossibilities(Dictionary<int, HashSet<string>> possibilities)
+        {
+            var total = possibilities.Values.Select(v => v.Count).Sum();
+
+            while (true)
+            {
+                foreach (int k in possibilities.Keys)
+                {
+                    if (possibilities[k].Count == 1)
+                        removeOption(possibilities, k, possibilities[k].ElementAt(0));
+                }
+
+                var newTotal = possibilities.Values.Select(v => v.Count).Sum();
+                if (newTotal == total)
+                {
+                    Console.WriteLine("We've filtered as many possibilities as we can!");
+                    break;
+                }
+                total = newTotal;
+            }
         }
 
         public void Solve()
-        {         
+        {
+            // For Part One, we just need to sum up all of the invalid fields found across the
+            // various tickets
             Console.WriteLine($"P1: {_nearbyTickets.SelectMany(t => invalidValues(t)).Sum()}");
+
+            // For Part Two, we want to toss out any of those invalid tickets and then use the
+            // nearby tickets to determine which field is which. (Order will be consistent across
+            // tickets
+            var validTickets = _nearbyTickets.Where(t => validTicket(t)).ToList();
+
+            // First, find possibilities looking at tickets and "what can possibly fit" based on the
+            // value in that column
+            Dictionary<int, HashSet<string>> fieldPossibilities = new Dictionary<int, HashSet<string>>();
+            for (int j = 0; j < _myTicket.Count; j++)
+                fieldPossibilities.Add(j, new HashSet<string>(_fields.Keys));
+
+            foreach (var ticket in validTickets)
+            {
+                for (int j = 0; j < _myTicket.Count; j++)
+                {
+                    fieldPossibilities[j].IntersectWith(findPossibleFields(ticket[j]));
+                }
+            }
+
+            filterPossibilities(fieldPossibilities);
+
+            // Luckily filtering down the possibilities left us with a unique set so we don't have
+            // to try remaining combos to find which ones result in invalid tickets, which I figured
+            // we were going to have to.
+            ulong product = 1;
+            foreach (var k in fieldPossibilities.Keys)
+            {
+                if (fieldPossibilities[k].ElementAt(0).StartsWith("departure"))
+                    product *= (ulong)_myTicket[k];
+            }
+
+            Console.WriteLine($"P2: {product}");
         }
     }
 }
