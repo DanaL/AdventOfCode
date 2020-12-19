d487d2aa01199f9bdc231d91f4ac294c41d54560
Dana Larose <ywg.dana@gmail.com>
2020-12-19 16:40:20 -0600

    First stab at Day 19 is to generate all possible strings from the rules and then test the input against them. This is too slow and there's a bug in the code somewhere

--- 2020/Day19.cs
+++ 2020/Day19.cs
@@ -9,6 +9,7 @@ namespace _2020
     {
         List<string> _messages;
         Dictionary<string, string[]> _rules;
+        Dictionary<string, string[]> _flattenedRules;
 
         public Day19() { }
 
@@ -41,10 +42,67 @@ namespace _2020
             }
         }
 
+        private bool complete(string msg)
+        {
+            return msg.ToCharArray().Where(c => c != 'a' && c != 'b' && c != ' ').Count() == 0;
+        }
+
+        private List<string> produceString(string production)
+        {
+            List<string> newPhrases = new List<string>();
+            string[] pieces = production.Split(' ');
+            for (int j = 0; j < pieces.Length; j++)
+            {
+                if (_rules.ContainsKey(pieces[j]))
+                {
+                    foreach (var rule in _rules[pieces[j]])
+                    {
+                        pieces[j] = rule;
+                        newPhrases.Add(string.Join(' ', pieces).Trim());
+                    }
+                    break; // Only doing one bit at a time to avoid creating redundant phrases
+                }
+            }
+
+            return newPhrases;
+        }
+
         public void Solve()
         {
             parseInput();
+            _flattenedRules = new Dictionary<string, string[]>();
 
+            List<string> productions = new List<string>() { _rules["0"][0] } ;
+            bool incomplete = true;
+
+            while (incomplete)
+            {
+                incomplete = false;
+                List<string> next = new List<string>();
+                foreach (var pr in productions)
+                {
+                    foreach (var result in produceString(pr))
+                    {
+                        next.Add(result);
+                        if (!complete(result))
+                            incomplete = true;
+                    }
+                }
+                productions = next;                
+            }
+
+            HashSet<string> possibleMessages = new HashSet<string>();
+            foreach (string msg in productions)
+                possibleMessages.Add(msg.Replace(" ", ""));
+
+            foreach (string m in _messages)
+            {
+                if (possibleMessages.Contains(m))
+                    Console.WriteLine(m);
+            }
+                
+            
+            Console.WriteLine($"P1: {_messages.Where(m => possibleMessages.Contains(m)).Count()}");
         }
     }
 }
