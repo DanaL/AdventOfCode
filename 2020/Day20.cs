1f058e405431268899ff21e581613522095b252a
Dana Larose <ywg.dana@gmail.com>
2020-12-20 11:03:59 -0600

    Day 20 Part 2 -- image rotation function done

--- 2020/Day20.cs
+++ 2020/Day20.cs
@@ -14,6 +14,64 @@ namespace _2020
         public Tile() { }
     }
 
+    class Piece
+    {
+        public static short TOP = 1;
+        public static short BOTTOM = 2;
+        public static short LEFT = 3;
+        public static short RIGHT = 4;
+
+        public string Pixels { get; set; }
+        public Dictionary<short, string> Edges { get; set; }
+
+        // txt is the image out of the file with line breaks
+        public Piece(string txt)
+        {
+            Pixels = txt.Replace("\n", "");
+            Edges = new Dictionary<short, string>();
+            Edges.Add(TOP, Pixels.Substring(0, 10));
+            Edges.Add(BOTTOM, Pixels.Substring(Pixels.Length - 10, 10));
+            string left = "";
+            string right = "";
+            for (int j = 0; j < Pixels.Length; j += 10)
+            {
+                left += Pixels[j];
+                right += Pixels[j + 9];
+            }
+            Edges.Add(LEFT, left);
+            Edges.Add(RIGHT, right);
+        }
+
+        // Magic numbers 'cause by spec all our tiles are 10x10
+        public Piece Rotate()
+        {
+            char[] pixels = new char[100];
+            int[] indices = new int[100];
+
+            for (int i = 0; i < 100; i++)
+            {
+                if (i < 10)
+                    indices[i] = i * 10 + 9;
+                else
+                    indices[i] = indices[i - 10] - 1;
+            }
+
+            foreach (var i in indices)
+                pixels[indices[i]] = Pixels[i];
+                
+            Piece rotated = new Piece(string.Concat(pixels));
+            return rotated;
+        }
+
+        public void Dump()
+        {
+            for (int j = 0; j < Pixels.Length; j += 10)
+            {
+                Console.WriteLine(Pixels.Substring(j, 10));
+            }
+        }
+    }
+
     public class Day20 : IDay
     {
         private Dictionary<string, int> _edgesSeen = new Dictionary<string, int>();
@@ -89,8 +147,23 @@ namespace _2020
             parseInput();
 
             var uniqueEdges = _edgesSeen.Keys.Where(e => _edgesSeen[e] == 1).ToArray();
-            var corners = _tiles.Where(t => isCorner(t, uniqueEdges)).Select(t => long.Parse(t.Num)).ToArray();
-            Console.WriteLine($"P1: {corners.Aggregate((total, next) => total * next)}");
+            var corners = _tiles.Where(t => isCorner(t, uniqueEdges)).ToArray();
+            Console.WriteLine($"P1: {corners.Select(t => long.Parse(t.Num)).ToArray().Aggregate((total, next) => total * next)}");
+
+            Piece p = new Piece(corners[0].Text.Substring(11));
+            p.Dump();
+            Console.WriteLine("\nT: " + p.Edges[Piece.TOP]);
+            Console.WriteLine("B: " + p.Edges[Piece.BOTTOM]);
+            Console.WriteLine("L: " + p.Edges[Piece.LEFT]);
+            Console.WriteLine("R: " + p.Edges[Piece.RIGHT]);
+
+            Console.WriteLine("\nFirst rotation:");
+            Piece r1 = p.Rotate();
+            r1.Dump();
+            Console.WriteLine("\nT: " + r1.Edges[Piece.TOP]);
+            Console.WriteLine("B: " + r1.Edges[Piece.BOTTOM]);
+            Console.WriteLine("L: " + r1.Edges[Piece.LEFT]);
+            Console.WriteLine("R: " + r1.Edges[Piece.RIGHT]);
         }
     }
 }
