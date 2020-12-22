57c3ec7c293f92e37d942adb963a3e8f58c5dbda
Dana Larose <ywg.dana@gmail.com>
2020-12-22 08:35:44 -0600

    Day 22 Part 2 done

--- 2020/Day22.cs
+++ 2020/Day22.cs
@@ -9,33 +9,77 @@ namespace _2020
     {
         public Day22() { }
 
-        public void Solve()
-        {
-            var cards = File.ReadAllText("inputs/day22.txt").Split("\n\n");
-            var deck1 = new Queue<int>(cards[0].Split('\n').Skip(1).Select(i => int.Parse(i)).ToList());
-            var deck2 = new Queue<int>(cards[1].Split('\n').Skip(1).Select(i => int.Parse(i)).ToList());
+        private (Queue<int>, Queue<int>) play(Queue<int> deck1, Queue<int> deck2, bool recursiveRules)
+        {            
+            var previousRoundsP1 = new HashSet<string>();
+            var previousRoundsP2 = new HashSet<string>();
 
             while (deck1.Count > 0 && deck2.Count > 0)
             {
+                // Prevent an endless game
+                if (recursiveRules)
+                {
+                    var s1 = string.Join('+', deck1.ToArray().Select(c => c.ToString()));
+                    var s2 = string.Join('+', deck1.ToArray().Select(c => c.ToString()));
+                    if (previousRoundsP1.Contains(s1) || previousRoundsP2.Contains(s2))
+                        return (deck1, new Queue<int>()); // Infinite loop so Player 1 automatically wins
+                    else
+                    {
+                        previousRoundsP1.Add(s1);
+                        previousRoundsP2.Add(s2);
+                    }
+                }
+                
                 var card1 = deck1.Dequeue();
                 var card2 = deck2.Dequeue();
 
-                if (card1 > card2)
+                if (recursiveRules && deck1.Count >= card1 && deck2.Count >= card2)
                 {
-                    deck1.Enqueue(card1);
-                    deck1.Enqueue(card2);
+                    var sub1 = new Queue<int>(deck1.ToArray().Take(card1));
+                    var sub2 = new Queue<int>(deck2.ToArray().Take(card2));
+                    var result = play(sub1, sub2, recursiveRules);
+                    if (result.Item1.Count > result.Item2.Count)
+                    {
+                        deck1.Enqueue(card1);
+                        deck1.Enqueue(card2);
+                    }
+                    else
+                    {
+                        deck2.Enqueue(card2);
+                        deck2.Enqueue(card1);
+                    }
                 }
                 else
                 {
-                    deck2.Enqueue(card2);
-                    deck2.Enqueue(card1);
+                    if (card1 > card2)
+                    {
+                        deck1.Enqueue(card1);
+                        deck1.Enqueue(card2);
+                    }
+                    else
+                    {
+                        deck2.Enqueue(card2);
+                        deck2.Enqueue(card1);
+                    }
                 }
             }
-            
-            var winner = deck1.Count > 0 ? deck1.ToArray() : deck2.ToArray();
+
+            return (deck1, deck2);
+        }
+
+        public void Solve()
+        {
+            var cards = File.ReadAllText("inputs/day22.txt").Split("\n\n");
+            var deck1 = cards[0].Split('\n').Skip(1).Select(i => int.Parse(i)).ToList();
+            var deck2 = cards[1].Split('\n').Skip(1).Select(i => int.Parse(i)).ToList();
+
+            var result = play(new Queue<int>(deck1), new Queue<int>(deck2), false);
+            var winner = result.Item1.Count > 0 ? result.Item1.ToArray() : result.Item2.ToArray();
             Console.WriteLine($"P1: {winner.Zip(Enumerable.Range(1, winner.Length).Reverse(), (a, b) => a * b).Sum()}");
 
-            
+            result = play(new Queue<int>(deck1), new Queue<int>(deck2), true);
+            winner = result.Item1.Count > 0 ? result.Item1.ToArray() : result.Item2.ToArray();
+            Console.WriteLine($"P2: {winner.Zip(Enumerable.Range(1, winner.Length).Reverse(), (a, b) => a * b).Sum()}");
         }
     }
 }
