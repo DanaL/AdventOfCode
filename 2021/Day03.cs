b78395f56ac93a3cda9271010028b3c5ced0d621
Dana Larose <dana@lrsd.net>
2021-12-03 14:33:35 -0600

    Day 3 Part 2 complete

--- 2021/Day03.cs
+++ 2021/Day03.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 using System.IO;
 using System.Linq;
 
@@ -8,9 +9,13 @@ namespace _2021
     {
         public Day03() { }
 
-        public void Solve()
+        private List<string> fetchProblemInput()
         {
-            var lines = File.ReadAllLines("inputs/day03.txt");
+            return File.ReadAllLines("inputs/day03.txt").ToList();
+        }
+
+        private int[] countSignals(List<string> lines)
+        {
             int[] signals = new int[lines[0].Length];
 
             foreach (string line in lines)
@@ -19,10 +24,59 @@ namespace _2021
                     signals[j] += line[j] == '1' ? 1 : 0;
             }
 
-            var gamma = new string(signals.Select(b => b > lines.Length / 2 ? '1' : '0').ToArray());
-            var epsilon = new string(signals.Select(b => b < lines.Length / 2 ? '1' : '0').ToArray());
+            return signals;
+        }
+
+        private void part1()
+        {
+            var lines = fetchProblemInput();
+            int[] signals = countSignals(lines);
+            var gamma = new string(signals.Select(b => b > lines.Count / 2 ? '1' : '0').ToArray());
+            var epsilon = new string(signals.Select(b => b < lines.Count / 2 ? '1' : '0').ToArray());
 
             Console.WriteLine($"P1: {Convert.ToInt32(gamma, 2) * Convert.ToInt32(epsilon, 2)}");
+        }
+
+        private string filterO2(List<string> lines, int position)
+        {
+            if (lines.Count == 1)
+                return lines[0];
+
+            int ones = lines.Select(l => l[position]).Where(i => i == '1').Count();
+            int zeroes = lines.Count - ones;
+            char seeking = ones >= zeroes ? '1' : '0';
+            var keep = lines.Where(l => l[position] == seeking).ToList();
+
+            return filterO2(keep, position + 1);
+        }
+
+        private string filterCO2(List<string> lines, int position)
+        {
+            if (lines.Count == 1)
+                return lines[0];
+
+            int ones = lines.Select(l => l[position]).Where(i => i == '1').Count();
+            int zeroes = lines.Count - ones;
+            char seeking = ones >= zeroes ? '0' : '1';
+            var keep = lines.Where(l => l[position] == seeking).ToList();
+
+            return filterCO2(keep, position + 1);
+        }
+
+        private void part2()
+        {
+            var lines = fetchProblemInput();
+
+            var o2 = filterO2(lines, 0);
+            var co2 = filterCO2(lines, 0);
+
+            Console.WriteLine($"P1: {Convert.ToInt32(o2, 2) * Convert.ToInt32(co2, 2)}");
+        }
+
+        public void Solve()
+        {
+            part1();
+            part2();
         }
     }
 }
