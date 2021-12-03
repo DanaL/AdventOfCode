e2a875f4e87880f02288f315ecd3175fe0ad54da
Dana Larose <ywg.dana@gmail.com>
2021-12-03 06:56:20 -0600

    Day 3 Part 1 complete

--- 2021/Day03.cs
+++ 2021/Day03.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.IO;
+using System.Linq;
 
 namespace _2021
 {
@@ -8,7 +10,19 @@ namespace _2021
 
         public void Solve()
         {
+            var lines = File.ReadAllLines("inputs/day03.txt");
+            int[] signals = new int[lines[0].Length];
 
+            foreach (string line in lines)
+            {
+                for (int j = 0; j < line.Length; j++)
+                    signals[j] += line[j] == '1' ? 1 : 0;
+            }
+
+            var gamma = new string(signals.Select(b => b > lines.Length / 2 ? '1' : '0').ToArray());
+            var epsilon = new string(signals.Select(b => b < lines.Length / 2 ? '1' : '0').ToArray());
+
+            Console.WriteLine($"P1: {Convert.ToInt32(gamma, 2) * Convert.ToInt32(epsilon, 2)}");
         }
     }
 }
