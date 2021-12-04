f2472ffb2c7855d0c3d0fb6e096eb5a3ef0f3bd7
Dana Larose <ywg.dana@gmail.com>
2021-12-04 00:55:28 -0600

    Day 4 Part 1 complete

--- 2021/Day04.cs
+++ 2021/Day04.cs
@@ -9,23 +9,72 @@ namespace _2021
     {
         public Day04() { }
 
+        bool bingo(int[] board, int width)
+        {
+            // check rows
+            foreach (var row in board.Chunk(width))
+            {
+                if (row.Sum() == 5)
+                    return true;                
+            }
+
+            // check cols
+            for (int r = 0; r < width; r++)
+            {
+                int sum = 0;
+                for (int c = 0; c < width; c++)               
+                    sum += board[r * width + c];
+                if (sum == 5)
+                    return true;
+            }
+
+            return false;
+        }
+
+        int calcPt1Score(int[] board, int[] marks, int n)
+        {
+            int sum = 0;
+            for (int j = 0; j < board.Length; j++)
+            {
+                if (marks[j] == 0)
+                    sum += board[j];
+            }
+
+            return sum * n;
+        }
+
         public void Solve()
         {
+            int width = 5;
             var lines = File.ReadAllLines("inputs/day04.txt");
             var numbers = lines[0].Split(',').Select(n => int.Parse(n));
 
+            var marks = new List<int[]>();
             var boards = new List<int[]>();
-            foreach (var board in lines.Skip(2).Chunk(6))
+            foreach (var board in lines.Skip(2).Chunk(width + 1))
             {
-                var b = string.Join(" ", board.Take(5)).Trim().Replace("  ", " ")
+                var b = string.Join(" ", board.Take(width)).Trim().Replace("  ", " ")
                               .Split(' ')
                               .Select(n => int.Parse(n)).ToArray();
                 boards.Add(b);
+                marks.Add(new int[width * width]);
             }
 
-            foreach (var board in boards)
+            foreach (int n in numbers)
             {
-                Console.WriteLine($"{board[0]}, {board[1]}, {board[2]}");
+                for (int b = 0; b < boards.Count; b++)
+                {
+                    for (int x = 0; x < boards[b].Length; x++)
+                    {
+                        if (boards[b][x] == n)
+                            marks[b][x] = 1;
+                        if (bingo(marks[b], width))
+                        {
+                            Console.WriteLine($"P1: {calcPt1Score(boards[b], marks[b], n)}");
+                            return;
+                        }
+                    }
+                }
             }
         }
     }
