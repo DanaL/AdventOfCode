ba14eb2b2df9fbb750aa11ae162496253e48bdc5
Dana Larose <ywg.dana@gmail.com>
2021-12-09 07:16:16 -0600

    Day 9 Part 2 complete

--- 2021/Day09.cs
+++ 2021/Day09.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 using System.IO;
 using System.Linq;
 
@@ -10,20 +11,18 @@ namespace _2021
         int _width;
         string _data;
 
-        public Day09() { }
-
         void FetchInput()
         {
-            //string data = "2199943210\n3987894921\n2856789892\n8767896789989\n9965678";
+            //_data = "2199943210\n3987894921\n9856789892\n8767896789989\n9965678";
             _data = File.ReadAllText("inputs/day09.txt").Trim();
             _height = _data.Where(c => c == '\n').Count() + 1;
             _data = _data.Replace("\n", "");
             _width = _data.Length / _height;
         }
 
-        bool IsLowPoint(string map, int row, int col)
+        bool IsLowPoint(int row, int col)
         {
-            char v = map[row * _width + col];
+            char v = _data[row * _width + col];
 
             foreach ((int R, int C) d in Util.Neighbours())
             {
@@ -31,30 +30,71 @@ namespace _2021
                 int nc = col + d.C;
                 int x = nr * _width + nc;
 
-                if (x >= 0 && x < map.Length && nc >= 0 && nc < _width && map[x] <= v)
+                if (x >= 0 && x < _data.Length && nc >= 0 && nc < _width && _data[x] <= v)
                     return false;
             }
 
             return true;
         }
 
-        public void Solve()
+        int BasinSize(int startR, int startC)
         {
-            FetchInput();
-            
-            int sum = 0;
-            for (int r = 0; r < _height; r++)
-            {
-                for (int c = 0; c < _width; c++)
+            // It's doesn't really feel like Advent of Code until I've written a floodfill routine!
+            var visited = new HashSet<(int, int)>();
+            var toVisit = new Queue<(int, int)>();
+            toVisit.Enqueue((startR, startC));
+
+            int total = 0;
+            while (toVisit.Count > 0)
+            {               
+                var curr = toVisit.Dequeue();
+                if (visited.Contains(curr))
+                    continue;
+                visited.Add(curr);
+
+                ++total;
+
+                foreach (var d in Util.Neighbours())
                 {
-                    if (IsLowPoint(_data, r, c))
-                        sum += _data[r * _width + c] - '0' + 1;                    
+                    var next = (curr.Item1 + d.Item1, curr.Item2 + d.Item2);                    
+                    if (visited.Contains(next))
+                        continue;
+
+                    var x = next.Item1 * _width + next.Item2;
+                    if (!(x >= 0 && x < _data.Length && next.Item2 >= 0 && next.Item2 < _width))
+                        continue;
+                    if (_data[x] != '9')
+                        toVisit.Enqueue(next);
                 }
             }
 
-            Console.WriteLine($"P1: {sum}");
+            return total;
+        }
 
-            // Part 2 will be classic flood fill
+        public void Solve()
+        {
+            FetchInput();
+
+            int totalPt1 = 0;
+            var basins = new List<int>();
+            for (int x = 0; x < _data.Length; x++)
+            {
+                int r = x / _height;
+                int c = x - r * _width;
+                if (IsLowPoint(r, c))
+                {
+                    totalPt1 += _data[r * _width + c] - '0' + 1;
+                    basins.Add(BasinSize(r, c));
+                }
+            }
+
+            basins.Sort();
+            long totalPt2 = 1;
+            foreach (int x in basins.TakeLast(3))
+                totalPt2 *= x;
+
+            Console.WriteLine($"P1: {totalPt1}");
+            Console.WriteLine($"P2: {totalPt2}");
         }
     }
 }
