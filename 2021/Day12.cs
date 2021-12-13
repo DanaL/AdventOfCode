9a5dc07d62a95d0a5ed7a1ad75f466548ce0a02d
Dana Larose <ywg.dana@gmail.com>
2021-12-12 21:58:25 -0600

    Day 12 - Got rid of the of slow, clunky implementation for part 1 since using part 2's method required only a trivial change.

--- 2021/Day12.cs
+++ 2021/Day12.cs
@@ -5,66 +5,7 @@ using System.Linq;
 using AoC;
 
 namespace _2021
-{
-    // For part 1, I implemented generating the paths with the first, simple idea that occurred to
-    // and didn't worry too much about computational efficiency. So, for example, I'm storing the paths
-    // I'm exploring as growing strings and when checking if a small cave has been previously visited, I
-    // just linear search through the string.
-    class Part1Solver
-    {
-        static bool SmallCaveInPath(string path, string cave)
-        {
-            if (cave.Where(c => char.IsUpper(c)).Count() > 0)
-                return false;
-            return path.IndexOf($"-{cave}-") > -1;
-        }
-
-        public static void SolvePart1(List<(string, string)> input)
-        {
-            var createdPaths = new HashSet<string>();
-            var nodes = new Dictionary<string, HashSet<string>>();
-            var beingBuilt = new Queue<(string, string)>();
-            beingBuilt.Enqueue(("start", "start"));
-            foreach (var pair in input)
-            {
-                if (!nodes.ContainsKey(pair.Item1))
-                    nodes.Add(pair.Item1, new HashSet<string>());
-                nodes[pair.Item1].Add(pair.Item2);
-
-                // Invert the paths because they should be directional
-                if (pair.Item1 == "start" || pair.Item2 == "end")
-                    continue;
-
-                if (!nodes.ContainsKey(pair.Item2))
-                    nodes.Add(pair.Item2, new HashSet<string>());
-                nodes[pair.Item2].Add(pair.Item1);
-            }
-
-            int pathCount = 0;
-            while (beingBuilt.Count > 0)
-            {
-                var pathInfo = beingBuilt.Dequeue();
-                var node = pathInfo.Item1;
-                var path = pathInfo.Item2;
-                foreach (var nextNode in nodes[node])
-                {
-                    var nextPath = $"{path}-{nextNode}";
-                    if (!createdPaths.Contains(nextPath))
-                    {
-                        createdPaths.Add(nextPath);
-
-                        if (nextNode != "end" && !SmallCaveInPath(path, nextNode))
-                            beingBuilt.Enqueue((nextNode, nextPath));
-                        else if (nextNode == "end")
-                            ++pathCount;
-                    }
-                }
-            }
-
-            Console.WriteLine($"P1: {pathCount}");
-        }
-    }
-
+{    
     // For part 2, since the # of paths through the graph are much, much bigger and
     // I need to allow one loop through a small cave, I got slightly more sophisticated
     // and use a Route struct to track info about the path we are following, and use
@@ -76,14 +17,16 @@ namespace _2021
         public bool Loop;
     }
 
-    class Part2Solver
+    class Day12Solver
     {
         readonly Dictionary<string, HashSet<string>> _vertexes;
         readonly Dictionary<string, uint> _smallCaveBitmasks;        
         int _pathCount;
+        bool _allowOneLoop;
 
-        public Part2Solver(List<(string, string)> input)
+        public Day12Solver(List<(string, string)> input, bool allowOneLoop)
         {
+            _allowOneLoop = allowOneLoop;
             _vertexes = new Dictionary<string, HashSet<string>>();
             
             // Build dictionary of paths through graph
@@ -127,16 +70,16 @@ namespace _2021
                     ++_pathCount;
                 else if (!_smallCaveBitmasks.ContainsKey(nextNode))
                     FollowPath(route, nextNode);
-                else if (!route.Loop || (_smallCaveBitmasks[nextNode] & route.Visited) == 0)                
+                else if ((_allowOneLoop && !route.Loop) || (_smallCaveBitmasks[nextNode] & route.Visited) == 0)                
                     FollowPath(route, nextNode);                
             }
         }
 
-        public void Solve()
+        public int Solve()
         {
             _pathCount = 0;
             FollowPath(new Route() { Visited = 0, Loop = false }, "start");
-            Console.WriteLine($"P2: {_pathCount}");
+            return _pathCount;
         }
     }
 
@@ -170,9 +113,10 @@ nx-end";
 
         public void Solve()
         {
-            Part1Solver.SolvePart1(FetchInput());
-            var part2 = new Part2Solver(FetchInput());
-            part2.Solve();
+            var p1 = new Day12Solver(FetchInput(), false);
+            Console.WriteLine($"P1: {p1.Solve()}");
+            var p2 = new Day12Solver(FetchInput(), true);
+            Console.WriteLine($"P2: {p2.Solve()}");
         }
     }
 }
