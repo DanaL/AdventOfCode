c06ef7ba36ef235f4d74213109922990fc3a1213
Dana Larose <dana@lrsd.net>
2021-12-14 15:25:57 -0600

    Day 14 Part 2 complete
    And replaced my 'actually build the polymers' solution to pt1 with pt2
    since they're the same problem just more iterations

--- 2021/Day14.cs
+++ 2021/Day14.cs
@@ -2,7 +2,6 @@
 using System.Collections.Generic;
 using System.IO;
 using System.Linq;
-using System.Text;
 using System.Text.RegularExpressions;
 
 using AoC;
@@ -10,60 +9,89 @@ using AoC;
 namespace _2021
 {    
     public class Day14 : IDay
-    {
-        Dictionary<char, int> _counts;
-
-        static (Dictionary<string, char>, string) FetchInput()
+    {        
+        static (Dictionary<string, (string, string, char)>, string) FetchInput()
         {
-            var rules = new Dictionary<string, char>();
-            Regex re = new Regex(@"^(\w+) -> (\w+)$");
-            var lines = File.ReadAllLines("inputs/day14.txt");
+            var rules = new Dictionary<string, (string, string, char)>();
+            Regex re = new Regex(@"^(\w+) -> (\w+)$"); var lines = File.ReadAllLines("inputs/day14.txt");
             foreach (var line in lines.Skip(2))
             {
                 Match m = re.Match(line.Trim());
-                if (m.Success)
-                    rules.Add(m.Groups[1].Value, m.Groups[2].Value[0]);
+                if (m.Success) 
+                {
+                    string lt = m.Groups[1].Value;
+                    string rt = m.Groups[2].Value;
+                    rules.Add(lt, ($"{lt[0]}{rt}", $"{rt}{lt[1]}", rt[0]));
+                }
             }
 
             return (rules, lines[0]);
         }
 
-        string Step(string chain, Dictionary<string, char> rules)
-        {
-            StringBuilder sb = new StringBuilder();
-            for (int j = 0; j < chain.Length - 1; j++)
-            {
-                sb.Append(chain[j]);
-                string pair = $"{chain[j]}{chain[j + 1]}";
-                if (rules.ContainsKey(pair))
-                {
-                    sb.Append(rules[pair]);
-                    ++_counts[rules[pair]];                    
-                }
-            }
-            sb.Append(chain[chain.Length - 1]);
-
-            return sb.ToString();
-        }
-
-        void Part1()
+        ulong ProcessPolymer(int generations)
         {
             var result = FetchInput();
-            var rules = result.Item1;            
-            var chain = result.Item2;
+            var rules = result.Item1;
+            var template = result.Item2;
 
-            _counts = rules.Values.Distinct().Select(c => (c, 0)).ToDictionary(c => c.Item1, c => c.Item2);
-            foreach (char ch in chain)
-                ++_counts[ch];
+            // Okay, set up the dictionary that tracks the number of times each pair
+            // appears in the current polymer
+            var pairCounts = new Dictionary<string, ulong>();
+            foreach (var k in rules.Keys)
+            {
+                if (!pairCounts.ContainsKey(k))
+                    pairCounts[k] = 0;
+            }
+            foreach (var v in rules.Values)
+            {
+                if (!pairCounts.ContainsKey(v.Item1))
+                    pairCounts[v.Item1] = 0;
+                if (!pairCounts.ContainsKey(v.Item2))
+                    pairCounts[v.Item2] = 0;
+            }
 
-            for (int _ = 0; _ < 10; _++)
-                chain = Step(chain, rules);
-            Console.WriteLine($"P1: {_counts.Values.Max() - _counts.Values.Min()}");
+            // Pairs found in the initial template
+            for (int j = 0; j < template.Length - 1; j++)
+                ++pairCounts[$"{template[j]}{template[j + 1]}"];
+
+            // Set up the dictionary to track the counts of each element
+            Dictionary<char, ulong> eltCount = "ABCDEFGHIJKLMNOPQRSTUVWXYZ".Select(c => (c, (ulong) 0)).ToDictionary(c => c.Item1, c => c.Item2);
+            foreach (char c in template)
+                ++eltCount[c];
+
+            // There's probably a less clunky way to do this, but I am figuring out, from the 
+            // current state, what new pairs will be added. I've built the table so that as the 
+            // original rule is, NN -> C, this turns NN into an NC and a CN. (So we have to subtract
+            // the removed NNs from their counts. So first generate the list of what will be changed
+            // in the current polymer, then actually do the changes and count the # of elements added.
+            for (int _ = 0; _ < generations; ++_)
+            {
+                var changes = new List<(string, ulong)>();
+                foreach (var k in pairCounts.Keys)
+                {                    
+                    ulong count = pairCounts[k];
+                    if (count > 0)
+                        changes.Add((k, count));                    
+                }
+
+                foreach (var change in changes)
+                {
+                    string pair = change.Item1;
+                    ulong count = change.Item2;
+                    eltCount[rules[pair].Item3] += count;
+                    pairCounts[rules[pair].Item1] += count;
+                    pairCounts[rules[pair].Item2] += count;
+                    pairCounts[pair] -= count;                   
+                }
+            }
+
+            return eltCount.Values.Max() - eltCount.Values.Where(v => v > 0).Min();
         }
 
         public void Solve()
         {
-            Part1();
+            Console.WriteLine($"P1: {ProcessPolymer(10)}");
+            Console.WriteLine($"P2: {ProcessPolymer(40)}");
         }
     }
 }
