6c0755a4a5634b9e7fcaaab35a672d141fe032cf
Dana Larose <dana@lrsd.net>
2021-12-17 13:20:04 -0600

    Day 17 projecticle calculation working, I think

---
+++ 2021/Day17.cs
@@ -0,0 +1,32 @@
+﻿using AoC;
+
+namespace _2021
+{
+    internal class Day17 : IDay
+    {
+        bool FireProbe(int initialVx, int initialVy, int targetMinX, int targetMaxX, int targetYLower, int targetYUpper)
+        {
+            int x = 0, y =0;
+            int xv = initialVx;
+            int yv = initialVy;
+
+            while (x <= targetMaxX && y >= targetYUpper)
+            {
+                x += xv;
+                if (xv > 0)
+                    --xv;
+                y += yv--;
+
+                if (x >= targetMinX && x <= targetMaxX && y <= targetYLower && y >= targetYUpper)
+                    return true;
+            }
+
+            return false;
+        }
+
+        public void Solve()
+        {
+            FireProbe(17, -4, 20, 30, -5, -10);
+        }
+    }
+}
