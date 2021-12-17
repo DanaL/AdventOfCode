34b43f8f8ef3e317d588e3221294ffb2fbc875cd
Dana Larose <dana@lrsd.net>
2021-12-17 15:47:24 -0600

    Day 17 Part 1 -- completed via brute force

--- 2021/Day17.cs
+++ 2021/Day17.cs
@@ -1,9 +1,13 @@
-﻿using AoC;
+﻿using System;
+
+using AoC;
 
 namespace _2021
 {
     internal class Day17 : IDay
     {
+        int _yApex;
+
         bool FireProbe(int initialVx, int initialVy, int targetMinX, int targetMaxX, int targetYLower, int targetYUpper)
         {
             int x = 0, y =0;
@@ -17,6 +21,11 @@ namespace _2021
                     --xv;
                 y += yv--;
 
+                // TODO: We can bail out if we've reached the apex and it is 
+                // less than _yApex
+                if (y > _yApex)
+                    _yApex = y;
+
                 if (x >= targetMinX && x <= targetMaxX && y <= targetYLower && y >= targetYUpper)
                     return true;
             }
@@ -26,7 +35,27 @@ namespace _2021
 
         public void Solve()
         {
-            FireProbe(17, -4, 20, 30, -5, -10);
+            // target area: x=25..67, y=-260..-200
+            //int minX = 20, maxX = 30;
+            //int minY = -5, maxY = -10;
+            int minX = 25, maxX = 67;
+            int minY = -200, maxY = -260;
+            
+            int maxDistance = Util.TaxiDistance(0, 0, maxX, maxY);
+
+            _yApex = 0;
+            int loftiest = 0;
+            for (int x = 0; x < minX; x++)
+            {
+                for (int y = 1; Util.TaxiDistance(0, 0, x ,y) < maxDistance; y++)                
+                {
+                    _yApex = 0;
+                    if (FireProbe(x, y, minX, maxX, minY, maxY) && _yApex > loftiest)
+                        loftiest = _yApex;                  
+                }                
+            }
+            
+            Console.WriteLine(loftiest);
         }
     }
 }
