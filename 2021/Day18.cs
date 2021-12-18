f22d85d9e63a821cd45e11d0324c064e693b6036
Dana Larose <ywg.dana@gmail.com>
2021-12-18 09:53:25 -0600

    Day 18 exploding pair working

--- 2021/Day18.cs
+++ 2021/Day18.cs
@@ -4,116 +4,166 @@ using System.Text;
 using AoC;
 
 namespace _2021
-{
-    class Pair
+{    
+    class Token
     {
-        public Elt Left { get; set; }
-        public Elt Right { get; set; }
+        public char Bit { get; set; }
+        public Token Left { get; set; }
+        public Token Right { get; set; }
 
-        public Pair() { }
-
-        public Pair(int left, int right)
+        public Token(char bit)
         {
-            Left = new Elt(left);
-            Right = new Elt(right);
+            Bit = bit;            
         }
 
-        public Pair(Elt left, Elt right)
+        public Token Tail()
         {
-            Left = left;
-            Right = right;
+            Token n;
+            for (n = this; n.Right != null; n = n.Right)
+                ;
+            return n;
         }
 
-        public Pair Add(Elt left, Elt right)
+        public bool IsDigit()
         {
-            return new Pair()
+            return Bit switch
             {
-                Left = left,
-                Right = right
+                '[' => false,
+                ']' => false,
+                ',' => false,
+                _ => true
             };
+        }
+    }
+
+    class SnailNum
+    {
+        Token _head;
+
+        public SnailNum(Token d)
+        {
+            _head = d;
+        }
+
+        public SnailNum(string snailStr)
+        {
+            _head = new Token(snailStr[0]);
+            Token curr = _head, prev = _head;
+            for (int j = 1; j < snailStr.Length; j++)
+            {
+                var d = new Token(snailStr[j]);
+                d.Left = curr;
+                curr.Right = d;
+                curr = d;
+            }
+        }
+
+        public SnailNum Add(SnailNum other)
+        {
+            var newHead = new Token('[');
+            var comma = new Token(',');
+            var close = new Token(']');
+
+            newHead.Right = _head;
+            _head.Left = newHead;
+
+            var tail = _head.Tail();
+            tail.Right = comma;
+            comma.Left = tail;
+
+            comma.Right = other._head;
+            other._head.Left = comma;
+
+            tail = other._head.Tail();
+            tail.Right = close;
+            close.Left = tail;
+
+            return new SnailNum(newHead);
+        }
+
+        private void ExplodeAt(Token d)
+        {
+            var v = d.Right;
+            // scan left looking for a number
+            int leftVal = v.Bit - '0';
+
+            for (var n = v.Left; n.Left != null; n = n.Left)
+            {
+                if (n.IsDigit())
+                {
+                    n.Bit = (char)(n.Bit + leftVal);   // I think this should temporarily handle the case of 9 + 3 = 12?
+                    break;                             // It'll store 12 as '<' and those are the numbers to shrink later
+                }
+            }
+
+            // check for rightward number
+            v = d.Right.Right.Right; // skip comma
+            int rightVal = v.Bit - '0';
+            v = v.Right; // pass right value
+            for (var n = v.Right; n.Right != null; n = n.Right)
+            {
+                if (n.IsDigit())
+                {
+                    n.Bit = (char)(n.Bit + rightVal);   // I think this should temporarily handle the case of 9 + 3 = 12?
+                    break;                             // It'll store 12 as '<' and those are the numbers to shrink later
+                }
+            }
+
+            // Now, replace pair with 0
+            var prev = d.Left;
+            var next = d.Right.Right.Right.Right.Right;
+            var zero = new Token('0');
+            prev.Right = zero;
+            zero.Left = prev;
+            zero.Right = next;
+            next.Left = zero;
+        }
+
+        public void CheckForExplosion()
+        {
+            int bcount = 0;
+            Token d = _head;
+            while (d != null)
+            {
+                if (d.Bit == '[')
+                    ++bcount;
+                if (bcount >= 5 && d.Right.IsDigit() && d.Right.Right.Right.IsDigit())
+                {
+                    ExplodeAt(d);
+                    break;
+                }
+                d = d.Right;
+            }
         }
 
         public override string ToString()
         {
             StringBuilder sb = new StringBuilder();
-            sb.Append('[');
-            if (Left.IsNum)
-                sb.Append(Left.Val);
-            else
-                sb.Append(Left.Pair.ToString());
-            sb.Append(", ");
-            if (Right.IsNum)
-                sb.Append(Right.Val);
-            else
-                sb.Append(Right.Pair.ToString());
-            sb.Append(']');
+
+            for (var n = _head; n != null; n = n.Right)
+            {
+                sb.Append(n.Bit);
+                if (n.Bit == ',')
+                    sb.Append(' ');
+            }
 
             return sb.ToString();
         }
     }
 
-    class Elt
-    {
-        public bool IsNum { get; set; }
-        public int Val { get; set; }
-        public Pair Pair { get; set; }
-
-        public Elt(int x)
-        {
-            Val = x;
-            IsNum = true;
-        }
-
-        public Elt(Pair p)
-        {
-            Pair = p;
-            IsNum = false;
-        }
-
-        public override string ToString()
-        {
-            return IsNum ? Val.ToString() : Pair.ToString();
-        }
-    }
-
     public class Day18 : IDay
-    {
-        Elt ParseSnailNumber(string txt, ref int pos)
-        {
-            if (txt[pos] == '[')
-            {
-                // start new pair
-                pos += 1;
-                var left = ParseSnailNumber(txt, ref pos);
-                var right = ParseSnailNumber(txt, ref pos);
-                pos += 1; // skip the closing ]
-                Pair p = new Pair(left, right);
-                return new Elt(p);
-            }
-            else
-            {
-                int x = txt[pos] - '0';
-                pos += 2;
-                return new Elt(x);
-            }
-        }
-
+    {       
         public void Solve()
         {
-            int x = 0;
-            var elt = ParseSnailNumber("[[9,9],[[3,[1,4]],[7,0]]]", ref x);
-            Console.WriteLine(elt.ToString());
-            Console.WriteLine(elt.Pair.Right.ToString());
+            var sn = new SnailNum("[[[[[9,8],1],2],3],4]");            
+            Console.WriteLine(sn);
+            sn.CheckForExplosion();
+            Console.WriteLine(sn);
 
-            x = 0;
-            var elt2 = ParseSnailNumber("[[[[1,3],[5,3]],[[1,3],[8,7]]],[[[4,9],[6,9]],[[8,2],[7,3]]]]", ref x);
-            Console.WriteLine(elt2.ToString());
-            Console.WriteLine(elt2.Pair.Left.ToString());
-
-            var a = new Elt(new Pair(4, 5));
-            var b = new Elt(new Pair(7, 2));
-            var c = new Elt(new Pair(a, b));
-            Console.WriteLine(c.ToString());
+            var sn2 = new SnailNum("[[3,[2,[8,0]]],[9,[5,[4,[3,2]]]]]");
+            Console.WriteLine(sn2);
+            sn2.CheckForExplosion();
+            Console.WriteLine(sn2);
         }
     }
 }
