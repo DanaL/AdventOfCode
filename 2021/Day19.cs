e70638793cea6f4364f39175c72ae43f92b213d5
Dana Larose <dana@lrsd.net>
2021-12-21 15:42:52 -0600

    Day 19 -- working on scanning rotations against other points

--- 2021/Day19.cs
+++ 2021/Day19.cs
@@ -9,7 +9,7 @@ namespace _2021
 {
     public class Day19 : IDay
     {
-        Dictionary<int, List<(int, int, int)>> _scanners;
+        Dictionary<int, HashSet<(int, int, int)>> _scanners;
 
         static int[] _r0  = new int[] {  0,  0,  1,  0,  1,  0, -1,  0,  0 };
         static int[] _r1  = new int[] { -1,  0,  0,  0,  1,  0,  0,  0, -1 };
@@ -60,37 +60,85 @@ namespace _2021
         void Input()
         {
             var lines = File.ReadAllLines("inputs/day19.txt").Where(l => l.Trim() != "");
-            _scanners = new Dictionary<int, List<(int, int, int)>>();
+            _scanners = new Dictionary<int, HashSet<(int, int, int)>>();
 
             foreach (var line in lines)
             {
                 if (line.StartsWith("---")) {
                     // I'm too tired and sad to remember how to format regexes...
                     int scannerNum = int.Parse(line.Split(' ')[2]);
-                    _scanners.Add(scannerNum, new List<(int, int, int)>());                   
+                    _scanners.Add(scannerNum, new HashSet<(int, int, int)>());                   
                 }
                 else
                 {
                     int[] b = line.Split(',').Select(int.Parse).ToArray();
                     _scanners[_scanners.Count - 1].Add((b[0], b[1], b[2]));
                 }
-                        
             }
         }
         
+        void PretendPointIsOtherPoint((int X, int Y, int Z) pt, (int X, int Y, int Z) otherPt, List<(int X, int Y, int Z)> pts, HashSet<(int, int, int)> otherPts)
+        {
+            var transpose = (otherPt.X - pt.X, otherPt.Y - pt.Y, otherPt.Z - pt.Z);
+
+            int count = 0;
+            foreach (var p in pts)
+            {
+                var tp = (p.X + transpose.Item1, p.Y + transpose.Item2, p.Z + transpose.Item3);
+                if (otherPts.Contains(tp))
+                    ++count;
+            }
+
+            if (count >= 12)
+                Console.WriteLine($"Fuck yeah, we found {count} matching points!");
+        }
+
+        void ComparePtsToOtherScanners(List<(int, int, int)> pts, int otherScannerID)
+        {
+            for (int p = 0; p < pts.Count; p++)
+            {
+                foreach (var otherPt in _scanners[otherScannerID])
+                {
+                    PretendPointIsOtherPoint(pts[p], otherPt, pts, _scanners[otherScannerID]);
+                }
+            }            
+        }
+
+        void CompareToOtherScanners((int, int, int)[,] allRotations, int otherScannerID)
+        {
+            for (int r = 0; r < 24; r++)
+            {
+                var pts = new List<(int, int, int)>();
+                for (int p = 0; p < allRotations.GetLength(1); p++)
+                    pts.Add(allRotations[r, p]);
+
+                // Okay, pts is one of the sets of rotated points from a scanner. We
+                // want to transpose/compare them to the other sets
+                ComparePtsToOtherScanners(pts, otherScannerID);
+            }
+        }
+
         void CheckScanner(int scannerNum)
         {
             // First, generate all the rotations of our selected scanner
             var pts = _scanners[scannerNum];
             (int, int, int)[,] rotated = new (int, int, int)[pts.Count, _rotations.Length];
 
-            for (int p = 0; p < pts.Count; p++)
+            int p = 0;
+            foreach (var pt in pts)
             {
                 for (int r = 0; r < _rotations.Length; r++)
                 {
-                    rotated[p, r] = Rotate(pts[p], r);
+                    rotated[p, r] = Rotate(pt, r);
                 }
-            }            
+                ++p;
+            }
+
+            // Next up: compare to the other points
+            foreach (var otherScanner in _scanners.Keys.Where(k => k != scannerNum))
+            {
+                CompareToOtherScanners(rotated, otherScanner);
+            }
         }
 
         public void Solve()
