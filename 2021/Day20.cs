b98309575f1e14adbf5ec2415f1f1c9acdb03f32
Dana Larose <ywg.dana@gmail.com>
2021-12-21 01:50:09 -0600

    Day 20 part 2 done

--- 2021/Day20.cs
+++ 2021/Day20.cs
@@ -9,30 +9,31 @@ namespace _2021
 {
     class Image
     {
-        public bool InfinityLit { get; set; }
-        List<List<bool>> _pixels;
+        bool _infinityLit;
+        int _imgWidth;
+        int _imgHeight;
+        bool[] _pixels;
+        char[] _alg;
 
-        public Image(List<List<bool>> pixels)
+        public Image(bool[] pixels, int width, bool infinityLit, char[] alg)
         {
             _pixels = pixels;
+            _imgWidth = width;
+            _imgHeight = pixels.Length / width;
+            _infinityLit = infinityLit;
+            _alg = alg;
         }
 
         public int PixelCount {
-            get {
-                int sum = 0;
-
-                foreach (var row in _pixels)
-                    sum += row.Where(p => p).Count();
-                return sum;
-            }            
+            get => _pixels.Where(p => p).Count();       
         }
 
         public bool InBounds(int x, int y)
         {
-            return x >= 0 && x < _pixels[0].Count && y >= 0 && y < _pixels.Count;
+            return x >= 0 && x < _imgWidth && y >= 0 && y < _imgHeight;
         }
 
-        bool CalcPixel(int x, int y, bool[] alg)
+        bool CalcPixel(int x, int y)
         {
             int val = 0;
 
@@ -40,85 +41,65 @@ namespace _2021
             {
                 for (int c = -1; c < 2; c++)
                 {
-                    val <<= 1;
+                    val *= 2;
                     if (InBounds(x + c, y + r)) {
-                        if (_pixels[y + r][x + c])
+                        if (_pixels[(y + r) * _imgWidth + x + c])
                             val |= 1;
                     }
-                    else if (InfinityLit)
+                    else if (_infinityLit)
                         val |= 1;
                 }
             }
 
-            return alg[val];
+            return _alg[val] == '#';
         }
 
-        public Image Retouch(bool[] alg)
-        {
-            int height = _pixels.Count;
-            int width = _pixels[0].Count;
-            int buffer = 2;
-
-            var retouched = new List<List<bool>>();
-            for (int y = 0; y < height + buffer * 2; y++)
+        public Image Retouch()
+        {            
+            var pts = new HashSet<(int, int)>();
+            for (int y = -1; y <= _imgHeight; y++)
             {
-                List<bool> row = new List<bool>();
-                for (int x = 0; x < width + buffer * 2; x++)
-                    row.Add(false);
-                retouched.Add(row);
-            }
-            
-            for (int y = -buffer; y <= height + buffer - 1; y++)
-            {
-                for (int x = -buffer; x <= width + buffer - 1; x++)
+                for (int x = -1; x <= _imgWidth; x++)
                 {
-                    var result = CalcPixel(x, y, alg);
-                    retouched[y + buffer][x + buffer] = result;
+                    var result = CalcPixel(x, y);
+                    if (result)
+                        pts.Add((x, y));
                 }
             }
 
-            // Toggle whether or not infinity is lit up or not
-            if (!InfinityLit && alg[0] || InfinityLit && !alg.Last())
-                InfinityLit = !InfinityLit;
+            int newWidth = _imgWidth + 2;
+            var retouched = new bool[newWidth * (_imgHeight + 2)];
+            foreach (var pt in pts)
+                retouched[(pt.Item2 + 1) * newWidth + pt.Item1 + 1] = true;
 
-            return new Image(retouched) {
-                InfinityLit = InfinityLit
-            };
+            // Toggle whether or not infinity is lit up or not
+            // I had to go to reddit to realize I had to account for the fact that
+            // 9 lit sqs will be extinguished every other generation but the last
+            // bit in the alg string
+            if (!_infinityLit && _alg[0] == '#' || _infinityLit && _alg.Last() == '.')
+                _infinityLit = !_infinityLit;
+
+            return new Image(retouched, newWidth, _infinityLit, _alg);
         }
     }
 
     internal class Day20 : IDay
     {
-        bool[] _alg;
-        
         Image Input()
         {
-            var lines = File.ReadAllLines("inputs/day20.txt");
-            _alg = lines[0].Select(c => c == '#').ToArray();
-
-            List<List<bool>> pixels = new List<List<bool>>();
-            foreach (var line in lines.Skip(2))
-            {
-                var row = line.Select(c => c == '#').ToList();
-                pixels.Add(row);
-            }
-
-            Image img = new Image(pixels)
-            {
-                InfinityLit = false
-            };
-
-            return img;
+            var lines = File.ReadAllLines("inputs/day20.txt");           
+            var pixels = string.Join("", lines.Skip(2)).Select(c => c == '#').ToArray();
+            return new Image(pixels, lines[2].Length, false, lines[0].Select(c => c).ToArray());
         }
 
         public void Solve()
         {
             var img = Input();
             
-            Image retouched = img.Retouch(_alg);
+            Image retouched = img.Retouch();
             for (int j = 0; j < 49; j++)
             {
-                retouched = retouched.Retouch(_alg);
+                retouched = retouched.Retouch();
                 if (j == 0)
                     Console.WriteLine($"P1: {retouched.PixelCount}");
             }
