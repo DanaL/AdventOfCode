5987a547f6dbcf771431711ac0781b8627dae313
Dana Larose <ywg.dana@gmail.com>
2022-11-04 18:13:42 -0500

    Recursive function for 2015 Day 01 part 02

--- 2015/Day01.fsx
+++ 2015/Day01.fsx
@@ -1,17 +1,28 @@
 open System
 open System.IO;
 
-let partOne =
-    let convert c =
-        if c = ')' then -1
-        else 1
+let convert c =
+    if c = ')' then -1
+    else 1
 
-    let input = File.ReadAllText("input.txt")
+let input = File.ReadAllText("input_day01.txt")
+
+let partOne =
     let result = input.ToCharArray()
-                 |> Array.map (fun c -> if c = ')' then -1
-                                        else 1)
+                 |> Array.map convert
                  |> Array.sum
 
-    Console.WriteLine(result)
+    Console.WriteLine($"Part 1: %d{result}")
 
+let partTwo =
+    let arr = input.ToCharArray() |> Array.map convert
+    
+    let rec elevate (arr : int[]) floor pos =
+        let newFloor = floor + arr[pos]
+        if newFloor < 0 then pos + 1
+        else elevate arr newFloor (pos + 1)
+
+    
+    let result = elevate arr 0 0
+    Console.WriteLine($"Part 2: %d{result}")
     
