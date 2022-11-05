93a1612ba5d7583ccca1143afb4420c3b9c44fca
Dana Larose <ywg.dana@gmail.com>
2022-11-05 08:20:18 -0500

    A bit of cody tidying

--- 2015/Day02.fsx
+++ 2015/Day02.fsx
@@ -2,26 +2,29 @@ open System
 open System.IO
 open System.Text.RegularExpressions
 
-let parseLine txt =
-    let m = Regex.Match(txt, @"(\d+)x(\d+)x(\d+)")
-    m.Groups[1].Value |> int, m.Groups[2].Value |> int, m.Groups[3].Value |> int
+let part1 readData =
+    let wrappingNeeded (l, w, h) =
+        let sides = [| l * w; l * h; h * w |] |> Array.sort
+        2 * (sides |> Array.sum) + sides[0]
 
-let wrappingNeeded (l, w, h) =
-    let sides = [| l * w; l * h; h * w |] |> Array.sort
-    2 * (sides |> Array.sum) + sides[0]
-
-let part1 =
-    File.ReadAllLines("input_day02.txt")
-    |> Array.map parseLine
+    readData
     |> Array.sumBy wrappingNeeded
 
-let part2 =
+let part2 readData =
     let ribbon (l, w, h) =
         let perim = [| (l + w) * 2; (l + h) * 2; (h + w) *2  |] |> Array.min
         perim + (l * w * h)
+        
+    readData
+    |> Array.sumBy ribbon
+
+let readData =
+    let parseLine txt =
+        let m = Regex.Match(txt, @"(\d+)x(\d+)x(\d+)")
+        m.Groups[1].Value |> int, m.Groups[2].Value |> int, m.Groups[3].Value |> int
+
     File.ReadAllLines("input_day02.txt")
     |> Array.map parseLine
-    |> Array.sumBy ribbon
-    
-Console.WriteLine($"Part 1: %d{part1}")
-Console.WriteLine($"Part 2: %d{part2}")
+
+Console.WriteLine($"Part 1: %d{part1 readData}")
+Console.WriteLine($"Part 2: %d{part2 readData}")
