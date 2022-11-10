ace848d35294135a9f044a179c4ebabc18bfb142
Dana Larose <ywg.dana@gmail.com>
2022-11-10 10:02:17 -0600

    2015 Day 5 part 2 done

--- 2015/Day05.fsx
+++ 2015/Day05.fsx
@@ -1,6 +1,10 @@
 open System
 open System.IO
 
+// This is on the whole probably more verbose than it needs to be
+// and I could definitely replace a bunch of the helper functions
+// with regular expressions but I don't typically try to code golf
+// AoC problems too hard and it's been food F# practice
 let threeVowels (s:string) =
     let isVowel (c:char) =
         "aeiouAEIOU".IndexOf(c) >= 0
@@ -46,9 +50,37 @@ let triplets (s:string) =
     |> Seq.fold(fun arr i ->
                 let t = s[i], s[i+1], s[i+2]
                 [| t |] |> Array.append arr) [| |]
-                
-let x = triplets "abcdefgh"
-x |> Array.map(fun a -> $"%A{a}")
-  |> Array.iter Console.WriteLine
 
+let repeats s =
+    s |> triplets
+      |> Array.exists (fun (a, b, c) -> a = c)
+
+let crossProduct s1 s2 =
+    seq { for elt1 in s1 do
+             for elt2 in s2 do
+                 yield elt1, elt2 }
+             
+let pairsOfPairs (s:string) =
+    let pairs =
+        { 0 .. s.Length - 2 }
+        |> Seq.fold (fun arr i ->
+                     let p = s[i], s[i+1], i, i+1
+                     [| p |] |> Array.append arr) [| |]
+    
+    // Okay, I have pairs from the string in the form:
+    //    ('a', 'a', 0, 1) (#s are their index in the string)
+    // Now I want a cross-join of all the elts of the list having
+    // matching letters and no shared indexes
+    let cp = crossProduct pairs pairs
+    cp |> Seq.exists (fun (p1, p2) ->
+                      let a1, a2, a3, a4 = p1
+                      let b1, b2, b3, b4 = p2
+                      a1 = b1 && a2 = b2 && a3 <> b3 && a3 <> b4
+                          && a4 <> b3 && a4 <> b4)
+
+let validate2 = makeValidator [ repeats; pairsOfPairs ]
+let p2 = File.ReadAllLines("input_day05.txt")
+         |> Array.filter(fun line -> validate2 line)
+         |> Array.length
+Console.WriteLine($"P2: %d{p2}")
 
