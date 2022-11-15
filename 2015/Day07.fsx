7f0f9fbbabb1a2a52f307a8649981a21d3faf235
Dana Larose <ywg.dana@gmail.com>
2022-11-14 18:42:00 -0600

    2015 Day 7 Part 1: implemented the rest of the eval functions

--- 2015/Day07.fsx
+++ 2015/Day07.fsx
@@ -8,8 +8,8 @@ type Expr =
     | Value of uint16
     | Var of string
     | Not of string
-    | LShift of string * uint16
-    | RShift of string * uint16
+    | LShift of string * int
+    | RShift of string * int
     | And of string * string
     | Or of string * string
         
@@ -27,8 +27,8 @@ let parse (line:string) =
         | _ -> match pieces[1] with
                    | "AND" -> (out, And(pieces[0], pieces[2]))
                    | "OR" -> (out, Or(pieces[0], pieces[2]))
-                   | "LSHIFT" -> (out, LShift(pieces[0], UInt16.Parse(pieces[2])))
-                   | "RSHIFT" -> (out, RShift(pieces[0], UInt16.Parse(pieces[2])))
+                   | "LSHIFT" -> (out, LShift(pieces[0], Int32.Parse(pieces[2])))
+                   | "RSHIFT" -> (out, RShift(pieces[0], Int32.Parse(pieces[2])))
                    | _ -> failwith "We should not ever get here :o"
 
 let evalSetVar out key (signals:Mem) =
@@ -44,6 +44,34 @@ let evalNot out key (signals:Mem) =
         true
     else
         false
+
+let evalAnd out a b (signals:Mem) =
+    if not <| (signals.ContainsKey(a) || signals.ContainsKey(b))  then
+        false
+    else
+        signals[out] <- signals[a] &&& signals[b]
+        true
+
+let evalOr out a b (signals:Mem) =
+    if not <| (signals.ContainsKey(a) || signals.ContainsKey(b))  then
+        false
+    else
+        signals[out] <- signals[a] ||| signals[b]
+        true
+
+let evalLShift out a b (signals:Mem) =
+    if not <| signals.ContainsKey(a) then
+        false
+    else
+        signals[out] <- signals[a] <<< b
+        true
+
+let evalRShift out a b (signals:Mem) =
+    if not <| signals.ContainsKey(a) then
+        false
+    else
+        signals[out] <- signals[a] >>> b
+        true
         
 let eval expr (signals:Mem) =
     match expr with
@@ -52,32 +80,50 @@ let eval expr (signals:Mem) =
             None
         | out, Var(v) -> 
             if evalSetVar out v signals then None else Some expr
-
         | out, Not(v) ->
             if evalNot out v signals then None else Some expr
-        | _ -> failwith "not implemented yet"
+        | out, And(a, b) ->
+            if evalAnd out a b signals then None else Some expr
+        | out, Or(a, b) ->
+            if evalOr out a b signals then None else Some expr
+        | out, LShift(a, b) ->
+            if evalLShift out a b signals then None else Some expr
+        | out, RShift(a, b) ->
+            if evalRShift out a b signals then None else Some expr
 
 let signals = new Mem()
 
 let s0 = parse "14 -> a"
-Console.WriteLine(s0)
 eval s0 signals
 let s1 = parse "5 -> b"
 eval s1 signals
 
-let s2 = parse "b -> c"
+let s2 = parse "1 -> c"
 eval s2 signals
 
 let s3 = parse "NOT a -> d"
 eval s3 signals
 
+let s4 = parse "a OR c -> xx"
+eval s4 signals
+
+let s5 = parse "a LSHIFT 2 -> xy"
+eval s5 signals
+
+let s6 = parse "20 -> rt"
+eval s6 signals
+let s7 = parse "rt RSHIFT 1 -> tr"
+eval s7 signals
+Console.WriteLine(signals["tr"])
+eval (parse "tr -> gb") signals
+Console.WriteLine(signals["gb"])
 //Console.WriteLine(s2)
 //let s3 = parse "a RSHIFT 2 -> d"
 //Console.WriteLine(s3)
 //let s4 = parse "jj -> wd"
 //Console.WriteLine(s4)
 
-Console.WriteLine($"%A{signals}")
+//Console.WriteLine($"%A{signals}")
 // So, now:
 // 1) parse all the lines, build a queue of statements
 // 2) eval statements one by one, eval returns Some statement
