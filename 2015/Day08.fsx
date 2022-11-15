1d922286159fc1ce95fafea5864dcc6b54fc9c15
Dana Larose <ywg.dana@gmail.com>
2022-11-14 22:29:35 -0600

    2015 Day 8 Part 2 complete

--- 2015/Day08.fsx
+++ 2015/Day08.fsx
@@ -3,9 +3,15 @@ open System.IO
 open System.Text.RegularExpressions
 
 let pattern = "(\\\\[\\\\\\\"])|(\\\\x[0-9|a-f][0-9|a-f])" 
-let count (s:string) =
+let countP1 (s:string) =
     s.Length - (Regex.Replace(s, pattern, "*").Length - 2)
 let p1 = File.ReadAllLines("input_day08.txt")
-         |> Array.fold(fun total s -> total + count s) 0
+         |> Array.fold(fun total s -> total + countP1 s) 0
 Console.WriteLine($"%d{p1}")
+
+let countP2 (s:string) =
+    s.Replace("\"", "\"\"").Replace("\\", "\\\\").Length - s.Length + 2
+let p2 = File.ReadAllLines("input_day08.txt")
+         |> Array.fold(fun total s -> total + countP2 s) 0
+Console.WriteLine($"%d{p2}")
 
