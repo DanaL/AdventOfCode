5b2df3bff89086bc6a52d6e59ea01feacbee3d76
Dana Larose <ywg.dana@gmail.com>
2022-11-16 20:41:44 -0600

    2015 Day 9 parts 1 and 2 complete

--- 2015/Day09.fsx
+++ 2015/Day09.fsx
@@ -1,4 +1,5 @@
 open System
+open System.IO
 
 // Permutation code stolen from a stack overflow question. It took me
 // a bit to grok how it was working
@@ -17,9 +18,32 @@ let rec permutations = function
         let tails = permutations xs
         List.concat (tails |> List.map f)
 
-// Okay now I want to:
-// 1) generate map of distances
-//    for each line foo to bar = 7, I want (foo, bar): 7 and (bar, foo): 7
-// 2) Generate all permutations of the locations
-// 3) Calculate the distance of each route and select shortest
-       
+// Generate map of distances
+let distances = File.ReadAllLines("input_day09.txt")
+                |> Array.map(fun line -> let p = line.Split(' ')
+                                         let d = p[4] |> int
+                                         [(p[0], p[2]), d; (p[2], p[0]), d] )
+                |> List.concat
+                |> Map.ofList
+
+// Make the list of towns and generate all the permutations of them                
+let towns = distances |> Map.keys
+                      |> Seq.map(fun (a, b) -> a)
+                      |> Seq.distinct
+                      |> List.ofSeq
+let routes = permutations towns
+
+// Calculate the distance of each route and select shortest
+let lengthOfRoute route =
+    route |> List.pairwise
+          |> List.map(fun p -> distances[p])
+          |> List.sum
+
+let p1 = routes |> List.map lengthOfRoute
+                |> List.min
+Console.WriteLine($"P1: %d{p1}")
+
+let p2 = routes |> List.map lengthOfRoute
+                |> List.max
+Console.WriteLine($"P2: %d{p2}")
+
