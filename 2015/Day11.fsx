9c31773bbe345f245d3479029ef1a024d7bc1d63
Dana Larose <ywg.dana@gmail.com>
2022-11-18 16:52:09 -0600

    2015 Day 11 part 1 -- function to convert from uint64 to base 26 string, Rule 2 implemented

--- 2015/Day11.fsx
+++ 2015/Day11.fsx
@@ -1,33 +1,37 @@
 open System
 
+// So for day 11 we have to generate sequential passwords by incrementing
+// one letter at a time: aaaa, aaab, aaac, ... aaaz, aaba, ... and checking
+// it conforms to a few rules. I overengineered the problem by writing a
+// functions to convert to/from a base-26 number
 let base26Str x =
     let rec digits n d =
-        let r = n % 26
-        let x = n / 26
-        if x = 0 then
+        let r = n % 26UL
+        let x = n / 26UL
+        if x = 0UL then
             r::d
         else
             digits x (r::d)
-
-    let y = digits x []
-    y |> List.iter(fun d -> Console.Write($"%d{d} "))
-    Console.WriteLine()
     
     [] |> digits x       
-       |> List.map(fun c -> char(c + int 'a') |> string)      
+       |> List.map(fun c -> char(c + 97UL) |> string)      
        |> String.concat ""
 
-//Console.WriteLine(base26Str 0)
-//Console.WriteLine(base26Str 25)
+let toDec (w:string) =
+    w.ToCharArray()
+    |> Array.mapi(fun i c ->
+                  // Sometimes a strict type system is a real pain...
+                  ((c |> uint64) - 97UL) * ((pown 26UL (w.Length - i - 1)) |> uint64))
+    |> Array.sum
 
-Console.WriteLine(base26Str 621)
-Console.WriteLine(base26Str 622)
-Console.WriteLine(base26Str 623)
-Console.WriteLine(base26Str 624)
-Console.WriteLine(base26Str 675)
-Console.WriteLine(base26Str 676)
-//Console.WriteLine(base26Str 27)
+let rule2 (s:string) =
+    not <| (s.IndexOf('i') > -1 || s.IndexOf('l') > -1 || s.IndexOf('o') > -1)
 
-//for j in 0 .. 30 do
-//    let s = base26Str j
-//    Console.WriteLine($"%3d{j} -> %s{s}")
+let s = "ghijklmn"
+Console.WriteLine(rule2 s)
+
+//let n = toDec s
+//for j in 0 .. 50 do
+//    let x = n + (j |> uint64)
+//    let s = base26Str x
+//    Console.WriteLine(s.PadLeft(8, 'a'))
