541df80e2157eb9c631caae690c824753e14684a
Dana Larose <ywg.dana@gmail.com>
2022-11-20 11:41:40 -0600

    2015 Day 14 part 2 complete

--- 2015/Day14.fsx
+++ 2015/Day14.fsx
@@ -19,6 +19,24 @@ let p1 = File.ReadAllLines("input_day14.txt")
          |> Array.map(parse)
          |> Array.map(distance 2503)
          |> Array.max
-printfn $"P1: {p1}"         
+printfn $"P1: {p1}"
 
-                        
+// Okay, for part 2 we need to check after each second to see which
+// reindeer is in the lead and award them a point
+let herd = File.ReadAllLines("input_day14.txt")
+           |> Array.map(parse)
+let pts = herd |> Array.map(fun r -> r.Name, 0)
+               
+let checkPlaces time herd pts =
+    let scores = herd |> Array.map(fun r -> r.Name, distance time r)         
+    let best = scores |> Array.map(fun (_, score) -> score) |> Array.max
+    let leaders = scores |> Array.filter(fun (r, score) -> score = best)
+                         |> Array.map(fun (r, _) -> r)
+    pts |> Array.map(fun (r, sc) -> if Array.contains r leaders then r, sc+1
+                                    else r, sc)
+    
+let p2 = seq { 1 .. 2503 }
+         |> Seq.fold(fun scores time -> checkPlaces time herd scores) pts
+         |> Array.map(fun (r, s) -> s) |> Array.max
+printfn $"P2: {p2}"
+
