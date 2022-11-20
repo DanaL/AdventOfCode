d24f5a2457522dd831be75f669841dde93fdef60
Dana Larose <ywg.dana@gmail.com>
2022-11-19 21:05:48 -0600

    2015 Day 13 part 2 done

--- 2015/Day13.fsx
+++ 2015/Day13.fsx
@@ -1,3 +1,4 @@
+open System
 open System.IO
 
 #load "Util.fsx"
@@ -21,11 +22,31 @@ let people =
     |> Array.distinct
     |> List.ofArray
     
-let happiness (seating:string list) =
-    (seating[0], seating |> List.last)::(seating |> List.pairwise)
-    |> List.map(fun (a, b) -> rels[a, b] + rels[b, a])
-    |> List.sum
+let happiness (seating:string list) (rels:Map<(string * string),int>)  =
+    let score = (seating[0], seating |> List.last)::(seating |> List.pairwise)
+                |> List.map(fun (a, b) -> rels[a, b] + rels[b, a])
+                |> List.sum
+    score, seating
     
-let p1 = (permutations people) |> List.map(fun s -> happiness s)
-                               |> List.max
+let p1,_ = (permutations people)
+           |> List.map(fun s -> happiness s rels)
+           |> List.reduce(fun (sa, pa) (sb, pb) ->                              
+                              if sb > sa then sb, pb else sa, pa)
 printfn $"P1: {p1}"
+
+// For part 2, add myself to the list of people, relatinships
+// of score 0 with me and everyone, then find the new best arrangement
+// and finally the difference without me in it. (I think that's what
+// the problem is asking...)
+let peopleP2 = "Dana"::people
+let relsWithMe = people |> List.map(fun p -> [(p, "Dana"), 0; ("Dana", p), 0])
+                        |> List.concat
+                        |> Map.ofList
+
+let relsP2 = Map.fold(fun acc key value -> Map.add key value acc) rels relsWithMe
+
+let p2, seating = (permutations peopleP2)
+                     |> List.map(fun s -> happiness s relsP2)
+                     |> List.reduce(fun (sa, pa) (sb, pb) ->
+                                        if sb > sa then sb, pb else sa, pa)
+printfn $"P2: {p2}"
