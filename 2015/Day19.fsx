bf4d9a097b1eda84f08080dfbd341be829f4c79f
Dana Larose <ywg.dana@gmail.com>
2022-11-23 17:09:46 -0600

    2015 Day 19 part 1 complete

--- 2015/Day19.fsx
+++ 2015/Day19.fsx
@@ -10,17 +10,22 @@ let dict = lines[0..lines.Length - 3]
            |> Array.map(fun (key, values) -> (key, [for k, v in values -> v]))
            |> Map.ofArray
 
-let find (m:string) (s:string) (i:int) =
+let rec find (m:string) (s:string) (i:int) =
     let i' = m.IndexOf(s, i)
     if i' = -1 then
         []
     else        
         let head = m[0..(i'-1)]
         let tail = m[i'+s.Length..]
-        dict[s] |> List.map(fun v -> head + v + tail)
-        
-let r = find molecule "H" 6
-printfn $"%A{r}"
+        let replaced = dict[s] |> List.map(fun v -> head + v + tail)
+        replaced @ find m s (i' + s.Length)
+        |> List.distinct
+
+let r = dict.Keys |> Seq.map(fun k -> find molecule k 0)
+                  |> Seq.concat
+                  |> Seq.distinct
+                  |> List.ofSeq
+printfn $"{r.Length}"
 //let p1 = dict.Keys |> Seq.map(fun k -> Regex.Matches(molecule, k).Count * dict[k].Length)
 //                   |> Seq.sum
 //printfn $"P1: {p1}"
