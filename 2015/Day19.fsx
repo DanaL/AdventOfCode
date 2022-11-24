1c79105f779cab470160ca428f3c08446bfaa28a
Dana Larose <ywg.dana@gmail.com>
2022-11-24 17:59:11 -0600

    2015 day 19 part 2: not working; converging on a molecule from which no      further progress can be made

--- 2015/Day19.fsx
+++ 2015/Day19.fsx
@@ -1,8 +1,10 @@
+open System
 open System.Collections.Generic
 open System.IO
+open System.Text.RegularExpressions
 
 let lines = File.ReadAllLines("input_day19.txt")
-let molecule = lines[lines.Length - 1]
+let mutable molecule = lines[lines.Length - 1]
 
 let dict = lines[0..lines.Length - 3]
            |> Array.map(fun line -> line.Split(" => "))
@@ -10,10 +12,6 @@ let dict = lines[0..lines.Length - 3]
            |> Array.groupBy(fst)
            |> Array.map(fun (key, values) -> (key, [for k, v in values -> v]))
            |> Map.ofArray
-let initial = dict["e"]
-
-// Filter the electrons out of the dictionary because it'll save a teeny bit of time
-let dict' = dict |> Map.filter(fun key items -> key <> "e")
 
 let rec find (m:string) (s:string) (i:int) =
     let i' = m.IndexOf(s, i)
@@ -22,23 +20,37 @@ let rec find (m:string) (s:string) (i:int) =
     else        
         let head = m[0..(i'-1)]
         let tail = m[i'+s.Length..]
-        let replaced = dict'[s] |> List.map(fun v -> head + v + tail)
+        let replaced = dict[s] |> List.map(fun v -> head + v + tail)
         replaced @ find m s (i' + s.Length)
         |> List.distinct
 
-let r = dict'.Keys |> Seq.map(fun k -> find molecule k 0)
-                   |> Seq.concat
-                   |> Seq.distinct
-                   |> List.ofSeq
+let r = dict.Keys |> Seq.map(fun k -> find molecule k 0)
+                  |> Seq.concat
+                  |> Seq.distinct
+                  |> List.ofSeq
 printfn $"P1 {r.Length}"
 
-let queue = new Queue<(int * string)>()
-initial |> List.iter(fun s -> queue.Enqueue(1, s))
+// For part 2, we are going in the reverse order to build a new dictionary
+let dict' = lines[0..lines.Length - 3]
+            |> Array.map(fun line -> line.Split(" => "))
+            |> Array.map(fun arr -> arr[1], arr[0])
+            |> Map.ofArray
 
-let steps, str = queue.Dequeue()
-printfn $"{str}"
-dict'.Keys |> Seq.map(fun k -> find str k 0)           
-           |> Seq.iter(fun strs ->                       
-                       strs |> List.iter(fun s -> queue.Enqueue(steps + 1, s)))
+let rec replace m k (v:string) n =
+    let re = Regex(k)
+    let m' = re.Replace(m, v, 1)
+    if m' <> m then replace m' k v (n + 1)
+    else m, n
 
-printfn $"%A{queue}"           
+let mutable count = 0
+let mutable steps = 0
+while molecule <> "e" do
+    dict'.Keys
+    |> Seq.iter(fun k -> let m', s = replace molecule k dict'[k] 0
+                         molecule <- m'
+                         steps <- steps + s)
+    printfn $"{molecule}"
+    count <- count + 1
+    if count = 10 then molecule <- "e"
+printfn $"P2: {steps}"
+
