f2de28c66ef6d37fede7414cc8d8d8c17cfdab74
Dana Larose <ywg.dana@gmail.com>
2022-11-24 07:36:28 -0600

    Working on main loop for 2015 day 19 pt 2

--- 2015/Day19.fsx
+++ 2015/Day19.fsx
@@ -1,3 +1,4 @@
+open System.Collections.Generic
 open System.IO
 
 let lines = File.ReadAllLines("input_day19.txt")
@@ -9,6 +10,10 @@ let dict = lines[0..lines.Length - 3]
            |> Array.groupBy(fst)
            |> Array.map(fun (key, values) -> (key, [for k, v in values -> v]))
            |> Map.ofArray
+let initial = dict["e"]
+
+// Filter the electrons out of the dictionary because it'll save a teeny bit of time
+let dict' = dict |> Map.filter(fun key items -> key <> "e")
 
 let rec find (m:string) (s:string) (i:int) =
     let i' = m.IndexOf(s, i)
@@ -17,15 +22,23 @@ let rec find (m:string) (s:string) (i:int) =
     else        
         let head = m[0..(i'-1)]
         let tail = m[i'+s.Length..]
-        let replaced = dict[s] |> List.map(fun v -> head + v + tail)
+        let replaced = dict'[s] |> List.map(fun v -> head + v + tail)
         replaced @ find m s (i' + s.Length)
         |> List.distinct
 
-let r = dict.Keys |> Seq.map(fun k -> find molecule k 0)
-                  |> Seq.concat
-                  |> Seq.distinct
-                  |> List.ofSeq
-printfn $"{r.Length}"
-//let p1 = dict.Keys |> Seq.map(fun k -> Regex.Matches(molecule, k).Count * dict[k].Length)
-//                   |> Seq.sum
-//printfn $"P1: {p1}"
+let r = dict'.Keys |> Seq.map(fun k -> find molecule k 0)
+                   |> Seq.concat
+                   |> Seq.distinct
+                   |> List.ofSeq
+printfn $"P1 {r.Length}"
+
+let queue = new Queue<(int * string)>()
+initial |> List.iter(fun s -> queue.Enqueue(1, s))
+
+let steps, str = queue.Dequeue()
+printfn $"{str}"
+dict'.Keys |> Seq.map(fun k -> find str k 0)           
+           |> Seq.iter(fun strs ->                       
+                       strs |> List.iter(fun s -> queue.Enqueue(steps + 1, s)))
+
+printfn $"%A{queue}"           
