37f2be779434bf1112e462125b243b16640d3c5f
Dana Larose <ywg.dana@gmail.com>
2022-12-01 07:56:04 -0600

    A bit of code tidying

--- 2022/Day01.fsx
+++ 2022/Day01.fsx
@@ -1,7 +1,6 @@
 open System
 open System.IO
 
-// Part One
 let elves = File.ReadAllLines("input_day01.txt")
             |> Array.fold(fun arr c ->
                           match Int32.TryParse c with
@@ -10,10 +9,9 @@ let elves = File.ReadAllLines("input_day01.txt")
                                        | h::t -> (n::h)::t
                           | _ -> []::arr) []
             |> List.map(List.sum)
+            |> List.sortDescending
+            
+printfn $"P1: {elves[0]}"
 
-let p1 = elves |> List.max
-printfn $"P1: {p1}"
-
-// Part Two
-let p2 = elves |> List.sort |> List.rev |> List.take(3) |> List.sum
+let p2 = elves[0..2] |> List.sum
 printfn $"P2: {p2}"
