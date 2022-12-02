fa5e196921c0c3afbe84bd804dc2bf762b230c71
Dana Larose <ywg.dana@gmail.com>
2022-12-02 00:25:07 -0600

    A bit of tidying

--- 2022/Day02.fsx
+++ 2022/Day02.fsx
@@ -1,61 +1,45 @@
 open System.IO
 
 type Move = Rock | Paper | Scissors
-type Result = Win | Loss | Draw
 
-let toMove = function
+let move = function
     | 'A' | 'X' -> Rock
     | 'B' | 'Y' -> Paper
     | 'C' | 'Z' -> Scissors
     | _ -> failwith "Hmm this shouldn't happen"
 
-let toRes = function
-    | 'X' -> Loss
-    | 'Y' -> Draw
-    | 'Z' -> Win
-    | _ -> failwith "Hmm this shouldn't happen"
-    
-let round m1 m2 =
-    if m1 = m2 then Draw
-    elif m1 = Rock && m2 = Scissors then Win
-    elif m1 = Paper && m2 = Rock then Win
-    elif m1 = Scissors && m2 = Paper then Win
-    else Loss
-
-let findMove mv res =
-    match res with
-    | Draw -> mv
-    | Win -> match mv with
-             | Rock -> Paper
-             | Paper -> Scissors
-             | Scissors -> Rock
-    | Loss -> match mv with
-              | Rock -> Scissors
-              | Paper -> Rock
-              | Scissors -> Paper
-    
-let score move result =
-    match move with
+let value = function
     | Rock -> 1
     | Paper -> 2
     | Scissors -> 3
-    +
-    match result with
-    | Loss -> 0 
-    | Draw -> 3
-    | Win -> 6
+    
+let scorePt1 m1 m2 =
+    if m1 = m2 then 3 + value m1
+    elif m1 = Rock && m2 = Scissors then 6 + value m1
+    elif m1 = Paper && m2 = Rock then 6 + value m1
+    elif m1 = Scissors && m2 = Paper then 6 + value m1
+    else value m1
 
-let part1 (line:string) =
-    score (toMove line[2]) (round (toMove line[2]) (toMove line[0]))
-
-let part2 (line:string) =    
-    score (findMove (toMove line[0]) (toRes line[2])) (toRes line[2])
+let scorePt2 mv res =
+    match res with
+    | 'Y' -> 3 + value mv
+    | 'Z' -> match mv with
+             | Rock -> 6 + value Paper
+             | Paper -> 6 + value Scissors
+             | Scissors -> 6 + value Rock
+    | 'X' -> match mv with
+             | Rock -> value Scissors
+             | Paper -> value Rock
+             | Scissors -> value Paper
+    | _ -> failwith "Hmm this shouldn't happen"
 
 let lines = File.ReadAllLines("input_day02.txt")
 
-let p1 = lines |> Array.map(part1) |> Array.sum
+let p1 = lines |> Array.map(fun t -> scorePt1 (move t[2]) (move t[0]))
+               |> Array.sum
 printfn $"P1: {p1}"
 
-let p2 = lines |> Array.map(part2) |> Array.sum
+let p2 = lines |> Array.map(fun t -> scorePt2 (move t[0]) t[2])
+               |> Array.sum
 printfn $"P2: {p2}"
 
