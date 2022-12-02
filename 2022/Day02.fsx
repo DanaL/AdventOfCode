25260bae69a4b72c6ba5fbf335f92b1da9e6d021
Dana Larose <ywg.dana@gmail.com>
2022-12-01 23:42:06 -0600

    Some code cleanup

--- 2022/Day02.fsx
+++ 2022/Day02.fsx
@@ -12,26 +12,25 @@ let toMove = function
 let round m1 m2 =
     if m1 = m2 then Draw
     elif m1 = Rock && m2 = Scissors then Win
-    elif m1 = Rock && m2 = Paper then Loss
     elif m1 = Paper && m2 = Rock then Win
-    elif m1 = Paper && m2 = Scissors then Loss
     elif m1 = Scissors && m2 = Paper then Win
     else Loss
 
 let score move result =
-    let pts = match move with
-              | Rock -> 1
-              | Paper -> 2
-              | Scissors -> 3
+    match move with
+    | Rock -> 1
+    | Paper -> 2
+    | Scissors -> 3
+    +
     match result with
-    | Loss -> 0 + pts
-    | Draw -> 3 + pts
-    | Win -> 6 + pts
+    | Loss -> 0 
+    | Draw -> 3
+    | Win -> 6
+
+let part1 (line:string) =
+    score (toMove line[2]) (round (toMove line[2]) (toMove line[0]))
     
 let p1 = File.ReadAllLines("input_day02.txt")
-         |> Array.map(fun line -> let o = (toMove line[0])
-                                  let me = (toMove line[2])
-                                  let res = round me o
-                                  score me res)
+         |> Array.map(part1)                      
          |> Array.sum
 printfn $"P1: {p1}"
