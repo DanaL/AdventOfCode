af42c24abc4ae4c7c4f604175afb6810463cc468
Dana Larose <ywg.dana@gmail.com>
2022-12-03 10:10:01 -0600

    A bit of code tidying

--- 2022/Day03.fsx
+++ 2022/Day03.fsx
@@ -1,20 +1,20 @@
 open System.IO
 
 let score = function
-    | when c <= 'Z' -> c - int 'A' + 27
-    | _ -> int c - int 'a' + 1
+    | c when c <= 'Z' -> int c - int 'A' + 27
+    | c -> int c - int 'a' + 1
     
 let shared (line:string) =
-    let mid = line.Length/2 - 1
-    score (Set.intersect (Set.ofSeq line[0..mid]) (Set.ofSeq line[mid+1..])
+    let a,b = line |> List.ofSeq |> List.splitAt (line.Length/2)
+    score (Set.intersect (Set.ofSeq a) (Set.ofSeq b)
            |> Set.minElement)
 
 let lines = File.ReadAllLines("input_day03.txt")   
 let p1 = lines |> Array.map shared |> Array.sum
 printfn $"P1 {p1}"
 
-let badge (grp:string array) =
-    grp |> Array.map Set.ofSeq |> Array.reduce Set.intersect
+let badge grp =
+    grp |> Array.map Set.ofSeq |> Set.intersectMany
         |> Set.minElement |> score
     
 let p2 = lines |> Array.chunkBySize 3 |> Array.map badge |> Array.sum
