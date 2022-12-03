0e3078dfb24e07d1df443018fe2f888f1853f23e
Dana Larose <ywg.dana@gmail.com>
2022-12-03 09:50:46 -0600

    Oohh, I can just use s |> List.ofSeq instead of s.ToCharArray()

--- 2022/Day03.fsx
+++ 2022/Day03.fsx
@@ -1,13 +1,12 @@
 open System.IO
 
-let score c =
-    if c <= 'Z' then int c - int 'A' + 27 else int c - int 'a' + 1
-    
-let chSet (s:string) = s.ToCharArray() |> Set.ofArray
+let score = function
+    | when c <= 'Z' -> c - int 'A' + 27
+    | _ -> int c - int 'a' + 1
     
 let shared (line:string) =
     let mid = line.Length/2 - 1
-    score (Set.intersect (chSet line[0..mid]) (chSet line[mid+1..])
+    score (Set.intersect (Set.ofSeq line[0..mid]) (Set.ofSeq line[mid+1..])
            |> Set.minElement)
 
 let lines = File.ReadAllLines("input_day03.txt")   
@@ -15,7 +14,7 @@ let p1 = lines |> Array.map shared |> Array.sum
 printfn $"P1 {p1}"
 
 let badge (grp:string array) =
-    grp |> Array.map chSet |> Array.reduce Set.intersect
+    grp |> Array.map Set.ofSeq |> Array.reduce Set.intersect
         |> Set.minElement |> score
     
 let p2 = lines |> Array.chunkBySize 3 |> Array.map badge |> Array.sum
