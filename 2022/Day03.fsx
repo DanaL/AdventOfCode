b990f0edb9f66199b9ef7d51a95e4c38ed069e65
Dana Larose <ywg.dana@gmail.com>
2022-12-02 23:49:51 -0600

    2022 Day 3 part 2 complete

--- 2022/Day03.fsx
+++ 2022/Day03.fsx
@@ -1,13 +1,23 @@
 open System.IO
 
+let score c =
+    if c <= 'Z' then int c - int 'A' + 27 else int c - int 'a' + 1
+    
 let chSet (s:string) =
     s.ToCharArray() |> Set.ofArray
     
 let shared (line:string) =
     let mid = line.Length/2 - 1
-    let c = Set.intersect (chSet line[0..mid])  (chSet line[mid+1..])
-            |> Set.minElement
-    if c <= 'Z' then int c - int 'A' + 27 else int c - int 'a' + 1
+    score (Set.intersect (chSet line[0..mid])  (chSet line[mid+1..])
+           |> Set.minElement)
 
-let p1 = File.ReadAllLines("input_day03.txt") |> Array.map shared |> Array.sum
+let lines = File.ReadAllLines("input_day03.txt")   
+let p1 = lines |> Array.map shared |> Array.sum
 printfn $"P1 {p1}"
+
+let badge (grp:string array) =
+    grp |> Array.map chSet |> Array.reduce Set.intersect
+        |> Set.minElement |> score
+    
+let p2 = lines |> Array.chunkBySize 3 |> Array.map badge |> Array.sum
+printfn $"P2 {p2}"
