1c5dac968696af5d76b98db52045da68a3d4938f
Dana Larose <ywg.dana@gmail.com>
2022-12-03 23:38:10 -0600

    2022 Day 4 parts 1 and 2

---
+++ 2022/Day04.fsx
@@ -0,0 +1,24 @@
+open System.IO
+open System.Text.RegularExpressions
+
+let parse s =
+    let m = Regex.Match(s, "(\d+)-(\d+),(\d+)-(\d+)")
+    m.Groups[1].Value |> int, m.Groups[2].Value |> int,
+    m.Groups[3].Value |> int, m.Groups[4].Value |> int
+
+let contained line =
+    let a,b,c,d = parse line
+    (a >= c && b <= d) || (c >= a && d <= b)
+
+let btw a b c = a >= b && a <= c
+let overlap line =
+    let a,b,c,d = parse line
+    (btw a c d) || (btw b c d) || (btw c a b) || (btw d a b)
+        
+let test f =
+    File.ReadAllLines("input_day04.txt")
+    |> Array.map f |> Array.filter(fun c -> c) |> Array.length
+    
+printfn $"P1: {test contained}"
+printfn $"P2: {test overlap}"
+
