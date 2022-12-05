ed8da8cd5e309162f39e9039b721d1d7da2a65d7
Dana Larose <ywg.dana@gmail.com>
2022-12-05 10:06:43 -0600

    2022 Day 5 part 2 complete

--- 2022/Day05.fsx
+++ 2022/Day05.fsx
@@ -3,14 +3,12 @@ open System.Collections.Generic
 open System.IO
 open System.Text.RegularExpressions
 
-let lines = File.ReadAllLines("input_day05.txt")
-
 let calcCrates line =
      line
      |> Seq.mapi(fun i c -> if c >= 'A' && c <= 'Z' then Some (c, i/4 + 1) else None)
      |> Seq.choose id
 
-let newStacks =
+let newStacks (lines: string array) =
     let stacks = new Dictionary<int, Stack<char>>()
     let initStacks ch (col:int) =
         if not (stacks.ContainsKey col) then stacks.Add(col, new Stack<char>())        
@@ -20,20 +18,28 @@ let newStacks =
           |> Array.map calcCrates |> Seq.collect id |> Seq.rev
           |> Seq.toList |> List.iter(fun (ch, col) -> initStacks ch col)
     stacks
-    
-let doMovePt1 amt src dest (stacks:Dictionary<int, Stack<char>>) =
-    for _ in 1..amt do
-        stacks[dest].Push(stacks[src].Pop())
 
-let sim (lines: string array) mv =
-    let stacks = newStacks
+let sim mv =
+    let lines = File.ReadAllLines("input_day05.txt")
+    let s = newStacks lines
     lines |> Array.filter(fun l -> l.StartsWith("move"))
           |> Array.iter(fun l -> let m = Regex.Match(l, "move (\d+) from (\d+) to (\d+)")
                                  mv (m.Groups[1].Value |> int)
                                     (m.Groups[2].Value |> int)
-                                    (m.Groups[3].Value |> int) stacks)
+                                    (m.Groups[3].Value |> int) s)
+    s.Keys |> Seq.sort |> Seq.map(fun k -> s[k].Pop()) |> String.Concat
 
-    stacks.Keys |> Seq.sort |> Seq.map(fun k -> stacks[k].Pop()) |> String.Concat
-
-let p1 = sim lines doMovePt1
+let doMovePt1 amt src dest (stacks:Dictionary<int, Stack<char>>) =
+    for _ in 1..amt do
+        stacks[dest].Push(stacks[src].Pop())
+let p1 = sim doMovePt1
 printfn $"P1: {p1}"
+
+let doMovePt2 amt src dest (stacks:Dictionary<int, Stack<char>>) =    
+    let tmp = new Stack<char>()
+    for _ in 1..amt do
+        tmp.Push(stacks[src].Pop())
+    for _ in 1..amt do
+        stacks[dest].Push(tmp.Pop())
+let p2 = sim doMovePt2
+printfn $"P2: {p2}"
