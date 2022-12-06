b41e7e9d73e76ea1d2583e976dfcbc09826d060b
Dana Larose <ywg.dana@gmail.com>
2022-12-06 03:10:14 -0600

    Code tidying

--- 2022/Day06.fsx
+++ 2022/Day06.fsx
@@ -1,11 +1,8 @@
-// Seq.windowed 4 piped to:
-// fold so that I can pass an indeox?
-// or just a recursive function?
-
-let s = System.IO.File.ReadAllText("input_day06.txt")
 let seek s size =
-    s |> Seq.windowed size
-      |> Seq.takeWhile(fun w -> let a = Array.distinct w
-                                a.Length <> 4)
-      |> Seq.length + size                                
+    (s |> Seq.windowed size
+       |> Seq.takeWhile(fun w -> (Array.distinct w).Length <> size)
+       |> Seq.length) + size
+let s = System.IO.File.ReadAllText("input_day06.txt")    
 printfn $"P1: {seek s 4}"
+printfn $"P1: {seek s 14}"
+
