a50f67e1cf359cb00a29ab3167bbd782fa4f909f
Dana Larose <ywg.dana@gmail.com>
2022-12-08 10:55:01 -0600

    2022 Day 8 part 2 complete

--- 2022/Day08.fsx
+++ 2022/Day08.fsx
@@ -6,27 +6,36 @@ let forest = lines |> String.concat ""
 let index r c = r * width + c
 let inBounds r c = r >= 0 && r < height && c >= 0 && c < width
 
-let rec blocked dr dc (forest:string) h r c =    
+let rec blocked (forest:string) h r c dr dc =    
     if not (inBounds r c) then false
     elif forest[index r c] >= h then true
-    else blocked dr dc forest h (r + dr) (c + dc)
-    
-let blockedWest = blocked 0 -1
-let blockedEast = blocked 0 1
-let blockedNorth = blocked -1 0
-let blockedSouth = blocked 1 0
+    else blocked forest h (r + dr) (c + dc) dr dc
 
 let visible (forest:string) r c =
     let t = forest[index r c]   
-    not (blockedWest forest t r (c-1) && blockedEast forest t r (c+1)
-         && blockedNorth forest t (r-1) c && blockedSouth forest t (r+1) c)
+    not (blocked forest t r (c-1) 0 -1 && blocked forest t r (c+1) 0 1
+         && blocked forest t (r-1) c -1 0 && blocked forest t (r+1) c 1 0)
 
+let pts = seq { for r in 1..height-2 do
+                    for c in 1..width-2 do
+                        yield r, c }
+          |> List.ofSeq
+          
 let p1 =
-    (seq { for r in 1..height-2 do
-               for c in 1..width-2 do
-                  yield r, c }
-     |> Seq.map(fun (r,c) -> visible forest r c)             
-     |> Seq.filter(fun p -> p)
-     |> Seq.length) + 4 * height
+     (pts
+      |> List.map(fun (r,c) -> visible forest r c)             
+      |> List.filter(fun p -> p)
+      |> List.length) + 4 * height - 4
 printfn $"P1: {p1}"
 
+let rec countTrees (forest:string) h r c dr dc v =
+    if not (inBounds r c) then v
+    elif forest[index r c] >= h then v + 1
+    else countTrees forest h (r + dr) (c + dc) dr dc v + 1
+
+let scenicity (forst:string) r c =
+    let t = forest[index r c]
+    (countTrees forest t (r-1) c -1 0 0) * (countTrees forest t (r+1) c 1 0 0)
+       * (countTrees forest t r (c-1) 0 -1 0) * (countTrees forest t r (c+1) 0 1 0)
+let p2 = pts |> List.map(fun (r,c) -> scenicity forest r c) |> List.max
+printfn $"P2: {p2}"
