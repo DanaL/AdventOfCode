7ac6db3ce8a68a83a0c1bcb4092e8b87ce4c52f1
Dana Larose <ywg.dana@gmail.com>
2022-12-07 21:15:40 -0600

    Rewrote 2022 Day 7 part 1 completely and avoided trying to build an actual tree structure. Lots of mutable values tho :/

--- 2022/Day07.fsx
+++ 2022/Day07.fsx
@@ -1,74 +1,29 @@
 open System.Collections.Generic
 open System.IO
 
-type File = { Name:string; Size: int }
-type Folder = {
-    Name:string
-    mutable Parent:Folder option
-    mutable Files:File list
-    mutable Children:Folder list
-}
+type Folder = { mutable Size:int; mutable Folders:string list }
+let lines = new Queue<string>(File.ReadAllLines("input_day07.txt")
+                              |> Array.skip(1)
+                              |> Array.filter(fun l -> l[0..3] <> "$ ls"))
 
-let rec changeDir curr name =
-    if name = ".." then
-        match curr.Parent with
-        | Some p -> p
-        | None -> failwith "Hmm this shouldn't happen"
-    else
-        curr.Children |> List.find(fun n -> n.Name = name)
+let mutable folders = new Stack<Folder>()
+let mutable visited = List.empty
+let mutable root = { Size=0; Folders=List.empty }
+folders.Push(root)
 
-let newFolder name = { Name=name; Parent=None; Files=list.Empty; Children=list.Empty }
-let addFile (folder:Folder) file = folder.Files <- file::folder.Files
-let addFolder folder child =
-    child.Parent <- Some folder
-    folder.Children <- child::folder.Children
+while lines.Count > 0 do
+    let line = lines.Dequeue()
+    let pieces = line.Split(' ')
+    match pieces[0] with
+    | "dir" -> let n = folders.Peek()
+               folders.Peek().Folders <- pieces[1]::n.Folders
+    | "$" -> match pieces[2] with
+             | ".." -> visited <- folders.Pop()::visited
+                       folders.Peek().Size <- folders.Peek().Size + visited[0].Size
+             | _ -> folders.Push({ Size=0; Folders=List.empty})
+    | _ -> folders.Peek().Size <- folders.Peek().Size + (int <| pieces[0])
 
-let mutable root = newFolder "/"
-let lines = new Queue<string>(File.ReadAllLines("input_day07.txt")[1..])
-
-let rec printTree node depth =    
-    printf $"{node.Name.PadLeft(depth * 4, ' ')}: "
-    for file in node.Files do
-        printf $"{file.Name} "
-    printfn ""
-    for dir in node.Children do
-        printTree dir (depth+1)
-
-let rec buildTree (root:Folder) (lines:Queue<string>) =
-    while lines.Count > 0 do
-        let line = lines.Dequeue()
-        let pieces = line.Split(' ')
-        if pieces[0] = "dir" then addFolder root (newFolder pieces[1])
-        elif pieces[0] = "$" then
-            if pieces[1] = "cd" then
-                let c = changeDir root pieces[2]
-                buildTree c lines
-            // $ ls doesn't really do anything in part 1
-        else
-            let f = { Name=pieces[1]; Size= (int <| pieces[0]) }
-            addFile root f
-        
-let rec calcFolderSizes node =
-    let size = node.Files |> List.sumBy(fun f -> f.Size)
-    if node.Children.Length = 0 then
-        [ size ]
-    else
-        let childSizes = node.Children |>  List.map(fun c -> calcFolderSizes c) |> List.concat
-        //(size + childSizes |> List.sum)::childSizes
-        let total = childSizes |> List.sum
-        (size + total)::childSizes
-
-let rec calc2 node =
-    let size = node.Files |> List.sumBy(fun f -> f.Size)    
-    let sizes = if node.Children.Length = 0 then
-                    [ size ]
-                else
-                    size::(node.Children |> List.map(fun c -> calc2 c) |> List.concat)
-    sizes |> List.filter(fun s -> s <= 100_000)
-        
-buildTree root lines
-printTree root 0
-let sizes = calc2 root
-let p1 = sizes |> List.sum
+let p1 = visited |> List.filter(fun f -> f.Size <= 100_000)
+                 |> List.sumBy(fun f -> f.Size)
 printfn $"P1: {p1}"
 
