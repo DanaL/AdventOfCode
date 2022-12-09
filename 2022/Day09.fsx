b590bd46f8e9b1a95ad78222c0ed4f316c4001a6
Dana Larose <ywg.dana@gmail.com>
2022-12-09 15:10:10 -0600

    2022 Day 9 part 1 complete

--- 2022/Day09.fsx
+++ 2022/Day09.fsx
@@ -22,6 +22,13 @@ let dist x0 y0 x1 y1 =
     let xd = pown (x0 - x1) 2
     let yd = pown (y0 - y1) 2
     int <| sqrt(float xd + float yd)
+
+// find the diaganol move that will catch the tail up to the
+// N, S, E, or W neighbour of the head
+let overlapNeighbour hr hc tr tc =
+    let neighboursHead = Set.ofList [(hr-1,hc); (hr+1,hc); (hr,hc-1); (hr,hc+1)]
+    let neighboursTail = Set.ofList [(tr-1,tc-1); (tr-1,tc+1); (tr+1,tc-1); (tr+1,tc+1)]
+    Set.intersect neighboursHead neighboursTail |> Set.maxElement
     
 let move state mv =
     let dr, dc = match mv with
@@ -30,13 +37,22 @@ let move state mv =
                  | Left -> 0, -1
                  | Right -> 0, 1
     let hr, hc = state.Head
-    { state with Head=hr+dr,hc+dc }
+    let tr, tc = state.Tail
+    let nhr, nhc = hr+dr, hc+dc
+    let ntr, ntc =
+        if dist nhr nhc tr tc > 1 then
+            // we need to move the tail
 
+            // if they are on the same row or col just follow, otherwise
+            // it's a diaganol move
+            if tr = nhr || tc = nhc then tr+dr, tc+dc
+            else overlapNeighbour nhr nhc tr tc
+        else
+            tr, tc
+    { Head=nhr,nhc; Tail=ntr,ntc; Visited = state.Visited.Add(ntr,ntc) }
+
+let q = fetchInput()
 let s0 = { Head= 0,0; Tail= 0,0; Visited= Set.empty.Add(0,0) }
-let s1 = move s0 Right
-printfn $"%A{s0}"
-printfn $"%A{s1}"
 
-printfn $"{dist 2 1 0 1}"
-//let q = fetchInput()
-//printfn $"%A{q}"
+let res = q |> Array.fold(fun s mv -> move s mv) s0
+printfn $"P1: {res.Visited.Count}"
