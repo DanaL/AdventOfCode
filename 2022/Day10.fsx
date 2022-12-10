323672aac41b41e928a9cdfce53d5ce521e538c1
Dana Larose <ywg.dana@gmail.com>
2022-12-10 06:01:42 -0600

    Day 10 part 2 complete

--- 2022/Day10.fsx
+++ 2022/Day10.fsx
@@ -1,6 +1,6 @@
+open System
+open System.IO
 open System.Collections.Generic
-
-let lines = System.IO.File.ReadAllLines("input_day10.txt")
 
 let eval (line:string) (stack:Stack<int*int>) =   
     let cycle,x = stack.Peek()
@@ -10,15 +10,21 @@ let eval (line:string) (stack:Stack<int*int>) =
         stack.Push(cycle+2,x+v)
 
 let stack = new Stack<int*int>()
-stack.Push(0,1)
-let l = "addx 3"
+stack.Push(1,1)
 
-lines |> Array.iter(fun line -> eval line stack)
+File.ReadAllLines("input_day10.txt") |> Array.iter(fun l -> eval l stack)
 let cycles = stack |> Map.ofSeq
 
-let p1 = cycles[19] * 20 + cycles[59] * 60 + cycles[99] * 100 +
-                cycles[139] * 140 + cycles[179] * 180 + cycles[219] * 220
-
+let p1 = cycles[20] * 20 + cycles[60] * 60 + cycles[100] * 100 +
+                cycles[140] * 140 + cycles[180] * 180 + cycles[220] * 220
 printfn $"P1: {p1}"             
 
-        
+let pixels =
+    cycles |> Seq.map(fun kvp -> kvp.Key - 1,kvp.Value)
+           |> Seq.map(fun (c, x) -> let p = c % 40
+                                    if p >= x - 1 && p <= x + 1 then '#'
+                                    else ' ')
+           |> Seq.chunkBySize 40
+           |> Seq.map(fun row -> row |> System.String.Concat)
+           |> Seq.iter(System.Console.WriteLine)
+
