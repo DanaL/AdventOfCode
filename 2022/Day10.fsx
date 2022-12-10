7c381896d8accbf087d0210dfcfb2a53c66eadfc
Dana Larose <ywg.dana@gmail.com>
2022-12-10 07:13:31 -0600

    2022 Day 10 -- List.rev call was unnecessary

--- 2022/Day10.fsx
+++ 2022/Day10.fsx
@@ -4,12 +4,11 @@ open System.IO
 let eval (line:string) x =
     match line with
     | "noop" -> [ x ]
-    | _ -> [ x + (line[5..] |> int); x ]
+    | _ -> [ x; x + (line[5..] |> int) ]
 
 let cycles = File.ReadAllLines("input_day10.txt")
-             |> Array.fold(fun arr line -> let n = eval line (arr |> List.head)
-                                           n @ arr) [ 1 ]
-             |> List.rev
+             |> Array.fold(fun arr line -> let n = eval line (arr |> List.last)
+                                           arr @ n) [ 1 ]
 
 let p1 = cycles[19] * 20 + cycles[59] * 60 + cycles[99] * 100 +
                 cycles[139] * 140 + cycles[179] * 180 + cycles[219] * 220
