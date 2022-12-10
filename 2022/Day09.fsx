5fe7d8be1f1ef49e5975a31cf3f665160b1254fe
Dana Larose <ywg.dana@gmail.com>
2022-12-09 18:55:19 -0600

    2022 Day 9 part 2 working

--- 2022/Day09.fsx
+++ 2022/Day09.fsx
@@ -24,17 +24,18 @@ let dist x0 y0 x1 y1 =
     int <| sqrt(float xd + float yd)
 
 let overlapMoveDiag hr hc tr tc =
-    let neighboursHead = Set.ofList [(hr-1,hc); (hr+1,hc); (hr,hc-1); (hr,hc+1)]
+    let neighboursHead = Set.ofList [(hr-1,hc); (hr+1,hc); (hr,hc-1); (hr,hc+1);
+                                     (hr-1,hc-1); (hr-1,hc+1); (hr+1,hc-1); (hr+1,hc+1)]
     let neighboursTail = Set.ofList [(tr-1,tc-1); (tr-1,tc+1); (tr+1,tc-1); (tr+1,tc+1)]
     Set.intersect neighboursHead neighboursTail |> Set.maxElement
 
 let overlapMove hr hc tr tc =
     let neighboursHead = Set.ofList [(hr-1,hc); (hr+1,hc); (hr,hc-1); (hr,hc+1)]
-    let neighboursTail = Set.ofList [(tr-1,tc); (tr+1,tc); (tr,tc-1); (tr,tc+1)]
+    let neighboursTail = Set.ofList [(tr-1,tc); (tr+1,tc); (tr,tc-1); (tr,tc+1)]    
     Set.intersect neighboursHead neighboursTail |> Set.maxElement
     
 let rec moveTails toR toC (tail:(int*int) list) =
-    let kr, kc = tail[0]
+    let kr, kc = tail[0]    
     let ntr, ntc =
         if dist toR toC kr kc > 1 then
             // Okay, we need to move this tail segment
@@ -52,7 +53,6 @@ let rec moveTails toR toC (tail:(int*int) list) =
         (ntr,ntc)::tail'
         
 let move state mv =
-    printfn "flag"
     let dr, dc = match mv with
                  | Up -> -1, 0
                  | Down -> 1, 0
@@ -72,3 +72,4 @@ let s1 = { Knots= [0,0; 0,0; 0,0; 0,0; 0,0; 0,0; 0,0; 0,0; 0,0; 0,0]
            Visited= Set.empty.Add(0,0) }
 let res2 = lines |> Array.fold(fun s mv -> move s mv) s1
 printfn $"P2: {res2.Visited.Count}"
+
