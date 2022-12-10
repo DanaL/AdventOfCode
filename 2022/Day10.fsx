ed680db56b5d9dc6afe1f673007be88c23e7f26c
Dana Larose <ywg.dana@gmail.com>
2022-12-10 07:18:40 -0600

    2022 day 10 -- cleaned up my if stmt for picking char to draw

--- 2022/Day10.fsx
+++ 2022/Day10.fsx
@@ -14,9 +14,7 @@ let p1 = cycles[19] * 20 + cycles[59] * 60 + cycles[99] * 100 +
                 cycles[139] * 140 + cycles[179] * 180 + cycles[219] * 220
 printfn $"P1: {p1}"             
 
-cycles |> Seq.mapi(fun c x -> let p = c % 40
-                              if p >= x - 1 && p <= x + 1 then '#'
-                              else ' ')
+cycles |> Seq.mapi(fun c x -> if abs(c % 40 - x) < 2 then '#' else ' ')
        |> Seq.chunkBySize 40
        |> Seq.map String.Concat
        |> Seq.iter Console.WriteLine
