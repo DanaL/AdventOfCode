bbaf44e19309a3eae903f785ab4c78bc75adf839
Dana Larose <ywg.dana@gmail.com>
2022-12-10 06:33:07 -0600

    2022 day 10: Replaced Stack with a list

--- 2022/Day10.fsx
+++ 2022/Day10.fsx
@@ -1,30 +1,27 @@
 open System
 open System.IO
-open System.Collections.Generic
 
-let eval (line:string) (stack:Stack<int*int>) =   
-    let cycle,x = stack.Peek()
-    stack.Push(cycle+1,x)
+let eval (line:string) (cycle,x) =
     if line <> "noop" then
         let v = line.Split(' ')[1] |> int
-        stack.Push(cycle+2,x+v)
+        [ cycle+2,x+v; cycle+1,x ]
+    else
+        [ cycle+1,x ]
 
-let stack = new Stack<int*int>()
-stack.Push(1,1)
-
-File.ReadAllLines("input_day10.txt") |> Array.iter(fun l -> eval l stack)
-let cycles = stack |> Map.ofSeq
+let cycles = File.ReadAllLines("input_day10.txt")
+             |> Array.fold(fun arr line -> let n = eval line (arr |> List.head)
+                                           n @ arr) [1,1]
+             |> List.rev |> Map.ofList
 
 let p1 = cycles[20] * 20 + cycles[60] * 60 + cycles[100] * 100 +
                 cycles[140] * 140 + cycles[180] * 180 + cycles[220] * 220
 printfn $"P1: {p1}"             
 
-let pixels =
-    cycles |> Seq.map(fun kvp -> kvp.Key - 1,kvp.Value)
-           |> Seq.map(fun (c, x) -> let p = c % 40
-                                    if p >= x - 1 && p <= x + 1 then '#'
-                                    else ' ')
-           |> Seq.chunkBySize 40
-           |> Seq.map(fun row -> row |> System.String.Concat)
-           |> Seq.iter(System.Console.WriteLine)
+cycles |> Seq.map(fun kvp -> kvp.Key - 1,kvp.Value)
+       |> Seq.map(fun (c, x) -> let p = c % 40
+                                if p >= x - 1 && p <= x + 1 then '#'
+                                else ' ')
+       |> Seq.chunkBySize 40
+       |> Seq.map(fun row -> row |> System.String.Concat)
+       |> Seq.iter(System.Console.WriteLine)
 
