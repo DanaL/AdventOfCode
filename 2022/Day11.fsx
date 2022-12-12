8a52257e3698b063a464d86f03d1c83b6efa979c
Dana Larose <ywg.dana@gmail.com>
2022-12-11 20:31:25 -0600

    Finished Day 11 part 2

--- 2022/Day11.fsx
+++ 2022/Day11.fsx
@@ -2,58 +2,67 @@ open System.Collections.Generic
 open System.IO
 open System.Text.RegularExpressions
 
-let inspections = new Queue<int>()
+let mutable inspections = [| 0UL; 0UL; 0UL; 0UL; 0UL; 0UL; 0UL; 0UL |]
 
-let monkey0 (items: int list) =
-    seq { 1..items.Length } |> Seq.iter(fun _ -> inspections.Enqueue(0))
-    items |> List.map(fun x -> (x * 13) / 3)
-          |> List.map(fun x -> if x % 19 = 0 then x, 6 else x, 7)
-let monkey1 (items: int list) =
-    seq { 1..items.Length } |> Seq.iter(fun _ -> inspections.Enqueue(1))
-    items |> List.map(fun x -> (x + 3) / 3)
-          |> List.map(fun x -> if x % 2 = 0 then x, 5 else x, 4)
-let monkey2 (items: int list) =
-    seq { 1..items.Length } |> Seq.iter(fun _ -> inspections.Enqueue(2))
-    items |> List.map(fun x -> (x + 6) / 3)
-          |> List.map(fun x -> if x % 13 = 0 then x, 4 else x, 1)
-let monkey3 (items: int list) =
-    seq { 1..items.Length } |> Seq.iter(fun _ -> inspections.Enqueue(3))
-    items |> List.map(fun x -> (x + 2) / 3)
-          |> List.map(fun x -> if x % 5 = 0 then x, 6 else x, 0)
-let monkey4 (items: int list) =
-    seq { 1..items.Length } |> Seq.iter(fun _ -> inspections.Enqueue(4))
-    items |> List.map(fun x -> (x * x) / 3)
-          |> List.map(fun x -> if x % 7 = 0 then x, 5 else x, 3)
-let monkey5 (items: int list) =
-    seq { 1..items.Length } |> Seq.iter(fun _ -> inspections.Enqueue(5))
-    items |> List.map(fun x -> (x + 4) / 3)
-          |> List.map(fun x -> if x % 11 = 0 then x, 3 else x, 0)
-let monkey6 (items: int list) =
-    seq { 1..items.Length } |> Seq.iter(fun _ -> inspections.Enqueue(6))
-    items |> List.map(fun x -> (x * 7) / 3)
-          |> List.map(fun x -> if x % 17 = 0 then x, 2 else x, 7)
-let monkey7 (items: int list) =
-    seq { 1..items.Length } |> Seq.iter(fun _ -> inspections.Enqueue(7))
-    items |> List.map(fun x -> (x + 7) / 3)
-          |> List.map(fun x -> if x % 3 = 0 then x, 2 else x, 1)
+let LCM = 9699690UL;;
 
-let mutable inventories =
-    File.ReadAllText("input_day11.txt").Split("\n\n")
-    |> Array.map(fun txt -> txt.Split("\n")[1])
-    |> Array.map(fun line -> Regex.Split(line, "\D+")[1..] |> Array.map int |> List.ofArray)
-    
+let monkey0 (items: uint64 list) destress =
+    inspections[0] <- inspections[0] + (items.Length |> uint64)
+    items |> List.map(fun x -> destress (x * 13UL) % LCM)
+          |> List.map(fun x -> if x % 19UL = 0UL then x, 6 else x, 7)
+let monkey1 (items: uint64 list) destress =
+    inspections[1] <- inspections[1] + (items.Length |> uint64)  
+    items |> List.map(fun x -> destress (x + 3UL) % LCM)
+          |> List.map(fun x -> if x % 2UL = 0UL then x, 5 else x, 4)
+let monkey2 (items: uint64 list) destress =
+    inspections[2] <- inspections[2] + (items.Length |> uint64)
+    items |> List.map(fun x -> destress (x + 6UL) % LCM)
+          |> List.map(fun x -> if x % 13UL = 0UL then x, 4 else x, 1)
+let monkey3 (items: uint64 list) destress =
+    inspections[3] <- inspections[3] + (items.Length |> uint64)   
+    items |> List.map(fun x -> destress (x + 2UL) % LCM) 
+          |> List.map(fun x -> if x % 5UL = 0UL then x, 6 else x, 0)
+let monkey4 (items: uint64 list) destress =
+    inspections[4] <- inspections[4] + (items.Length |> uint64)       
+    items |> List.map(fun x -> destress (x * x) % LCM)
+          |> List.map(fun x -> if x % 7UL = 0UL then x, 5 else x, 3)
+let monkey5 (items: uint64 list) destress =
+    inspections[5] <- inspections[5] + (items.Length |> uint64)    
+    items |> List.map(fun x -> destress (x + 4UL) % LCM)
+          |> List.map(fun x -> if x % 11UL = 0UL then x, 3 else x, 0)
+let monkey6 (items: uint64 list) destress =
+    inspections[6] <- inspections[6] + (items.Length |> uint64)       
+    items |> List.map(fun x -> destress (x * 7UL) % LCM)
+          |> List.map(fun x -> if x % 17UL = 0UL then x, 2 else x, 7)
+let monkey7 (items: uint64 list) destress =
+    inspections[7] <- inspections[7] + (items.Length |> uint64)        
+    items |> List.map(fun x -> destress (x + 7UL) % LCM)
+          |> List.map(fun x -> if x % 3UL = 0UL then x, 2 else x, 1)
+
 let monkeys = [| monkey0; monkey1; monkey2; monkey3; monkey4;
                  monkey5; monkey6; monkey7 |]
 
-for _ in 1..20 do
-    for m in 0..7 do
-        let res = monkeys[m] inventories[m]        
-        res |> List.iter(fun (v, nm) -> inventories[nm] <- v::inventories[nm])
-        inventories[m] <- []
+let calc rounds stressFun = 
+    let mutable inventories =
+        File.ReadAllText("input_day11.txt").Split("\n\n")
+        |> Array.map(fun txt -> txt.Split("\n")[1])
+        |> Array.map(fun line -> Regex.Split(line, "\D+")[1..] |> Array.map uint64 |> List.ofArray)
 
-let monkeyBusiness = inspections |> Seq.groupBy id
-                                 |> Seq.map(fun (i, items) -> items |> List.ofSeq |> List.length)
-                                 |> List.ofSeq |> List.sortDescending
-printfn $"P1: {monkeyBusiness[0] * monkeyBusiness[1]}"
-                       
-                       
+    for _ in 1..rounds do
+        for m in 0..7 do
+            let res = monkeys[m] inventories[m] stressFun
+            res |> List.iter(fun (v, nm) -> inventories[nm] <- v::inventories[nm])
+            inventories[m] <- []
+
+    inspections <- inspections |> Array.sortDescending
+    inspections[0] * inspections[1]
+
+let dBy3 x = x / 3UL
+let p1 = calc 20 dBy3
+printfn $"P1: {p1}"
+
+inspections <- [| 0UL; 0UL; 0UL; 0UL; 0UL; 0UL; 0UL; 0UL |]
+let d x = x
+let p2 = calc 10_000 d
+printfn $"P2: {p2}"
+
