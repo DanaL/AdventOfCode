ea8bc85c56f756a7e01f9124b51ee46c61a153e0
Dana Larose <ywg.dana@gmail.com>
2022-12-12 16:58:13 -0600

    Working on 2022 Day 12 part 2

--- 2022/Day12.fsx
+++ 2022/Day12.fsx
@@ -15,9 +15,14 @@ let graph = File.ReadAllLines("input_day12.txt")
             |> Array.mapi(fun r s -> s |> Array.map(fun (c, ch) -> (r, c), toInt ch))
             |> Array.concat |> Map.ofArray
 
+// For part 2, I want to work in reverse. So to find adjacent squares,
+// I need to see, for whichever sq I am looking it, if it was adjacent
+// to its neighbour in part 1. (Because the graph edges aren't
+// necessarily bidirectional)
+
 let isVertex (graph:Graph) v p =
     match graph.ContainsKey(p) with
-    | true -> if v+1 >= graph[p] then Some (p)
+    | true -> if v+1 >= graph[p] then Some(p)
               else None
     | false -> None
     
@@ -27,26 +32,35 @@ let adj (graph:Graph) (r, c) =
                                      let v = graph[r, c]
                                      isVertex graph v (nr,nc))
 
-let shortestPath (graph:Graph) start goal =
+let shortestPath (graph:Graph) start goalElevation =
     let distances = new Dictionary<int*int, int>()
     let keys = graph.Keys |> List.ofSeq
     keys |> List.iter(fun k -> distances.Add(k, Int32.MaxValue))
     distances[start] <- 0
     let visited = new HashSet<int*int>()
 
+    let mutable goal = start
+    let mutable cont = true
     let mutable v = start
-    while not <| visited.Contains v do        
-        ignore(visited.Add(v))
-        adj graph v |> List.iter(fun c -> if distances[c] > distances[v] + 1 then
-                                              distances[c] <- distances[v] + 1)
-        let mutable dist = Int32.MaxValue
-        keys |> List.iter(fun j -> if not <| visited.Contains(j) && dist > distances[j] then
-                                       dist <- distances[j]
-                                       v <- j)
+    while cont do
+        if graph[v] = goalElevation then
+            goal <- v
+            cont <- false
+        else            
+            ignore(visited.Add(v))
+            adj graph v |> List.iter(fun c -> if distances[c] > distances[v] + 1 then
+                                                  distances[c] <- distances[v] + 1)
+            let mutable dist = Int32.MaxValue
+            keys |> List.iter(fun j -> if not <| visited.Contains(j) && dist > distances[j] then
+                                           dist <- distances[j]
+                                           v <- j)
     
     distances[goal]
 
 let start = graph.Keys |> Seq.find(fun k -> graph[k] = -1)
-let goal = graph.Keys |> Seq.find(fun k -> graph[k] = 26)
-let p1 = shortestPath graph start goal
+let peak = graph.Keys |> Seq.find(fun k -> graph[k] = 26)
+
+//printfn $"{peak}"
+//printfn $"%A{adj graph (20,60)}"
+let p1 = shortestPath graph peak -1
 printfn $"P1: {p1}"
