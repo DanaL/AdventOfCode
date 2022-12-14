876c537b124f20271a9c611e4dd586c87b5e1775
Dana Larose <ywg.dana@gmail.com>
2022-12-14 11:31:28 -0600

    2022 Day 14 part 1 complete

--- 2022/Day14.fsx
+++ 2022/Day14.fsx
@@ -1,8 +1,10 @@
 open System.IO
 open System.Text.RegularExpressions
 
+exception IntoTheAbyss
 type Tile = Stone | Sand
-
+type Tiles = Map<int*int,Tile>
+    
 let parse line =
     Regex.Split(line, "\D+")
     |> Array.map int
@@ -29,13 +31,52 @@ let calcRock (a,b) =
                  |> List.ofSeq
     coords @ [b]
 
-let tiles =
+let coords =
     File.ReadAllLines("input_day14.txt")
     |> Array.map parse
     |> List.ofArray
     |> List.map(fun line -> line |> List.map calcRock |> List.concat |> List.distinct)
     |> List.concat |> List.distinct
-    |> List.map(fun p -> p, Stone)
-    |> Map.ofList
+    
+let maxY = (coords |> List.map (fun (_,y) -> y) |> List.max) + 2
+let mutable part1 = true
 
-printfn $"%A{tiles}"
+let dump (tiles:Tiles) =    
+    for r in 0..maxY do
+        let mutable s = ""
+        for c in 490..510 do            
+            s <- s + if not(tiles |> Map.containsKey (c,r)) then "."
+                     else if tiles[c,r] = Stone then "#"
+                     else "o"
+        printfn $"{s}"
+    printfn ""
+    
+let rec drop (tiles:Tiles) x y =
+    if part1 && y = maxY then
+        raise IntoTheAbyss
+    elif not (tiles |> Map.containsKey (x, y+1)) then
+        drop tiles x (y+1)
+    elif not (tiles |> Map.containsKey (x-1, y+1)) then
+        drop tiles (x-1) (y+1)
+    elif not (tiles |> Map.containsKey (x+1, y+1)) then
+        drop tiles (x+1) (y+1)
+    else
+        x,y
+
+printfn $"{maxY}"
+let p1 =
+    let mutable tiles = coords |> List.map(fun p -> p, Stone)
+                               |> Map.ofList
+
+    let mutable c = 0
+    let mutable go = true
+    while go do
+        try
+            let r = drop tiles 500 0
+            tiles <- tiles |> Map.add r Sand
+            //dump tiles
+        with
+        | :? IntoTheAbyss -> go <- false
+    tiles |> Map.values |> Seq.filter(fun t -> t = Sand) |> Seq.length
+printfn $"P1: {p1}"    
+
