1b06f921d19bfeb50a9b15b65793f630dfedd600
Dana Larose <ywg.dana@gmail.com>
2022-12-14 10:10:29 -0600

    Parsing input into a Map containing the locations of all the Stone tiles

--- 2022/Day14.fsx
+++ 2022/Day14.fsx
@@ -1,4 +1,7 @@
+open System.IO
 open System.Text.RegularExpressions
+
+type Tile = Stone | Sand
 
 let parse line =
     Regex.Split(line, "\D+")
@@ -6,7 +9,33 @@ let parse line =
     |> Array.chunkBySize 2
     |> Array.map(fun a -> a[0],a[1])
     |> Array.pairwise
+    |> List.ofArray
     
-let a = parse "503,4 -> 502,4 -> 502,9 -> 494,9"
+let calcRock (a,b) =
+    let calcDelta a b =
+        if a = b then 0
+        elif a > b then -1
+        else 1
+    let xa, ya = a
+    let xb, yb = b
+    let xd = calcDelta xa xb
+    let yd = calcDelta ya yb
 
-printfn $"%A{a}"
+    let coords = seq { let mutable x,y = a
+                       while (x,y) <> b do
+                       yield x,y
+                       x <- x + xd
+                       y <- y + yd }
+                 |> List.ofSeq
+    coords @ [b]
+
+let tiles =
+    File.ReadAllLines("input_day14.txt")
+    |> Array.map parse
+    |> List.ofArray
+    |> List.map(fun line -> line |> List.map calcRock |> List.concat |> List.distinct)
+    |> List.concat |> List.distinct
+    |> List.map(fun p -> p, Stone)
+    |> Map.ofList
+
+printfn $"%A{tiles}"
