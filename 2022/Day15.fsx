ce6eddc6456ca8de144108413112f376ae9bed14
Dana Larose <ywg.dana@gmail.com>
2022-12-16 13:02:12 -0600

    2022 Day 15 part 2 complete

--- 2022/Day15.fsx
+++ 2022/Day15.fsx
@@ -34,8 +34,8 @@ let sensors = File.ReadAllLines("input_day15.txt")
 let beacons = sensors |> Array.map(fun s -> s.BeaconX, s.BeaconY)
                       |> List.ofArray |> List.distinct |> Set.ofList
 
-let maxDim = 20L // 4_000_000L
-let targetRow = 10L // 2000000L
+let maxDim = 4_000_000L
+let targetRow = 2000000L
 
 let part1() =
     let scanned = sensors |> Array.map(fun s -> scan s targetRow)
@@ -50,122 +50,33 @@ let skipTo sensor row =
 let inRange sensor x y =
     taxi sensor.X sensor.Y x y <= sensor.D
 
-let scanned sensors x y =
-    let res = sensors |> Array.map(fun s -> inRange s x y)
-                      |> Array.tryFind(fun r -> r)
-    match res with
-    | Some _ -> true
-    | _ -> false
-    
-// let checkRow sensors row =
-//     let mutable x = 0L
-//     let mutable scanned = true
-      
-//     for sensor in sensors do
-//         if inRange sensor x row then
-//             x <- skipTo sensor row
-//             found <- true
-            
-//     if not found then
-//         // actually x will be past the end of row at this point
-//         printfn $"P2: {x}, {row}"
-//         true
-//     else
-//         false
+// Look for a square in the row not covered by the sensors
+//   - get all relevant sensors (sensorX, row within range)
+//   - sort them by x co-ord (this is probably unnecessary but it
+//     helps me conceptualize what's going on)
+//   - start at (0, row). While x is < maxDim, check if square is in
+//     scan range of sensor. If so, x becomes skipTo. If x > 4_000_000
+//     entire row is covered. If we find a point that isn't covered, we win!
+let testRow (sensors:Sensor array) row =
+    let mutable x = 0L
+    let mutable i = 0
+    let mutable found = false    
+    while x <= maxDim && i < sensors.Length do
+        found <- false
+        if inRange sensors[i] x row then
+            x <- skipTo sensors[i] row
+            i <- 0
+            found <- true
+        else
+            i <- i + 1
 
-let perimeter sensor =
-    // (x, y) deltas
-    let ds = [1L,1L; -1L,1L; -1L,-1L; 1L,-1L]
-
-    // start at 'north' and walk around the perimeter of the diamond
-    // that is the radius of the sensor's scan area
-    seq {
-        let mutable pt = sensor.X, sensor.Y - sensor.D    
-        for dx,dy in ds do
-            for _ in 1L..sensor.D do
-                let x, y = pt            
-                pt <- (x+dx, y+dy)
-                yield pt
-    } |> Set.ofSeq
-
-// calc the complete edge of squares running along outside the perimeter
-// (including diaganols)
-let edges sensor =
-    let ds = [ 1L,1L,1L; -1L,1L,1L; -1L,-1L,-1L; 1L,-1L,-1L ]
-
-    seq {
-        let mutable pt = sensor.X, sensor.Y - sensor.D
-        for i in 0..3 do
-            for _ in 1L..sensor.D do               
-                let x,y = pt
-                let dx,dy,d = ds[i]
-                pt <- (x+dx, y+dy)
-                yield if y+dy = sensor.Y then                    
-                          [x+dx+d, y+dy]
-                      elif x+dx = sensor.X && y < sensor.Y then
-                          [x+dx+d, y+dy; x+dx+d+d, y+dy;
-                           x+dx+1L, y+dy; x+dx+2L, y+dy;
-                           x+dx, y+dy-1L; x+dx-1L, y+dy-1L; x+dx+1L, y+dy-1L]
-                      elif x+dx = sensor.X && y > sensor.Y then
-                          [x+dx+d, y+dy; x+dx+d+d, y+dy;
-                           x+dx-1L, y+dy; x+dx-2L, y+dy;
-                           x+dx, y+dy+1L; x+dx+1L, y+dy+1L; x+dx-1L, y+dy+1L]
-                      else                    
-                          [x+dx+d, y+dy; x+dx+d+d, y+dy] }
-    |> Seq.concat |> Set.ofSeq
-    
-let test() =
-    let s = { X=10; Y=10; BeaconX=0; BeaconY=0; D=9 }
-    let p = perimeter s
-    let p' = edges s
-
-    let minX = p' |> Seq.map(fun (x, _) -> x) |> Seq.min
-    let maxX = p' |> Seq.map(fun (x, _) -> x) |> Seq.max
-    let minY = p' |> Seq.map(fun (_, y) -> y) |> Seq.min
-    let maxY = p' |> Seq.map(fun (_, y) -> y) |> Seq.max
-    
-    for y in minY-1L..maxY+1L do
-        let sb = System.Text.StringBuilder()
-        for x in minX-1L..maxX+1L do
-            let ch = if s.X = x && s.Y = y then 's'
-                     elif Set.contains (x,y) p then '#'
-                     elif Set.contains (x,y) p' then '*'
-                     else '.'
-            ignore(sb.Append(ch))
-        printfn $"{sb.ToString()}"
+    if x < maxDim then Some ((x, row))
+    else None
 
 let part2 =
     let sensors' = sensors |> Array.sortBy(fun s-> s.Y, s.X)
 
-    printfn $"%A{sensors[6]}"
-    printfn $"{scanned sensors 5 0 }"
-    printfn $"{taxi 8 7 5 0 }"
-    // let mutable sum = 0UL
-    // for s in sensors' do
-    //     let p = edges s
-    //     printfn $"{p.Count}"
-    //     sum <- sum + (p |> Set.count |> uint64)
-    // printfn $"{sum} squares to check"
-    //test()
-    //checkRow sensors' 11
-    
-    // Okay, we want start at 0,0 and then scan forward.
-    //   - if pt is within a scanner's range, skip ahead to the next one
-    //   - go row-by-row
-    // let maxX = 20UL
-    // let maxY = 20UL
-    // let mutable x = 15L
-    // let mutable y = 0L
-    // let mutable go = true
-    
-    // while go do
-    //     for sensor in sensors' do
-    //         if inRange sensor x y then
-    //             printfn $"{sensor} {inRange sensor x y}"
-    //             printfn $"  next x = {skipTo sensor y}"
-    //     go <- false
-
-    //printfn $"{skipTo sensors[7] 2}"
-    //let f = { X=(4L); Y=(5L); BeaconX=0; BeaconY=0L; D=2 }
-    //printfn $"{skipTo f 3}"
+    let (x,y) = seq { 0L..maxDim } |> Seq.choose(fun row -> testRow sensors' row)
+                                   |> Seq.head
+    printfn $"P2: {x * 4_000_000L + y}"
     
