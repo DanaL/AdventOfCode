9ea4ab9b108888902c1784e3438ee0f2aff80c32
Dana Larose <ywg.dana@gmail.com>
2022-12-15 23:08:58 -0600

    2022 day 15 pt 2 -- new approach of searching along perimeters of all sensors. Gap will be 1 square not contained in another of the scan areas

--- 2022/Day15.fsx
+++ 2022/Day15.fsx
@@ -34,6 +34,7 @@ let sensors = File.ReadAllLines("input_day15.txt")
 let beacons = sensors |> Array.map(fun s -> s.BeaconX, s.BeaconY)
                       |> List.ofArray |> List.distinct |> Set.ofList
 
+let maxDim = 20L // 4_000_000L
 let targetRow = 10L // 2000000L
 
 let part1 =
@@ -43,30 +44,110 @@ let part1 =
     let p1 = Set.difference scanned beacons |> Set.count          
     printfn $"P1: {p1}"
 
-let skipTo scanner row =
-    printfn $"{scanner.X} {scanner.Y} row={row} d={scanner.D}"
-    scanner.D - ((scanner.Y - row) |> abs) - (-scanner.X) + 1L
+let skipTo sensor row =
+    sensor.D - ((sensor.Y - row) |> abs) - (-sensor.X) + 1L
 
 let inRange sensor x y =
     taxi sensor.X sensor.Y x y <= sensor.D
 
-let sensors' = sensors |> Array.sortBy(fun s-> s.Y, s.X)    
+// let checkRow sensors row =
+//     let mutable x = 0L
+//     let mutable scanned = true
+      
+//     for sensor in sensors do
+//         if inRange sensor x row then
+//             x <- skipTo sensor row
+//             found <- true
+            
+//     if not found then
+//         // actually x will be past the end of row at this point
+//         printfn $"P2: {x}, {row}"
+//         true
+//     else
+//         false
+
+let perimeter sensor =
+    // (x, y) deltas
+    let ds = [1L,1L; -1L,1L; -1L,-1L; 1L,-1L]
+
+    // start at 'north' and walk around the perimeter of the diamond
+    // that is the radius of the sensor's scan area
+    seq {
+        let mutable pt = sensor.X, sensor.Y - sensor.D    
+        for dx,dy in ds do
+            for _ in 1L..sensor.D do
+                let x, y = pt            
+                pt <- (x+dx, y+dy)
+                yield pt
+    } |> Set.ofSeq
+
+// calc the complete edge of squares running along outside the perimeter
+// (including diaganols)
+let edges sensor =
+    let ds = [ 1L,1L,1L; -1L,1L,1L; -1L,-1L,-1L; 1L,-1L,-1L ]
+
+    seq {
+        let mutable pt = sensor.X, sensor.Y - sensor.D
+        for i in 0..3 do
+            for _ in 1L..sensor.D do               
+                let x,y = pt
+                let dx,dy,d = ds[i]
+                pt <- (x+dx, y+dy)
+                yield if y+dy = sensor.Y then                    
+                          [x+dx+d, y+dy]
+                      elif x+dx = sensor.X && y < sensor.Y then
+                          [x+dx+d, y+dy; x+dx+d+d, y+dy;
+                           x+dx+1L, y+dy; x+dx+2L, y+dy;
+                           x+dx, y+dy-1L; x+dx-1L, y+dy-1L; x+dx+1L, y+dy-1L]
+                      elif x+dx = sensor.X && y > sensor.Y then
+                          [x+dx+d, y+dy; x+dx+d+d, y+dy;
+                           x+dx-1L, y+dy; x+dx-2L, y+dy;
+                           x+dx, y+dy+1L; x+dx+1L, y+dy+1L; x+dx-1L, y+dy+1L]
+                      else                    
+                          [x+dx+d, y+dy; x+dx+d+d, y+dy] }
+    |> Seq.concat |> Set.ofSeq
+    
+let test() =
+    let s = { X=10; Y=10; BeaconX=0; BeaconY=0; D=9 }
+    let p = perimeter s
+    let p' = edges s
+
+    let minX = p' |> Seq.map(fun (x, _) -> x) |> Seq.min
+    let maxX = p' |> Seq.map(fun (x, _) -> x) |> Seq.max
+    let minY = p' |> Seq.map(fun (_, y) -> y) |> Seq.min
+    let maxY = p' |> Seq.map(fun (_, y) -> y) |> Seq.max
+    
+    for y in minY-1L..maxY+1L do
+        let sb = System.Text.StringBuilder()
+        for x in minX-1L..maxX+1L do
+            let ch = if s.X = x && s.Y = y then 's'
+                     elif Set.contains (x,y) p then '#'
+                     elif Set.contains (x,y) p' then '*'
+                     else '.'
+            ignore(sb.Append(ch))
+        printfn $"{sb.ToString()}"
+    
 let part2 =
+    let sensors' = sensors |> Array.sortBy(fun s-> s.Y, s.X)
+    
+    test()
+    //checkRow sensors' 11
+    
     // Okay, we want start at 0,0 and then scan forward.
     //   - if pt is within a scanner's range, skip ahead to the next one
     //   - go row-by-row
-    let maxX = 20UL
-    let maxY = 20UL
-    let mutable x = 15L
-    let mutable y = 0L
-    let mutable go = true
+    // let maxX = 20UL
+    // let maxY = 20UL
+    // let mutable x = 15L
+    // let mutable y = 0L
+    // let mutable go = true
     
-    while go do
-        for sensor in sensors' do
-            if inRange sensor x y then
-                printfn $"{sensor} {inRange sensor x y}"
-                printfn $"  next x = {skipTo sensor y}"
-        go <- false
+    // while go do
+    //     for sensor in sensors' do
+    //         if inRange sensor x y then
+    //             printfn $"{sensor} {inRange sensor x y}"
+    //             printfn $"  next x = {skipTo sensor y}"
+    //     go <- false
 
     //printfn $"{skipTo sensors[7] 2}"
     //let f = { X=(4L); Y=(5L); BeaconX=0; BeaconY=0L; D=2 }
