58a4fb66215531d730075ebbe7d4023821aacd72
Dana Larose <ywg.dana@gmail.com>
2022-12-20 15:03:00 -0600

    Dash of cleanup

--- 2022/Day20.fsx
+++ 2022/Day20.fsx
@@ -1,5 +1,3 @@
-open System.IO
-
 let score (arr:int64 list) =
     let l = arr.Length
     let zi = arr |> List.findIndex(fun x -> x = 0)    
@@ -14,15 +12,14 @@ let switch (arr:List<int64*int>) i item =
     arr' |> List.insertAt ni item
 
 let shuffle rounds encryptKey =
-    let mutable arr = File.ReadAllLines("input_day20.txt")    
+    let mutable arr = System.IO.File.ReadAllLines("input_day20.txt")    
                       |> Array.mapi(fun i l -> int64(l)*encryptKey, i)
                       |> List.ofArray    
     let orig = arr        
     for _ in 1..rounds do
         for num in orig do
             let i = arr |> List.findIndex(fun x -> x = num)
-            arr <- switch arr i num
-                                 
+            arr <- switch arr i num                                 
     score (arr |> List.map(fun (x,_) -> x))
     
 let part1 =
