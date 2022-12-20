9ebda961bf4286decbe0006ff62fe28bb1ae638e
Dana Larose <ywg.dana@gmail.com>
2022-12-20 14:52:53 -0600

    2022 Day 20 part 2 done god finally

--- 2022/Day20.fsx
+++ 2022/Day20.fsx
@@ -5,29 +5,30 @@ let score (arr:int64 list) =
     let zi = arr |> List.findIndex(fun x -> x = 0)    
     arr[(zi + 1000) % l] + arr[(zi + 2000) % l] + arr[(zi + 3000) % l]
     
-let shuffle (arr:List<int64*bool>) i =    
-    let v,_ = arr[i]
-    let iv = (i |> int64) + v        
+let switch (arr:List<int64*int>) i item =    
+    let v, _ = item
     let arr' = arr |> List.removeAt i
-    let len = arr'.Length |> int64
-    let ni = if iv = i then i
-             elif iv >= len then (iv % len) |> int
-             elif v > 0L || iv > 0L then iv |> int            
-             else (len - (-iv % len)) |> int
-    arr' |> List.insertAt ni (v, true)
+    let n = int ((int64 i + v) % int64 arr'.Length)
+    let ni = if n < 0 then arr'.Length + n
+             else n        
+    arr' |> List.insertAt ni item
 
-let mutable arr = File.ReadAllLines("input_day20.txt")
-                  |> Array.map(fun l -> (l |> int64), false)
-                  |> List.ofArray
-//let mutable arr = [1,false; 2,false; -3,false; 3,false; -2,false; 0,false; 4,false]
-//let mutable arr = [-6,false; 0,false; -4,false; -10,false; 37,false; 1,false]
-
+let shuffle rounds encryptKey =
+    let mutable arr = File.ReadAllLines("input_day20.txt")    
+                      |> Array.mapi(fun i l -> int64(l)*encryptKey, i)
+                      |> List.ofArray    
+    let orig = arr        
+    for _ in 1..rounds do
+        for num in orig do
+            let i = arr |> List.findIndex(fun x -> x = num)
+            arr <- switch arr i num
+                                 
+    score (arr |> List.map(fun (x,_) -> x))
+    
 let part1 =
-    let mutable go = true
-    while go do
-        match arr |> List.tryFindIndex(fun (_,b) -> b = false) with
-        | Some i -> arr <- shuffle arr i                
-        | None -> go <- false    
-    printfn $"P1: {score (arr |> List.map(fun (x,_) -> x))}"
-
-
+    let score = shuffle 1 1L
+    printfn $"P1: {score}"
+    
+let part2 =
+     let score = shuffle 10 811589153L
+     printfn $"P2: {score}"
