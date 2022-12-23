d56dbe91589100310bd5f0de177fce677ec0f359
Dana Larose <ywg.dana@gmail.com>
2022-12-23 00:04:51 -0600

    2022 Day 22 part 2 complete

--- 2022/Day22.fsx
+++ 2022/Day22.fsx
@@ -48,67 +48,68 @@ let rec nextSqPt1 (map:Tile[,]) r c dr dc =
              else (c+dc) % w
 
     match map[r',c'] with
-    | Wall -> r,c
-    | Floor -> r',c'
-    | Space -> let tr,tc = nextSqPt1 map r' c' dr dc
-               if map[tr,tc] = Space then r,c
-               else tr,tc
+    | Wall -> r,c,dr,dc
+    | Floor -> r',c',dr,dc
+    | Space -> let tr,tc,_,_ = nextSqPt1 map r' c' dr dc
+               if map[tr,tc] = Space then r,c,dr,dc
+               else tr,tc,dr,dc
 
 // A cursed function...
-let rec nextSqPt2 (map:Tile[,]) r c dr dc =
+let nextSqPt2 (map:Tile[,]) r c dr dc =
     let h = map |> Array2D.length1
     let w = map |> Array2D.length2
     let r' = r+dr
     let c' = c+dc
 
-    let fr, fc = if r' = -1 && c >= 100 then
+    let fr,fc,dr',dc' =
+                 if r' = -1 && c >= 100 then
                      // top edge of Panel 1
-                     199, c'-100
+                     199, c'-100, -1, 0
                  elif c' = 150 then
                      // right edge of Panel 1
-                     149 - r', 99
+                     149 - r', 99, 0, -1
                  elif r' = 50 && c' >= 100 && dr = 1 then
                      // bottom edge of Panel 1
-                     c'-50, 99
+                     c'-50, 99, 0, -1
                  elif c' = 49 && r' < 50 then
                      // left edge of Panel 2
-                     149-r' , 0
+                     149-r', 0, 0, 1
                  elif r' = -1 && c' < 100 then
                      // top edge of Panel 2
-                     100+c', 0
+                     100+c', 0, 0, 1
                  elif c' = 49 && r' < 100 && dc = -1 then
                      // left edge of Panel 3
-                     100, r' - 50
+                     100, r' - 50, 1, 0
                  elif c' = 100 && r' >= 50 && r' <= 99 then
                      // right edge of Panel 3
-                     49, r' + 50
+                     49, r' + 50, -1, 0
                  elif c' = 100 && r' >= 100 && r' <= 149 then
                      // right edge of Panel 4
-                     149 - r', 149
+                     149 - r', 149, 0, -1
                  elif r' = 150 && c' >= 50 && dr = 1 then
                      // bottom edge of panel 4
-                     c' + 100, 49
+                     c' + 100, 49, 0, -1
                  elif r' = 99 && c' <= 49 && dr = -1 then
                      // top edge of panel 5
-                     c' + 50, 50
+                     c' + 50, 50, 0, 1
                  elif c' = -1 && r' < 150 then
                      // left edge of panel 5
-                     149 - r', 50
+                     149 - r', 50, 0, 1
                  elif r' = 200 then
                      // bottom edge of panel 6
-                     0, c' + 100
+                     0, c' + 100, 1, 0
                  elif c' = -1 && r' >= 150 then
                      // left edge of panel 6
-                     0, r' - 100
+                     0, r' - 100, 1, 0
                  elif c' = 50 && r' >= 150 && dc = 1 then
                      // right edge of panel 6
-                     149, r' - 100
+                     149, r' - 100, -1, 0
                  else
-                     r', c'
+                     r', c',dr,dc
                      
     match map[fr,fc] with
-    | Wall -> r,c
-    | Floor -> fr,fc
+    | Wall -> r,c,dr,dc
+    | Floor -> fr,fc,dr',dc'
     | Space -> failwith "Hmm should never land on a space in Part 2"
     
 // This took me so long to debug T_T
@@ -125,11 +126,11 @@ let turn d r c =
 let rec move (map:Tile[,]) (actions:(int*string)[]) r c  dr dc i f =
     if i < (Array.length actions) then
         let steps, t = actions[i]              
-        let r', c' = seq { 1..steps }
-                     |> Seq.fold(fun(r,c) _ -> f map r c dr dc) (r,c)
-        let dr', dc' = if t = "" then dr,dc
-                       else turn t dr dc
-        move map actions r' c' dr' dc' (i+1) f
+        let r', c', dr', dc' = seq { 1..steps }
+                               |> Seq.fold(fun(r,c,dr,dc) _ -> f map r c dr dc) (r,c,dr,dc)
+        let dr'', dc'' = if t = "" then dr',dc'
+                         else turn t dr' dc'
+        move map actions r' c' dr'' dc'' (i+1) f
     else
         r, c, dr, dc        
 
@@ -148,16 +149,10 @@ let sr,sc = 0, lines[1] |> Seq.findIndex(fun ch -> ch = '.')
 let part1 =
     let r,c,dr,dc = move map actions sr sc 0 1 0 nextSqPt1
     let p1 = 1000 * (r+1) + 4 * (c+1) + (dirToN (dr,dc))
-    printfn $"Last facing: {dr} {dc}"
     printfn $"P1: {p1}"
 
-let part2 =
-    dump map sr sc
-    //let sr = 199
-    //let sc = 49
-    let r,c,dr,dc = move map actions sr sc 0 1 0 nextSqPt2
+let part2 =    
+    let r,c,dr,dc = move map actions sr sc 0 1 0 nextSqPt2    
     let p2 = 1000 * (r+1) + 4 * (c+1) + (dirToN (dr,dc))
     printfn $"P2: {p2}"
-    //dump map r c
-    //printfn $"{r} {c}"
     
