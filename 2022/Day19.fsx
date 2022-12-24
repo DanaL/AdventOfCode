4358e78f73e1a1cc89a0cf9d2a53299226f65ad0
Dana Larose <ywg.dana@gmail.com>
2022-12-24 17:04:20 -0600

    Woo! Parts 1 and 2 of Day 19 are working and the run time is tolerable!

--- 2022/Day19.fsx
+++ 2022/Day19.fsx
@@ -1,75 +1,169 @@
+open System.Collections.Generic
+open System.Diagnostics
 open System.IO
 open System.Text.RegularExpressions
 
-type Build = Orebot | Claybot | ObsidianBot | GeodeBot
-
 type State = { BluePrintID:int; Turn:int; Orebots:int; Claybots:int;
-               Obsidianbots:int; Geodebots:int
-               Ore:int; Clay:int; Obsidian:int; Geodes:int }
+               Obsidianbots:int; Ore:int; Clay:int; Obsidian:int; Geodes:int }
                
 type BluePrint = { ID:int; Orebot:int; Claybot:int;
                  OBsOre:int; OBsClay:int;
-                 GBOre:int; GBObsid:int }
+                 GBOre:int; GBObsid:int; MaxOre:int }
                  
 let bluePrints = File.ReadAllLines("input_day19.txt")
                  |> Array.map(fun l -> Regex.Split(l, "\D+"))
                  |> Array.map(fun arr -> let nums =  arr[1..arr.Length-2] |> Array.map int
+                                         let maxOre = [ nums[1]; nums[2]; nums[3]; nums[5] ] |> List.max
                                          { ID=nums[0];
                                            Orebot=nums[1];
                                            Claybot=nums[2];
                                            OBsOre=nums[3]; OBsClay=nums[4];
-                                           GBOre=nums[5]; GBObsid=nums[6] })
+                                           GBOre=nums[5]; GBObsid=nums[6]; MaxOre=maxOre })
                  |> Array.map(fun bp -> bp.ID, bp )
                  |> Map.ofArray
 
-// What robots can we build with our current inventory?
-let canBuild state =
-    let bp = bluePrints[state.BluePrintID]
-
-    printfn $"state {state}"
-    printfn $"blueprint {bp}"
-    let orebot = if state.Ore >= bp.Orebot then Some Orebot
-                 else None
-    let claybot = if state.Ore >= bp.Claybot then Some Claybot
-                  else None
-    let obsidBot = if state.Ore >= bp.OBsOre && state.Clay >= bp.OBsClay then Some ObsidianBot
-                   else None
-    let geodeBot = if state.Ore >= bp.GBOre && state.Obsidian >= bp.GBObsid then Some GeodeBot
-                   else None
-    printfn $"{orebot}"
-    [orebot; claybot; obsidBot; geodeBot]
-    |> List.choose id
-
-let build state option =
-    let bp = bluePrints[state.BluePrintID]
-    match option with
-    | Orebot -> { state with Orebots=state.Orebots+1;
-                             Ore=state.Ore - bp.Orebot }
-    | Claybot -> { state with Claybots=state.Claybots+1;
-                              Ore=state.Ore - bp.Claybot }
-    | ObsidianBot -> { state with Obsidianbots=state.Obsidianbots+1;
-                                  Ore=state.Ore - bp.OBsOre;
-                                  Clay=state.Clay - bp.OBsClay }
-    | GeodeBot -> {state with Geodebots=state.Geodebots+1;
-                              Ore=state.Ore - bp.GBOre;
-                              Obsidian=state.Obsidian - bp.GBObsid }
-let calcNextStates state =
-    let options = canBuild state
+let nextOreBot state blueprint maxTurns =
+    let needed = blueprint.Orebot - state.Ore
+    let turns = 1 + if needed <= 0 then 0
+                    elif needed % state.Orebots <> 0 then
+                        needed / state.Orebots + 1
+                    else
+                        needed / state.Orebots
     
-    let state' = { state with Turn=state.Turn+1;
-                              Ore=state.Ore + state.Orebots
-                              Clay=state.Clay + state.Claybots
-                              Obsidian=state.Obsidian + state.Obsidianbots
-                              Geodes=state.Geodes + state.Geodebots }    
-    if options.Length = 0 then
-        [ state' ]
+    if state.Turn + turns > maxTurns then
+        None
     else
-        options |> List.map(fun opt -> build state' opt)
+        Some { state with Turn=state.Turn+turns; Orebots=state.Orebots + 1
+                          Ore=state.Ore + state.Orebots * turns - blueprint.Orebot
+                          Clay=state.Clay + state.Claybots * turns
+                          Obsidian=state.Obsidian + state.Obsidianbots * turns }
 
-let bp = bluePrints[1]
-let initial = { BluePrintID=bp.ID; Turn=0;
-                Orebots=1; Claybots=0;Obsidianbots=0; Geodebots=0;
-                Ore=4; Clay=0; Obsidian=0; Geodes=0 }
+let nextClayBot state blueprint maxTurns =
+    let needed = blueprint.Claybot - state.Ore
+    let turns = 1 + if needed <= 0 then 0
+                    elif needed % state.Orebots <> 0 then
+                        needed / state.Orebots + 1
+                    else
+                        needed / state.Orebots
+    if state.Turn + turns > maxTurns then
+        None
+    else        
+        Some { state with Turn=state.Turn+turns; Claybots=state.Claybots + 1
+                          Ore=state.Ore + state.Orebots * turns - blueprint.Claybot
+                          Clay=state.Clay + state.Claybots * turns
+                          Obsidian=state.Obsidian + state.Obsidianbots * turns }
 
-let next = calcNextStates initial
-printfn $"%A{next}"
+let nextObsidianBot state blueprint maxTurns =
+    let neededOre = blueprint.OBsOre - state.Ore
+    let turnsToOre = if neededOre <= 0 then 0
+                     elif neededOre % state.Orebots <> 0 then
+                         neededOre / state.Orebots + 1
+                     else
+                         neededOre / state.Orebots
+    let neededClay = blueprint.OBsClay - state.Clay
+    let turnsToClay = if neededClay <= 0 then 0
+                      elif neededClay % state.Claybots <> 0 then
+                          neededClay / state.Claybots + 1
+                      else
+                          neededClay / state.Claybots
+    let turns = 1 + if turnsToClay > turnsToOre then turnsToClay else turnsToOre
+    
+    if state.Turn + turns > maxTurns then
+        None
+    else
+        Some { state with Turn=state.Turn+turns; Obsidianbots=state.Obsidianbots + 1
+                          Ore=state.Ore + state.Orebots * turns - blueprint.OBsOre
+                          Clay=state.Clay + state.Claybots * turns - blueprint.OBsClay
+                          Obsidian=state.Obsidian + state.Obsidianbots * turns }
+
+let nextGeode state blueprint maxTurns =
+    let neededOre = blueprint.GBOre - state.Ore
+    let turnsToOre = if neededOre <= 0 then 0
+                     elif neededOre % state.Orebots <> 0 then
+                         neededOre / state.Orebots + 1
+                     else
+                         neededOre / state.Orebots
+    let neededObsidian = blueprint.GBObsid - state.Obsidian
+    let turnsToObs = if neededObsidian <= 0 then 0
+                     elif neededObsidian % state.Obsidianbots <> 0 then
+                         neededObsidian / state.Obsidianbots + 1
+                     else
+                         neededObsidian / state.Obsidianbots
+    let turns = 1 + if turnsToObs > turnsToOre then turnsToObs else turnsToOre
+    
+    if state.Turn + turns > maxTurns then
+        None
+    else
+        let geodes = state.Geodes + (maxTurns - (state.Turn + turns))
+        Some { state with Turn=state.Turn+turns
+                          Ore=state.Ore + state.Orebots * turns - blueprint.GBOre
+                          Clay=state.Clay + state.Claybots * turns
+                          Obsidian=state.Obsidian + state.Obsidianbots * turns - blueprint.GBObsid
+                          Geodes=geodes }
+    
+let calcNextStates state maxTurns =
+    let blueprint = bluePrints[state.BluePrintID]
+    let ob = if state.Orebots < blueprint.MaxOre then
+                nextOreBot state blueprint maxTurns
+             else
+                 None
+    let cb = if state.Claybots < blueprint.OBsClay then
+                 nextClayBot state blueprint maxTurns
+             else
+                 None
+    let obsb = if state.Claybots > 0 && state.Obsidianbots < blueprint.GBObsid then
+                   nextObsidianBot state blueprint maxTurns
+               else
+                   None
+    let gb = if state.Obsidianbots > 0 then
+                 nextGeode state blueprint maxTurns
+             else
+                 None
+    [ ob; cb; obsb; gb ] |> List.choose id
+    
+let sim blueprintID rounds =
+    let mutable mostGeodes = 0
+    let bp = bluePrints[blueprintID]
+    let initial = { BluePrintID=bp.ID; Turn=0 
+                    Orebots=1; Claybots=0;Obsidianbots=0
+                    Ore=0; Clay=0; Obsidian=0; Geodes=0 }
+    
+    // minGuess -- a guess at the minimum geodes I should be able to fetch. So if turns remaining
+    // don't allow for us to beat that guess, abandon this path. 
+    //let minGuess = rounds - (bp.Claybot + bp.OBsClay + bp.GBObsid / 2) + 1
+    //printfn $"{minGuess}"
+    let states = Queue<State>()
+    states.Enqueue(initial)
+    
+    while states.Count > 0 do
+        let state = states.Dequeue()       
+        let next = calcNextStates state rounds
+        for n in next do            
+            if n.Geodes > mostGeodes then
+                mostGeodes <- n.Geodes
+            
+            if n.Turn < rounds then
+                states.Enqueue(n)
+            
+    mostGeodes     
+
+let stopWatch = Stopwatch()
+stopWatch.Start()
+
+let part =
+    let p1 = bluePrints.Keys |> Seq.map(fun id -> let geodes = sim id 24
+                                                  printfn $"Testing blueprint {id}  {geodes}"
+                                                  id * geodes)
+                             |> Seq.sum
+    printfn $"P1: {p1}"
+    
+let part2 =
+    let p2 = bluePrints.Keys |> Seq.take 3 |> Seq.map(fun id -> let geodes = sim id 32
+                                                                printfn $"Testing blueprint {id} {geodes}"
+                                                                geodes)
+                             |> List.ofSeq
+    printfn $"P2: {p2[0] * p2[1] * p2[2]}"
+stopWatch.Stop()
+
+let ts = stopWatch.Elapsed
+printfn $"Run time: {ts.TotalSeconds}"
