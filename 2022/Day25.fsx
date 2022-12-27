65995857b28dbd735fc86df9c46518e6585301db
Dana Larose <ywg.dana@gmail.com>
2022-12-26 21:29:42 -0600

    Day 25 and Advent of Code 2022 done!!

--- 2022/Day25.fsx
+++ 2022/Day25.fsx
@@ -8,80 +8,36 @@ let toInt (s:string) =
     v                                       
 
 let snafuDigit = function
-    | 0 -> ('0', '0')
-    | 1 -> ('0', '1')
-    | 2 -> ('0', '2')
-    | 3 -> ('1', '=')
-    | 4 -> ('1', '-')
+    | 0L -> (0, 0)
+    | 1L -> (0, 1)
+    | 2L -> (0, 2)
+    | 3L -> (1, 3)
+    | 4L -> (1, 4)
     | _ -> failwith "Hmm this shouldn't happen"
     
-let rec toSnafuDigits x =
-    if x / 5 <> 0 then
-        snafuDigit(x % 5) :: toSnafuDigits (x/5)
+let rec toSnafuDigits (x:int64) =
+    if x / 5L <> 0L then
+        snafuDigit(x % 5L) :: toSnafuDigits (x/5L)
     else
         [ snafuDigit(x) ]
 
-let syms = [ '0'; '1'; '2'; '='; '-' ]
-
-let rec mashUp (digits:List<char*char>) i carry =
+let syms = [ "0"; "1"; "2"; "="; "-" ]
+let rec mashUp (digits:List<int*int>) i carry =
     if i < digits.Length then        
         let a,b = digits[i]
-        let s = syms |> List.findIndex(fun c -> c = b)
-        let cs = syms |> List.findIndex(fun c -> c = carry)
-        let sym = syms[(s + cs) % syms.Length]
-        printfn $"{carry} {s + cs}"
-        string(sym) :: mashUp digits (i + 1) a
-    elif carry = '0' then
+        let j = (b + carry) % syms.Length
+        let nc = if b + carry = 3 then 1
+                 else a
+        syms[j] :: mashUp digits (i + 1) nc
+    elif carry = 0 then
         [ "" ]
     else
-        [ string(carry) ]
+        [ string(syms[carry]) ]
         
 let toSnafu x =
     let digits = toSnafuDigits x
-    printfn $"%A{digits}"
-    mashUp digits 0 '0' |> List.rev |> String.concat ""
-printfn $"%A{toSnafu 1747}"
-// let mutable n = "0"
-// for _ in 1..3 do
-//     printf $"{n} + {1} = "
-//     n <- snafuAdd n "1"
-//     printfn $" {n}"
+    mashUp digits 0 0 |> List.rev |> String.concat ""
     
-    //let pairs = Seq.zip a b
-    //p/rintfn $"{pairs}"
-// let rec snafuDigits x =
-//     let v = x / 5L
-//     let b10 = x % 10L
-//     let d = match b10 with
-//     | 0 | 1 | 2 -> string(b10) + " "
-//     | 3 -> "1= "
-//     | 4 -> "1- "
-//     | 5 -> "10 "
-//     | 6 -> "11 "
-//     | 7 -> "12 "
-//     | 8 -> "2= "
-//     | 9 -> "2- "
-    
-//     printfn $"{b10}"
-//     let d = match x % 5L with
-//                          | 0L | 1L | 2L -> string(x % 5L) + " "
-//                          | 3L -> "1= "
-//                          | 4L -> "1- "
-//                          | _ -> failwith "Hmm this shouldn't happen"
-//     if v > 0 then
-//         d :: snafuDigits v
-//     else
-//         [d]
-
-// let toSnafu x = String.concat "" (snafuDigits x |> List.rev)
-
-//let x = snafuAdd "22" "1"
-//printfn $"%A{x}"
-//let v0 = toInt "10-"
-//printfn $"{v0}"
-//let v1 = toInt "1121-1110-1=0"
-//printfn $"{v1}"
-
-//printfn $"{toSnafu 9L}"
-//printfn $"{toSnafu 12345L}"
-
+let p1 = System.IO.File.ReadAllLines("input_day25.txt")
+         |> Array.map toInt |> Array.sum |> toSnafu
+printfn $"P1: {p1}"
