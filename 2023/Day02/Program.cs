7a13f10e576bbc88809613b756a43cee1ad69836
Dana Larose <ywg.dana@gmail.com>
2023-12-02 16:31:49 -0600

    Day 2 Part 1 complete

--- 2023/Day02/Program.cs
+++ 2023/Day02/Program.cs
@@ -1,9 +1,68 @@
 ﻿namespace Day02;
 
+class Game 
+{
+    public int ID { get; set; }
+    public List<(int, int, int)> Rounds { get; set; }
+
+    public Game() => Rounds = new List<(int, int, int)>();
+}
+
 class Program
 {
+    static Game ParseLine(string line) 
+    {
+        int sc = line.IndexOf(':');
+        int gameID = int.Parse(line.Substring(5, sc - 5));
+        var game = new Game() { ID = gameID };
+
+        foreach (var piece in line.Substring(sc + 1).Trim().Split(';').Select(p => p.Trim())) 
+        {
+            var round = (0, 0, 0);
+            foreach (var color in piece.Split(',').Select(c => c.Trim())) 
+            {
+                int sp = color.IndexOf(' ');
+                int v = int.Parse(color.Substring(0, sp));
+                string c = color.Substring(sp + 1);
+                if (c == "red" && v > round.Item1)
+                    round.Item1 = v;
+                else if (c == "green" && v > round.Item2)
+                    round.Item2 = v;
+                else if (c == "blue" && v > round.Item3)
+                    round.Item3 = v;                
+            }
+
+            game.Rounds.Add(round);
+        }
+
+        return game;
+    }
+
+    static bool ValidGame(Game game, int red, int green, int blue) 
+    {
+        foreach (var round in game.Rounds) 
+        {
+            if (round.Item1 > red || round.Item2 > green || round.Item3 > blue)
+                return false;
+        }
+
+        return true;
+    }
+
+    static void Part1(string[] lines) 
+    {
+        var sum = lines.Select(l => ParseLine(l))
+                        .Where(g => ValidGame(g, 12, 13, 14))
+                        .Select(g => g.ID)
+                        .Sum();
+        
+        Console.WriteLine($"P1: {sum}");
+    }
+
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello, World!");
+        var lines = File.ReadAllLines("input.txt");
+
+        Part1(lines);
     }
 }
