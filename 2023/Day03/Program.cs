716f7848c45314fd82d10d339180171e805781c4
Dana Larose <ywg.dana@gmail.com>
2023-12-03 19:43:09 -0600

    Day 3 part 2 complete

--- 2023/Day03/Program.cs
+++ 2023/Day03/Program.cs
@@ -10,19 +10,23 @@ class Part
 
 class Program
 {
-    public static List<(int, int)> _neighbours = new() { (-1, -1), (-1, 0), (-1, 1), 
-                                                         ( 0, -1),          ( 0, 1), 
-                                                         ( 1, -1), ( 1, 0), ( 1, 1)};
-                                                             
-    static List<Part> ParseLine(int row, string line) 
+    static List<(int, int)> _neighbours = new() { (-1, -1), (-1, 0), (-1, 1), 
+                                                  ( 0, -1),          ( 0, 1), 
+                                                  ( 1, -1), ( 1, 0), ( 1, 1)};
+
+    static List<Part> ParseLine(int row, string line, HashSet<(int, int)> gears) 
     {
         int c = 0;
         var parts = new List<Part>();
         while (c < line.Length) 
         {
-            // find a digit
-            while (c < line.Length && !char.IsNumber(line[c]))
+            // skip non-digits, and look for gears
+            while (c < line.Length && !char.IsNumber(line[c])) 
+            {
+                if (line[c] == '*')
+                    gears.Add((row, c));
                 ++c;
+            }
 
             var p = new Part();
             var n = "";
@@ -37,7 +41,6 @@ class Program
                 p.Num = int.Parse(n);
                 parts.Add(p);
             }
-            ++c;
         }
 
         return parts;
@@ -64,21 +67,46 @@ class Program
         return false;
     }
 
+    static long GearRatio((int, int) gear, List<Part> parts) 
+    {
+        var adj = new HashSet<int>();
+        foreach (var n in _neighbours) 
+        {
+            var coord = (gear.Item1 + n.Item1, gear.Item2 + n.Item2);
+            foreach (var p in parts) 
+            {
+                if (p.Coords.Contains(coord))
+                    adj.Add(p.Num);
+            }
+        }
+
+        var pns = adj.ToList();
+        return pns.Count == 2 ? pns[0] * pns[1] : 0;
+    }
+
     static void Main(string[] args)
     {
+        var gears = new HashSet<(int, int)>();
         var lines = File.ReadAllLines("input.txt");
 
         var parts = new List<Part>();
         for (int j = 0; j < lines.Length; j++) 
         {
-            var p = ParseLine(j, lines[j]);
+            var p = ParseLine(j, lines[j], gears);
             parts.AddRange(p);
         }
 
+        // for part 1, we need to find the sum of any part #s
+        // which are adjacent to a symbol
         var p1 = parts.Where(p => IsPartNumber(p, lines))
                       .Select(p => p.Num)
                       .Sum();
-
         Console.WriteLine($"P1: {p1}");
+        
+        // for part 2, we need to check all gears (the *s) and if they are
+        // adjacent to two parts, multiply their part numbers for the 'gear ratio'
+        // and sum them up
+        var p2 = gears.Select(g => GearRatio(g, parts)).Sum();
+        Console.WriteLine($"P2: {p2}");
     }
 }
