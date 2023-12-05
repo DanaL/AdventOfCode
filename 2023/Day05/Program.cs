cadc75a285cf509fb885ca76570c5b61e07e4164
Dana Larose <ywg.dana@gmail.com>
2023-12-05 11:19:21 -0600

    2023 Day 5 part 1 complete

--- 2023/Day05/Program.cs
+++ 2023/Day05/Program.cs
@@ -19,6 +19,26 @@ static List<Span> FindRanges(string section, string[] lines)
     return ranges;
 }
 
+static long FindInMap(long val, List<Span> map) 
+{
+    foreach (var span in map)
+    {
+        if (val >= span.Src && val < span.Src + span.Len) 
+        {
+            long diff = val - span.Src;
+            return span.Dest + diff;
+        }
+    }
+
+    return val;
+}
+
+static long LocForSeed(long seed, List<Span> seedToSoil, List<Span> soilToFertilizer, List<Span> fertilizerToWater, List<Span> waterToLight, List<Span> lightToTemp, List<Span> tempToHumidity, List<Span> humidityToLocation)
+{
+    // Incredibly beautiful and elegant...
+    return FindInMap(FindInMap(FindInMap(FindInMap(FindInMap(FindInMap(FindInMap(seed, seedToSoil), soilToFertilizer), fertilizerToWater), waterToLight), lightToTemp), tempToHumidity), humidityToLocation);
+}
+
 var lines = File.ReadAllLines("input.txt");
 var seeds = lines[0].Substring(lines[0].IndexOf(":") + 1).Trim().Split(' ').Select(n => long.Parse(n)).ToList();
 
@@ -29,6 +49,9 @@ var waterToLight = FindRanges("water-to-light map:", lines);
 var lightToTemp = FindRanges("light-to-temperature map:", lines);
 var tempToHumidity = FindRanges("temperature-to-humidity map:", lines);
 var humidityToLocation = FindRanges("humidity-to-location map:", lines);
+
+var p1 = seeds.Select(s => LocForSeed(s, seedToSoil, soilToFertilizer, fertilizerToWater, waterToLight, lightToTemp, tempToHumidity, humidityToLocation)).Min();
+Console.WriteLine($"P1: {p1}");
 
 namespace Day05
 {
