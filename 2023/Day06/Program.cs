8049b2ab000db505094341d8b844b47e06e1bf88
Dana Larose <ywg.dana@gmail.com>
2023-12-06 11:58:14 -0600

    2023 Day 6 part 2 complete

--- 2023/Day06/Program.cs
+++ 2023/Day06/Program.cs
@@ -1,14 +1,14 @@
 ﻿
-static int NumThatWin(int time, int distance)
+static ulong NumThatWin(ulong time, ulong distance)
 {
     // I'm assuming there's a bell curve situation here and that 
     // the best result will be time/2 and then search up and down 
     // from there. (There's almost certainly simply arithmetic to 
     // determine this...)
-    var wins = 0;
+    ulong wins = 0UL;
 
-    var start = time/2;
-    var d = 0;
+    ulong start = time/2UL;
+    ulong d = 0UL;
     do 
     {
         d = start * (time - start);
@@ -19,8 +19,8 @@ static int NumThatWin(int time, int distance)
             break;
     } while (true);
 
-    start = time/2 - 1 ;
-    d = 0;
+    start = time/2UL - 1UL;
+    d = 0UL;
     do 
     {
         d = start * (time - start);
@@ -34,8 +34,10 @@ static int NumThatWin(int time, int distance)
     return wins;
 }
 
-// var races = new List<(int, int)>() { (7, 9), (15, 40), (30, 200) };
-var races = new List<(int, int)>() { (38, 234), (67, 1027), (76, 1157), (73, 1236) };
+var races = new List<(ulong, ulong)>() { (38, 234), (67, 1027), (76, 1157), (73, 1236) };
 
-var p1 = races.Select(r => NumThatWin(r.Item1, r.Item2)).Aggregate(1, (x, y) => x * y);
+var p1 = races.Select(r => NumThatWin(r.Item1, r.Item2)).Aggregate(1UL, (x, y) => x * y);
 Console.WriteLine($"P1: {p1}");
+
+// Can...can I just brute force part 2??
+Console.WriteLine($"P2: {NumThatWin(38_677_673, 234_102_711_571_236)}");
