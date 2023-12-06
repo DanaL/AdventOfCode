ccb81b295bed3c1a486a165038e36b9c38aba5ac
Dana Larose <ywg.dana@gmail.com>
2023-12-06 11:38:14 -0600

    2023 Day 6 part 1 complete

--- 2023/Day06/Program.cs
+++ 2023/Day06/Program.cs
@@ -1,5 +1,41 @@
-﻿using Day06;
+﻿
+static int NumThatWin(int time, int distance)
+{
+    // I'm assuming there's a bell curve situation here and that 
+    // the best result will be time/2 and then search up and down 
+    // from there. (There's almost certainly simply arithmetic to 
+    // determine this...)
+    var wins = 0;
 
-Console.WriteLine("Hello, World!");
+    var start = time/2;
+    var d = 0;
+    do 
+    {
+        d = start * (time - start);
+        ++start;
+        if (d > distance)
+            ++wins;
+        else
+            break;
+    } while (true);
 
-namespace Day06 {}
+    start = time/2 - 1 ;
+    d = 0;
+    do 
+    {
+        d = start * (time - start);
+        --start;
+        if (d > distance)
+            ++wins;
+        else
+            break;
+    } while (true);
+
+    return wins;
+}
+
+// var races = new List<(int, int)>() { (7, 9), (15, 40), (30, 200) };
+var races = new List<(int, int)>() { (38, 234), (67, 1027), (76, 1157), (73, 1236) };
+
+var p1 = races.Select(r => NumThatWin(r.Item1, r.Item2)).Aggregate(1, (x, y) => x * y);
+Console.WriteLine($"P1: {p1}");
