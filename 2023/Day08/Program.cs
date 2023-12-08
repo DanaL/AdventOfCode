a4671deea55fd88ccc592add52a67d7ca2fa7171
Dana Larose <ywg.dana@gmail.com>
2023-12-08 14:33:56 -0600

    MOAR code cleanup

--- 2023/Day08/Program.cs
+++ 2023/Day08/Program.cs
@@ -1,9 +1,9 @@
 ﻿using System.Text.RegularExpressions;
 
-static int PathLength(char[] moves, Dictionary<string, (string Left, string Right)> nodes, string node, string ending)
+static long PathLength(char[] moves, Dictionary<string, (string Left, string Right)> nodes, string node, string ending)
 {
-    int count = 0;
-    int curr = 0;
+    long count = 0;
+    long curr = 0;
     while (!node.EndsWith(ending)) 
     {
         node = moves[curr] == 'L' ? nodes[node].Left : nodes[node].Right;
@@ -29,16 +29,6 @@ static long GCD(long a, long b)
 }
 
 static long LCM(long a, long b) => (a * b) / GCD(a, b);
-static int Part1(char[] moves, Dictionary<string, (string Left, string Right)> nodes) => PathLength(moves, nodes, "AAA", "ZZZ");
-
-static long Part2(char[] moves, Dictionary<string, (string Left, string Right)> nodes) 
-{
-    var starts = nodes.Keys.Where(k => k[2] == 'A').ToList();
-    var paths = starts.Select(p => PathLength(moves, nodes, p, "Z"))
-                      .Select(n => (long) n).ToList();
-    
-    return paths.Skip(1).Aggregate(paths[0], (lcm, n) => LCM(lcm, n));
-}
 
 var lines = File.ReadAllLines("input.txt");
 var moves = lines[0].ToCharArray();
@@ -46,5 +36,9 @@ var nodes = lines[2..].Select(line => Regex.Match(line, @"(\w+) = \((\w+), (\w+)
                       .Select(m => (m.Groups[1].Value, (m.Groups[2].Value, m.Groups[3].Value)))
                       .ToDictionary(e => e.Item1, e => e.Item2);
 
-Console.WriteLine($"P1: {Part1(moves, nodes)}");
-Console.WriteLine($"P2: {Part2(moves, nodes)}");
+Console.WriteLine($"P1: {PathLength(moves, nodes, "AAA", "ZZZ")}");
+
+var p2 = nodes.Keys.Where(k => k[2] == 'A')
+                   .Select(p => PathLength(moves, nodes, p, "Z"))
+                   .Aggregate(LCM);
+Console.WriteLine($"P2: {p2}");
