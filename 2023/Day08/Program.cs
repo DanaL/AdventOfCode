c559bc310f83b3769de399d88d20eea512d402d3
Dana Larose <ywg.dana@gmail.com>
2023-12-08 14:14:54 -0600

    Some code cleanup

--- 2023/Day08/Program.cs
+++ 2023/Day08/Program.cs
@@ -1,10 +1,8 @@
 ﻿using System.Text.RegularExpressions;
-using Day08;
 
-static int PathLength(char[] moves, Dictionary<string, Exits> nodes, string start, string ending)
+static int PathLength(char[] moves, Dictionary<string, (string Left, string Right)> nodes, string node, string ending)
 {
     int count = 0;
-    string node = start;
     int curr = 0;
     while (!node.EndsWith(ending)) 
     {
@@ -31,10 +29,9 @@ static long GCD(long a, long b)
 }
 
 static long LCM(long a, long b) => (a * b) / GCD(a, b);
+static int Part1(char[] moves, Dictionary<string, (string Left, string Right)> nodes) => PathLength(moves, nodes, "AAA", "ZZZ");
 
-static int Part1(char[] moves, Dictionary<string, Exits> nodes) => PathLength(moves, nodes, "AAA", "ZZZ");
-
-static long Part2(char[] moves, Dictionary<string, Exits> nodes) 
+static long Part2(char[] moves, Dictionary<string, (string Left, string Right)> nodes) 
 {
     var starts = nodes.Keys.Where(k => k[2] == 'A').ToList();
     var paths = starts.Select(p => PathLength(moves, nodes, p, "Z"))
@@ -45,15 +42,9 @@ static long Part2(char[] moves, Dictionary<string, Exits> nodes)
 
 var lines = File.ReadAllLines("input.txt");
 var moves = lines[0].ToCharArray();
-var r = new Regex(@"(\w+) = \((\w+), (\w+)\)");
-var nodes = lines[2..].Select(line => r.Match(line))
-                      .Select(m => (m.Groups[1].Value, new Exits(m.Groups[2].Value, m.Groups[3].Value)))
+var nodes = lines[2..].Select(line => Regex.Match(line, @"(\w+) = \((\w+), (\w+)\)"))
+                      .Select(m => (m.Groups[1].Value, (m.Groups[2].Value, m.Groups[3].Value)))
                       .ToDictionary(e => e.Item1, e => e.Item2);
 
 Console.WriteLine($"P1: {Part1(moves, nodes)}");
 Console.WriteLine($"P2: {Part2(moves, nodes)}");
-
-namespace Day08
-{
-    record Exits(string Left, string Right);
-}
