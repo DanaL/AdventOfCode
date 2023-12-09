a17d03b7e24d2dbd44b79684a2bf6ba97a4b4ec7
Dana Larose <ywg.dana@gmail.com>
2023-12-09 10:29:52 -0600

    2023 Day 8 part 1 complete

--- 2023/Day09/Program.cs
+++ 2023/Day09/Program.cs
@@ -1,2 +1,18 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿
+static int NextTerm(IEnumerable<int> arr) 
+{
+    var reduced = arr.Zip(arr.Skip(1), (a, b) => b - a);
+ 
+    if (reduced.Distinct().Count() == 1)
+        return reduced.First();
+    
+    return reduced.Last() + NextTerm(reduced);
+}
+
+var arrs = File.ReadAllLines("input.txt")
+               .Select(l => l.Trim().Split(' ').Select(n => int.Parse(n)).ToArray()).ToList();
+
+var p1 = arrs.Select(arr => arr.Last() + NextTerm(arr)).Sum();
+Console.WriteLine($"P1: {p1}");
+
+//var pairs = nums.Zip(nums.Skip(1), (a, b) => b - a).ToArray();
