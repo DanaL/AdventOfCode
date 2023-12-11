4ee10fd6df35129b5622f4c5421b1c2e713d0182
Dana Larose <ywg.dana@gmail.com>
2023-12-10 21:47:25 -0600

    2023 Day 10 part 1 complete

--- 2023/Day10/Program.cs
+++ 2023/Day10/Program.cs
@@ -1,18 +1,4 @@
-﻿static ((int, int), (int, int)) ASCIIToCoords(char ch) 
-{
-    return ch switch
-    {
-        '|' => ((-1, 0), (1, 0)),
-        '-' => ((0, -1), (0, 1)),
-        'L' => ((-1, 0), (0, 1)),
-        'J' => ((-1, 0), (0, -1)),
-        '7' => ((1, 0), (0, -1)),
-        'F' => ((1, 0), (0, 1)),
-        _ => throw new Exception("Hmm this shouldn't happen")
-    };
-}
-
-static ((int, int), Dictionary<(int, int), ((int, int), (int, int))>) FetchMaze()
+﻿static ((int, int), Dictionary<(int, int), ((int, int), (int, int))>) FetchMaze()
 {
     var sqs = new Dictionary<(int, int), ((int, int), (int, int))>();
     var lines = File.ReadAllLines("input.txt");
@@ -29,7 +15,17 @@ static ((int, int), Dictionary<(int, int), ((int, int), (int, int))>) FetchMaze(
                 start = (r, c);
             else 
             {
-                var exits = ASCIIToCoords(lines[r][c]);
+                //var exits = ASCIIToCoords(lines[r][c]);
+                var exits = lines[r][c] switch
+                {
+                    '|' => ((-1, 0), (1, 0)),
+                    '-' => ((0, -1), (0, 1)),
+                    'L' => ((-1, 0), (0, 1)),
+                    'J' => ((-1, 0), (0, -1)),
+                    '7' => ((1, 0), (0, -1)),
+                    'F' => ((1, 0), (0, 1)),
+                    _ => throw new Exception("Hmm this shouldn't happen")
+                };
                 sqs.Add((r, c), ((r + exits.Item1.Item1, c + exits.Item1.Item2), (r + exits.Item2.Item1, c + exits.Item2.Item2)));
             }
         }            
@@ -42,5 +38,26 @@ static ((int, int), Dictionary<(int, int), ((int, int), (int, int))>) FetchMaze(
     return (start, sqs);
 }
 
+static int FollowTunnels((int, int) start, Dictionary<(int, int), ((int, int), (int, int))> sqs)
+{
+    int steps = 1;
+    (int, int) a = sqs[start].Item1;
+    (int, int) b = sqs[start].Item2;
+    (int, int) prevA = start, prevB = start;
+
+    do 
+    {
+        var nextA = sqs[a].Item1 == prevA ? sqs[a].Item2 : sqs[a].Item1;
+        var nextB = sqs[b].Item1 == prevB ? sqs[b].Item2 : sqs[b].Item1;
+        prevA = a;
+        prevB = b;
+        a = nextA;
+        b = nextB;
+        ++steps;
+    } while (a != b);
+
+    return steps;
+}
+
 var (start, sqs) = FetchMaze();
-//Console.WriteLine(sqs[(3, 1)]);
+Console.WriteLine($"P1 {FollowTunnels(start, sqs)}");
