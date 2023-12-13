fa269aaa40130ea008e11696fc6c4535a6a15020
Dana Larose <ywg.dana@gmail.com>
2023-12-13 07:40:56 -0600

    2023 day 13 part 1 complete

--- 2023/Day13/Program.cs
+++ 2023/Day13/Program.cs
@@ -1,2 +1,46 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System.Text;
+
+static uint ToNum(string line) => Convert.ToUInt32(line.Replace('.', '0').Replace('#', '1').PadLeft(32, '0'), 2);
+
+static bool IsReflection(uint[] lines, int a, int b)
+{
+    while (a >= 0 && b < lines.Length)
+    {
+        if (lines[a--] != lines[b++])
+            return false;
+    }
+
+    return true;
+}
+
+static int ReflectionInArray(uint[] arr)
+{
+    for (int j = 0; j < arr.Length - 1; j++)
+    {
+        if (arr[j] == arr[j + 1] && IsReflection(arr, j, j + 1)) 
+            return j + 1;
+    }
+    
+    return 0;
+}
+
+static int CheckForRowReflect(string[] block) => ReflectionInArray(block.Select(line => ToNum(line)).ToArray());
+
+static int CheckForColumnReflection(string[] block)
+{
+    uint[] cols = new uint[block[0].Length];
+    for (int c = 0; c < block[0].Length; c++) 
+    {
+        StringBuilder col = new StringBuilder();
+        for (int r = 0; r < block.Length; r++)
+            col.Append(block[r][c]);
+        cols[c] = ToNum(col.ToString());
+    }
+
+    return ReflectionInArray(cols);
+}
+
+var input = File.ReadAllText("input.txt").Split("\n\n").Select(b => b.Split('\n')).ToList();
+
+int p1 = input.Select(CheckForRowReflect).Sum() * 100 + input.Select(CheckForColumnReflection).Sum();
+Console.WriteLine($"P1: {p1}");
