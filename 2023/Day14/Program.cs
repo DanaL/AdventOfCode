56a3926408846ac058208d00541f277a71b9d35a
Dana Larose <ywg.dana@gmail.com>
2023-12-14 07:09:36 -0600

    2023 Day 14 part 1 done

--- 2023/Day14/Program.cs
+++ 2023/Day14/Program.cs
@@ -1,2 +1,60 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System.Text;
+
+static void MoveRocks(char[,] map, int size)
+{
+    for (int r = 1; r < size; r++)
+    {
+        for (int c = 0; c < size; c++)
+        {
+            if (map[r, c] == 'O')
+            {
+                int nr = r;
+                while (nr - 1 >= 0 && map[nr - 1, c] == '.') 
+                {
+                    map[nr - 1, c] = 'O';
+                    map[nr, c] = '.';
+                    --nr;
+                }
+            }
+        }
+    }
+}
+
+static int CountLoad(char[,] map, int size)
+{
+    int load = 0;
+    for (int r = 0; r < size; r++) 
+    {
+        for (int c = 0; c < size; c++) 
+        {
+            if (map[r, c] == 'O')
+                load += size - r;
+        }
+    }
+
+    return load;
+}
+
+var input = File.ReadAllLines("input.txt");
+int size = input.Length;
+
+char[,] map = new char[size, size];
+
+for (int r = 0; r < size; r++) 
+{
+    for (int c = 0; c < size; c++) 
+    {
+        map[r, c] = input[r][c];
+    }
+}
+
+MoveRocks(map, size);
+Console.WriteLine($"P1: {CountLoad(map, size)}");
+
+// for (int r = 0; r < size; r++) 
+// {
+//     var sb = new StringBuilder();
+//     for (int c = 0; c < size; c++) 
+//         sb.Append(map[r, c]);
+//     Console.WriteLine(sb.ToString());
+// }
