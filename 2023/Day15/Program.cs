75e38c6fc59a5469790d3b2188c30a5f53649d5b
Dana Larose <ywg.dana@gmail.com>
2023-12-15 09:49:59 -0600

    2023 Day 15 part 2 done

--- 2023/Day15/Program.cs
+++ 2023/Day15/Program.cs
@@ -13,3 +13,48 @@
 
 var steps = File.ReadAllText("input.txt").Trim().Split(',');
 Console.WriteLine($"P1: {steps.Select(Hash).Sum()}");
+
+static void UpdateBox(Dictionary<int, List<(string, int)>> boxes, string instruction)
+{   
+    int eq = instruction.IndexOf('=');
+    if (eq > -1)
+    {
+        string label = instruction.Substring(0, eq);
+        int box = Hash(label);
+        int i = boxes[box].FindIndex(p => p.Item1 == label);
+        if (i == -1)
+            boxes[box].Add((label, int.Parse(instruction.Substring(eq + 1))));
+        else
+            boxes[box][i] = (label, int.Parse(instruction.Substring(eq + 1)));
+    }
+    else
+    {
+        string label = instruction.Substring(0, instruction.IndexOf('-'));
+        int box = Hash(label);
+        boxes[box].RemoveAll(p => p.Item1 == label);
+    }
+}
+
+static int CalcFocusPower(Dictionary<int, List<(string, int)>> boxes)
+{
+    int fp = 0;
+    for (int j = 0; j < 256; j++)
+    {
+        int sum = 0;
+        for (int k = 0; k < boxes[j].Count; k++)
+            sum += (k + 1) * boxes[j][k].Item2;
+        fp += (j + 1) * sum;
+    }
+
+    return fp;
+}
+
+var boxes = new Dictionary<int, List<(string, int)>>();
+for (int j = 0; j < 256; j++)
+    boxes[j] = new List<(string, int)>();
+
+foreach (string s in steps)
+    UpdateBox(boxes, s);
+
+Console.WriteLine($"P2: {CalcFocusPower(boxes)}");
+
