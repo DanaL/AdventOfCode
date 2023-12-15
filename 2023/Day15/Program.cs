9c2f18538736220f212d13cd5f8133134d919ba1
Dana Larose <ywg.dana@gmail.com>
2023-12-15 07:14:49 -0600

    2023 Day 15 part 1 done

---
+++ 2023/Day15/Program.cs
@@ -0,0 +1,15 @@
+﻿static int Hash(string s)
+{
+    int v = 0;
+    foreach (char c in s)
+    {
+        v += (int)c;
+        v *= 17;
+        v %= 256;
+    }
+
+    return v;
+}
+
+var steps = File.ReadAllText("input.txt").Trim().Split(',');
+Console.WriteLine($"P1: {steps.Select(Hash).Sum()}");
