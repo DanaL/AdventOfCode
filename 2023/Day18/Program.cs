5aa58ad9738780be84faae974944f7c14f8b0e78
Dana Larose <ywg.dana@gmail.com>
2023-12-18 16:55:43 -0600

    2023 Day 18 part 2 done

--- 2023/Day18/Program.cs
+++ 2023/Day18/Program.cs
@@ -1,42 +1,62 @@
-﻿static void Part1()
+﻿using Day18;
+
+static (char, long) ParseLine(string line)
 {
-    var instrs = File.ReadAllLines("input.txt").Select(l => l.Split(' '))
-                     .Select(l => (l[0][0], int.Parse(l[1]))).ToList();
-    HashSet<(int, int)> sqs = [(0, 0)];
-    List<(int, int)> neighours = [ (0, 1), (0, -1), (-1, 0), (1, 0)];
-
-    int r = 0, c = 0;
-    foreach (var instr in instrs)
+    int i = line.IndexOf('#') + 1;
+    long d = Convert.ToInt64(line[i..(i+5)], 16);
+    char ch = line[i + 5] switch
     {
-        (int Row, int Col) dir = instr.Item1 switch
-        {
-            'R' => neighours[0],
-            'L' => neighours[1],
-            'U' => neighours[2],
-            _ => neighours[3]
-        };
-        for (int n = 0; n < instr.Item2; n++, r+= dir.Row, c += dir.Col)
-            sqs.Add((r, c));
-    }
+        '0' => 'R',
+        '1' => 'D',
+        '2' => 'L',
+        _ => 'U'
+    };
 
-    // Fill in interior (not strictly needed for pt 1 but I assume I'll need to for pt 2)    
-    var q = new Queue<(int Row, int Col)>();
-    q.Enqueue((1, 1));
-    while (q.Count > 0)
-    {
-        var sq = q.Dequeue();
-        if (sqs.Contains(sq))
-            continue;        
-        sqs.Add(sq);
-        foreach (var n in neighours)
-        {
-            var next = (sq.Item1 + n.Item1, sq.Item2 + n.Item2);
-            if (!sqs.Contains(next)) 
-                q.Enqueue(next);            
-        }
-    }
-
-    Console.WriteLine($"P1: {sqs.Count}");
+    return (ch, d);
 }
 
-Part1();
+static long Determinant(long x1, long y1, long x2, long y2)
+{
+    return x1 * y2 - x2 * y1;
+}
+
+static long CalcArea2(List<(char, long)> instrs)
+{
+    Pt pt = new Pt(0, 0);
+    List<Pt> pts = [new Pt(0, 0)];
+    long perimeter = 0;
+
+    foreach (var p in instrs)
+    {
+        long d = p.Item2;        
+        pt = p.Item1 switch
+        {
+            'R' => pt with { X = pt.X + d},
+            'L' => pt with { X = pt.X - d},
+            'U' => pt with { Y = pt.Y + d},
+            _ => pt with { Y = pt.Y - d}
+        };
+
+        perimeter += d;
+        pts.Add(pt);        
+    }
+
+    // The shoelace formula sums the determinants of the points in sequence. These are the interior
+    // squares but we need to add the perimiter and divide by 2 + 1 because we have chonky ASCII lines
+    long area = 0;
+    for (int j = 0; j < pts.Count - 1; j++)
+          area += Determinant(pts[j].X, pts[j].Y, pts[j + 1].X, pts[j + 1].Y);
+    area = Math.Abs(area);
+        
+    return (area + perimeter) / 2 + 1;
+}
+
+var instrs = File.ReadAllLines("input.txt").Select(l => l.Split(' '))
+                 .Select(l => (l[0][0], long.Parse(l[1]))).ToList();
+Console.WriteLine($"P1: {CalcArea2(instrs)}");
+Console.WriteLine($"P2: {CalcArea2(File.ReadAllLines("input.txt").Select(ParseLine).ToList())}");
+
+namespace Day18
+{
+    public record Pt(long X, long Y);
+}
