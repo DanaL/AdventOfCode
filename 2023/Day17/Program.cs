e0300608c9b0a8edec2cd88fdec8cc06acf62eaa
Dana Larose <ywg.dana@gmail.com>
2023-12-18 03:33:45 -0600

    2023 day 17 project file/program stub/reading input

---
+++ 2023/Day17/Program.cs
@@ -0,0 +1,11 @@
+﻿var lines = File.ReadAllLines("input.txt");
+int size = lines.Length;
+int[,] map = new int[size, size];
+
+for (int r = 0; r < size; r++)
+{
+    for (int c = 0; c < size; c++)
+        map[r, c] = Convert.ToInt32(lines[r][c].ToString());
+}
+
+Console.WriteLine(map[3,7]);
