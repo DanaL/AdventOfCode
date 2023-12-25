f3c70acc7d12a8f9e13e76c7191873a19e2a54ef
Dana Larose <ywg.dana@gmail.com>
2023-12-25 05:37:52 -0600

    Another not working attempt at day 17

--- 2023/Day17/Program.cs
+++ 2023/Day17/Program.cs
@@ -3,25 +3,25 @@ using Day17;
 
 static int Manhattan((int, int) a, (int, int) b) => Math.Abs(a.Item1 - b.Item1) + Math.Abs(a.Item2 - b.Item2);
 
-static List<Node> Neighbours(Node node, int size)
+static List<Route> Neighbours2(Route route, int size)
 {
-    var neighbours = new List<Node>();
-
-    //(int, int)[] adj = [ (-1, 0), (1, 0), (0, 1), (0, -1)];
+    var neighbours = new List<Route>();
     (int, int)[] adj = new (int, int)[] { (-1, 0), (1, 0), (0, 1), (0, -1) };
+
     foreach (var a in adj)
     {
-        (int, int) n = (node.Sq.Item1 + a.Item1, node.Sq.Item2 + a.Item2);        
-        var moves = node.Moves.TakeLast(3).ToList();
-        
-        // If the last 3 moves have been in the same direction, we can't go that way
-        if (moves.Where(p => p == a).Count() >= 3 && moves[0] == a)
+        (int, int) n = (route.Head.Item1 + a.Item1, route.Head.Item2 + a.Item2);
+        if (n.Item1 < 0 || n.Item2 < 0 || n.Item1 >= size || n.Item2 >= size)
             continue;
 
-        if (n.Item1 < 0 ||  n.Item2 < 0 || n.Item1 >= size || n.Item2 >= size) 
-            continue;
+        // We can't go backwards
+        if (route.Nodes.Count > 2)
+        {
+            var secondLast = route.Nodes[route.Nodes.Count - 2];
+            if (n == secondLast)
+                continue;
+        }
 
-        var path = node.Path.Select(n => n).ToList();
         char ch = a switch
         {
             (-1, 0) => '^',
@@ -30,20 +30,28 @@ static List<Node> Neighbours(Node node, int size)
             _ => '>'
         };
 
-        path.Add((n.Item1, n.Item2, ch));
-        moves.Add(a);
-        neighbours.Add(new Node(n, moves, path));
+        // If we've gone the same direction 3 times in a row, we can't go
+        // in that direction again
+        var moves = route.Moves.TakeLast(3)
+                         .Where(mv => mv == ch).Count();
+        if (moves >= 3)
+            continue;
+
+        var neighbour = route.Copy();
+        neighbour.Nodes.Add(n);
+        neighbour.Moves.Add(ch);
+        neighbours.Add(neighbour);
     }
 
-    return neighbours;    
+    return neighbours;
 }
 
-static void DumpPath(int[,] map, int size, Node goal)
+static void DumpPath(int[,] map, int size, Route goal)
 {
     var goalPath = new Dictionary<(int, int), char>();
-    foreach (var p in goal.Path)
+    for (int m = 0; m < goal.Moves.Count; m++)
     {
-        goalPath.Add((p.Item1, p.Item2), p.Item3);
+        goalPath.Add(goal.Nodes[m + 1], goal.Moves[m]);
     }
 
     for (int r = 0; r < size; r++)
@@ -51,7 +59,7 @@ static void DumpPath(int[,] map, int size, Node goal)
         for (int c = 0; c < size; c++)
         {
             if (goalPath.ContainsKey((r, c)))
-                Console.Write(goalPath[(r,c)]);
+                Console.Write(goalPath[(r, c)]);
             else
                 Console.Write(map[r, c]);
         }
@@ -59,40 +67,55 @@ static void DumpPath(int[,] map, int size, Node goal)
     }
 }
 
-static int Pathfind(int[,] map, int size)
+static int Pathfind2(int[,] map, int size)
 {
-    var goal = (size - 1, size - 1);
-    var q = new PriorityQueue<Node, int>();
-    q.Enqueue(new Node((0, 0), new List<(int, int)>(), new List<(int, int, char)>()), 0);
-    var costs = new Dictionary<(int, int), int>() { { (0, 0), 0 } };
-    
+    var goal = (size - 1, size -1);
+    var q = new PriorityQueue<Route, int>();
+    var start = new Route();
+    start.Nodes.Add((0, 0));
+    q.Enqueue(start, 0);
+    var costs = new Dictionary<(int, int), int>() { { (0, 0), 2 } };
+    int cheapest = int.MaxValue;
+
     while (q.Count > 0)
     {
         var curr = q.Dequeue();
 
-        if (curr.Sq == goal)
-        {
-            Console.WriteLine($"hurrah we're done! {costs[goal]} {costs[curr.Sq]}, cost to (5,11): {costs[(5,11)]}");        
-            DumpPath(map, size, curr);
+        if (curr.Cost >= cheapest)
+            continue;
 
-            var pathCost = curr.Path.Select(p => map[p.Item1, p.Item2]).Sum();
-            foreach (var p in curr.Path)
-                 Console.WriteLine($"{p.Item1},{p.Item2} -> {map[p.Item1, p.Item2]}");
-            Console.WriteLine($"Path cost: {pathCost}");
-            return costs[goal];            
-        }
-        foreach (var n in Neighbours(curr, size))
+        if (curr.Head == goal)
         {
-            int nextCost = costs[curr.Sq] + map[n.Sq.Item1, n.Sq.Item2];
-            if (!costs.ContainsKey(n.Sq) || nextCost <= costs[n.Sq])
+            if (curr.Cost < cheapest)
+                cheapest = curr.Cost;
+            Console.WriteLine($"hurrah! {curr.Cost}");
+            var pathCost = curr.Nodes.Select(p => map[p.Item1, p.Item2]).Sum();
+            Console.WriteLine($"Path cost: {pathCost}");
+            //DumpPath(map, size, curr);
+            continue;
+        }
+
+        // This is causing me to miss moves it's more expensive to get to
+        // a node along one path, but will ultimately be cheaper because
+        // of the no three-in-a-row rule so I need to prune fewer routes
+        // (but also not be a computational nightmare)
+        if (costs.TryGetValue(curr.Head, out int value) && curr.Cost - value > 2)
+        {
+            continue;
+        }
+
+        foreach (var n in Neighbours2(curr, size))
+        {
+            n.Cost += map[n.Head.Item1, n.Head.Item2];
+            if (!costs.ContainsKey(n.Head) || n.Cost < costs[n.Head])
             {
-                costs[n.Sq] = nextCost;
-                q.Enqueue(n, nextCost + Manhattan(n.Sq, goal));
-            }            
+                costs[n.Head] = n.Cost;                
+            }
+            q.Enqueue(n, n.Cost + Manhattan(n.Head, goal));
         }
     }
 
-    return -1;
+    return costs[goal];
 }
 
 var lines = File.ReadAllLines("input.txt");
@@ -105,9 +128,35 @@ for (int r = 0; r < size; r++)
         map[r, c] = Convert.ToInt32(lines[r][c].ToString());
 }
 
-Console.WriteLine(Pathfind(map, size));
+Console.WriteLine(Pathfind2(map, size));
 
 namespace Day17
 {
-    record Node((int, int) Sq, List<(int, int)> Moves, List<(int, int, char)> Path);
+    class Route
+    {
+        public List<(int, int)> Nodes { get; set; }
+        public List<char> Moves { get; set; }
+        public int Cost { get; set; }
+
+        public (int, int) Head => Nodes.Count > 0 ? Nodes.Last() : (-1, -1);
+
+        public Route() 
+        {
+            Nodes = new List<(int, int)>();
+            Moves = new List<char>();
+            Cost = 0;
+        }
+
+        public Route Copy()
+        {
+            var copy = new Route
+            {
+                Nodes = Nodes.Select(n => n).ToList(),
+                Moves = Moves.Select(m => m).ToList(),
+                Cost = Cost
+            };
+
+            return copy;
+        }
+    }
 }
