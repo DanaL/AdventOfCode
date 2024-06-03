91e8903519f81861361e16dd932720354e3ea371
Dana Larose <ywg.dana@gmail.com>
2024-06-03 13:12:20 -0500

    2016 Day 3 part 2 done

--- 2016/day03.c
+++ 2016/day03.c
@@ -1,6 +1,8 @@
 #include <stdio.h>
 #include <stdbool.h>
 
+#define TABLE_SIZE 2000
+
 bool valid(int a, int b, int c)
 {
   if (a + b <= c)
@@ -13,7 +15,7 @@ bool valid(int a, int b, int c)
   return true;
 }
 
-int main(void)
+void p1(void)
 {
   FILE *fp;
   char line[20];
@@ -28,7 +30,52 @@ int main(void)
       ++valid_count;
   }
  
-  printf("P1: %d", valid_count);
+  printf("P1: %d\n", valid_count);
 
   fclose(fp);
+}
+
+// return the # of valids triangles found in a column
+int check_col(int *arr, int len)
+{
+  int valid_count = 0;
+  for (int j = 0; j < len - 2; j += 3) {
+    if (valid(arr[j], arr[j+1], arr[j+2])) {
+      ++valid_count;
+    }
+  }
+
+  return valid_count;
+}
+
+void p2(void)
+{
+  int col_a[TABLE_SIZE];
+  int col_b[TABLE_SIZE];
+  int col_c[TABLE_SIZE];
+  FILE *fp;
+  char line[20];
+
+  fp = fopen("inputs/day03.txt", "r");
+
+  int a, b, c;
+  int n = 0;
+  while (fgets(line, sizeof line, fp) != NULL) {
+    sscanf(line, "%d %d %d", &a, &b, &c);
+    col_a[n] = a;
+    col_b[n] = b;
+    col_c[n] = c;
+    ++n;
+  }
+
+  int total = check_col(col_a, n) + check_col(col_b, n) + check_col(col_c, n);
+  printf("P2: %d", total);
+
+  fclose(fp);
+}
+
+int main(void)
+{
+  p1();
+  p2();
 }
