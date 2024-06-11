74a9927f0982a103e5731e448d27e247e207323c
Dana Larose <ywg.dana@gmail.com>
2024-06-11 15:47:01 -0500

    Still working on 2016 day 10

--- 2016/day10.c
+++ 2016/day10.c
@@ -1,9 +1,8 @@
 #include <stdio.h>
+#include <stdbool.h>
 #include <stdlib.h>
 #include <string.h>
 
-#define BOT 0
-#define OUTPUT 1
 #define LINE_W 100
 
 struct bot {
@@ -21,10 +20,60 @@ struct rule {
   int hi_val;
 };
 
+int get_val(char *s, char *word)
+{
+  char *v = s + strlen(word) + 1;
+  char *w = v + 1;
+  while (*w != ' ' && *w != '\0')
+    ++w;
+  int len = w - v;
+  char buffer[100];
+  strncpy(buffer, v, len);
+  buffer[len] = '\0';
+
+  int val;
+  sscanf(buffer, "%d", &val);
+
+  return val;
+}
+
+int find_max_val(char *s, char *word)
+{
+  char *sub_str = s;
+  char *w;
+  int max = -1;
+
+  while ((w = strstr(sub_str, word)) != NULL) {
+    if (w != NULL) {
+      int val = get_val(w, word);
+      if (val > max)
+        max = val;
+    }
+    sub_str = w + 1;
+  }
+  
+  return max;
+}
+
+bool contains(char *s, char *word, int target)
+{
+  char *sub_str = s;
+  char *w;
+
+  while ((w = strstr(sub_str, word)) != NULL) {
+    if (w != NULL) {
+      int val = get_val(w, word);
+      if (val == target)
+        return true;
+    }
+    sub_str = w + 1;
+  }
+
+  return false;
+}
+
 int main(void)
 {
-  struct bot *bots = NULL;
-  int bot_count = 0;
   struct rule *rules = NULL;
   int rule_count = 0;
 
@@ -66,20 +115,32 @@ int main(void)
   }
   fclose(fp);
 
-  for (int i = 0; i < lc; i++)
-    printf("%s\n", lines[i]);
-  /*
-  for (int i = 0; i < bot_count; i++)
-      printf("bot %d:\n  val 1: %d\n  val 2: %d\n", bots[i].id, bots[i].val1, bots[i].val2);
+  int max_bot = 0, max_output = 0;
+  for (int i = 0; i < lc; i++) {
+    int max = find_max_val(lines[i], "bot"); 
+    if (max > max_bot)
+      max_bot = max;
+    max = find_max_val(lines[i], "output");
+    if (max > max_output)
+      max_output = max;
+  }
 
-  for (int i = 0; i < rule_count; i++)
-    printf("bot %d gives low to %d and hi to %d\n", rules[i].bot_id, rules[i].lo, rules[i].hi);
+  int *outputs = calloc(max_output + 1, sizeof(int));
+  struct bot *bots = calloc(max_bot + 1, sizeof(struct bot));
+  for (int i = 0; i < max_bot; i++) {
+    bots[i].id = i;
+    bots[i].val1 = -1;
+    bots[i].val2 = -1;
+  }
 
-  free(bots);
-  free(rules);
-  */
+  for (int j = 0; j < lc; j++) {
+    
+  }
 
+  // end-of-program cleanup
   for (int i = 0; i < lc; i++)
     free(lines[i]);
   free(lines);
+  free(bots);
+  free(outputs);
 }
