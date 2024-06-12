9c67226d64972bbf56c352258ae2b32c741a92cc
Dana Larose <ywg.dana@gmail.com>
2024-06-12 13:17:36 -0500

    2016 Day 5 -- converting the txt to be hashed into padded char array

--- 2016/day05.c
+++ 2016/day05.c
@@ -1,4 +1,37 @@
 #include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <stdint.h>
+
+char *pad(char *txt)
+{
+  int len = strlen(txt);
+  int padded_len = len + (63 - len % 64);
+
+  char *padded = calloc(padded_len, sizeof(char));
+  memcpy(padded, txt, len);
+  padded[len] = 0x80;
+
+  uint64_t og_len_in_bits = (uint64_t)len * 8;
+ 
+  // If the length of the text to be hashed can't be represented in 64 bits,
+  // we'd use the 64 least significant bits but I'm not going to worry about
+  // that here.
+  int len_bit_count = 0;
+  uint8_t bytes[8];
+  while (og_len_in_bits > 0) {
+    bytes[len_bit_count++] = og_len_in_bits & 255;
+    og_len_in_bits = og_len_in_bits >> 8;
+  }
+
+  // Copy the byte representation into the last 8 bytes
+  int i = padded_len - 8;
+  for (int j = 0; j < len_bit_count; j++) {
+    padded[i++] = bytes[j];
+  }
+
+  return padded;
+}
 
 int main(void)
 {
@@ -26,4 +59,9 @@ int main(void)
     0x6fa87e4f, 0xfe2ce6e0, 0xa3014314, 0x4e0811a1,
     0xf7537e82, 0xbd3af235, 0x2ad7d2bb, 0xeb86d391
   };
+
+  char *txt = "Hello, world?0";
+  char *padded = pad(txt);
+
+  free(padded);
 }
