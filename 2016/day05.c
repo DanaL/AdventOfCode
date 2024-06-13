09bfcc7c3a2fd77f812e3d2b64c9b3569a0b36cc
Dana Larose <ywg.dana@gmail.com>
2024-06-13 07:43:55 -0500

    2016 day 5 part 2 done

--- 2016/day05.c
+++ 2016/day05.c
@@ -54,9 +54,8 @@ uint32_t to_32b_word(uint32_t a, uint32_t b, uint32_t c, uint32_t d)
 
 // 01111011 10000010 00001111 10011001
 // 153 15 130 123
-char *word_to_bytes(uint32_t x)
+char *word_to_bytes(uint32_t x, char *s)
 {
-  char *s = malloc(9 * sizeof(char));
   uint8_t a = (x & 255);
   uint8_t b = (x & 65280) >> 8;
   uint8_t c = (x & 16711680) >> 16;
@@ -151,21 +150,20 @@ char *md5(char *txt)
   }
 
   char *hex_str = malloc(33 * sizeof(char));
+  char hex_bytes[9];
   offset = 0;
   uint32_t bytes[] = { a0, b0, c0, d0 };
   for (int j = 0; j < 4; j++) {
-    char *hex_bytes = word_to_bytes(bytes[j]);
+    word_to_bytes(bytes[j], hex_bytes);
     strcpy(hex_str + offset, hex_bytes);
-    free(hex_bytes);
     offset += 8;
   }
-  
   free(padded);
 
   return hex_str;
 }
 
-int main(void)
+void p1(void)
 {
   char pwd[9] = { '\0' };
   int digit = 0;
@@ -176,14 +174,47 @@ int main(void)
     char *s = md5(buffer);
     if (strncmp(s, "00000", 5) == 0) {
       pwd[digit++] = s[5];
-      printf("%s\n", buffer);
-      printf("%s\n", s);
     }
     free(s);
     x++;
   }
 
   printf("P1: %s\n", pwd);
+}
+
+void p2(void)
+{
+  char pwd[9];
+  for (int j = 0; j < 8; j++)
+    pwd[j] = '_';
+  pwd[8] = '\0';
+  printf("%s\n", pwd);
+  int i = 0;
+  uint32_t x = 0;
+  char buffer[100];
+  while (i < 8) {
+    sprintf(buffer, "uqwqemis%d", x);
+    char *s = md5(buffer);
+    if (strncmp(s, "00000", 5) == 0) {
+      int spot = s[5] - '0';
+      char digit = s[6];      
+      if (spot >= 0 && spot < 8 && pwd[spot] == '_') {
+        pwd[spot] = digit;        
+        printf("%  s\n", pwd);
+        ++i;
+      }
+    }
+    free(s);
+    x++;
+  }
+
+  printf("P2: %s\n", pwd);
+}
+
+int main(void)
+{
+  p1();
+  p2();
 }
 
 
