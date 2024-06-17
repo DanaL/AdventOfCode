2953eda5e783d3d4f8d3fa29a04e720a5ce62dd7
Dana Larose <ywg.dana@gmail.com>
2024-06-16 19:03:07 -0500

    2016 day 11: working on validation function for checking states

--- 2016/day11.c
+++ 2016/day11.c
@@ -9,6 +9,9 @@
 
 #define VT_SIZE 101719
 
+#define NUM_OF_ELTS 2
+#define STATE_LEN (2 * NUM_OF_ELTS + 1)
+
 struct vt_entry {
   char *key;
   struct vt_entry *next;
@@ -87,19 +90,117 @@ bool visited_table_contains(struct vt_entry  **vt, const char *key)
   return false;
 }
 
+bool valid_state(uint8_t *state)
+{  
+  // check where the elevator is
+  if (state[0] < 1 || state[0] > 4) {
+    return false;
+  }
+
+  // check to make sure no elements are on a floor with a
+  // generator when their generator isn't present
+  for (int i = 1; i < STATE_LEN; i += 2) {
+    // if the generator is on the same floor as the element,
+    // we're good, otherwise we have to make sure there are
+    // no other generators on the same floor.
+    uint8_t elt_floor = state[i];
+    uint8_t elt_gen_floor = state[i+1];
+    if (elt_floor == elt_gen_floor) {
+      continue;
+    }
+
+    for (int j = 2; j < STATE_LEN; j += 2) {
+      if (state[j] == elt_floor) {
+        return false;
+      }
+    }
+  }
+
+  return true;
+}
+
+// moves should start off as null
+void find_valid_moves(uint8_t **moves, int *moves_len, uint8_t *curr_state)
+{
+  uint8_t elevator = curr_state[0];
+  uint8_t *other_state = malloc(STATE_LEN * sizeof(uint8_t));
+
+  printf("Elevator at: %d\n", elevator);
+  
+  // I'm going to loop over each thing on the floor and see what is
+  // possible to be moved. Nested loop to check all possible combos?
+  for (int i = 1; i < STATE_LEN; i++) {
+    if (curr_state[i] == elevator) {
+      for (int j = 0; j < STATE_LEN; j++)
+        other_state[j] = curr_state[j];
+
+      other_state[0] = elevator + 1;
+      other_state[i] = curr_state[i] + 1;
+      printf("Checking: \nup ");
+      for (int k = 0; k < STATE_LEN; k++)
+        printf(" %d ", other_state[k]);
+      printf("\n");
+      
+      printf("  valid? %d\n", valid_state(other_state));
+      other_state[0] = elevator - 1;
+      other_state[i] = curr_state[i] - 1;
+      printf(" down ");
+      for (int k = 0; k < STATE_LEN; k++)
+        printf(" %d ", other_state[k]);
+      printf("\n  valid? %d\n\n", valid_state(other_state));
+
+      // for (int j = i + 1; j < STATE_LEN; j++) {
+      //   if (curr_state[i] == curr_state[j]) {
+          
+      //     printf("  -- possibly can also move %d\n", j);
+      //   } 
+      // }
+    }
+  }
+  
+  free(other_state);
+
+  *moves_len = 44;  
+}
+
 int main(void)
 { 
   struct vt_entry **vt = visited_table_create();
 
-  visited_table_insert(vt, "hello, world?");
-  visited_table_insert(vt, "test");
-  visited_table_insert(vt, "lorem ipsum");
+  // visited_table_insert(vt, "hello, world?");
+  // visited_table_insert(vt, "test");
+  // visited_table_insert(vt, "lorem ipsum");
 
-  char *s = "foo";
-  printf("%d\n", visited_table_contains(vt, "hello, world?"));
-  printf("%d\n", visited_table_contains(vt, "hello, world!"));
-  printf("%d\n", visited_table_contains(vt, "test"));
-  printf("%d\n", visited_table_contains(vt, s));
+  // char *s = "foo";
+  // printf("%d\n", visited_table_contains(vt, "hello, world?"));
+  // printf("%d\n", visited_table_contains(vt, "hello, world!"));
+  // printf("%d\n", visited_table_contains(vt, "test"));
+  // printf("%d\n", visited_table_contains(vt, s));
+
+  uint8_t *state = calloc(STATE_LEN, sizeof(uint8_t));
+  state[0] = 1; // elevator
+  
+  // in order of chip then generator so chips are ood indexes
+  // and their corresponding generators are even indexes
+  state[1] = 1;
+  state[2] = 2;
+  state[3] = 1;
+  state[4] = 3;
+
+  uint8_t **next_moves = NULL;
+  int moves_len = 0;
+
+  find_valid_moves(next_moves, &moves_len, state);
+
+  //  2  2  2  1  3 
+  state[0] = 2;
+  state[1] = 2;
+  state[2] = 2;
+  state[3] = 1;
+  state[4] = 3;
+  printf("fucking valid? %d\n", valid_state(state));
 
   visited_table_destroy(vt);
+
+  free(state);
 }
