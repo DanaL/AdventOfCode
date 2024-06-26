53294e08dfb251dc27bacc7e6ba96583504f93cf
Dana Larose <ywg.dana@gmail.com>
2024-06-25 21:06:17 -0500

    2016 day 16 part 2 done

--- 2016/day16.c
+++ 2016/day16.c
@@ -2,15 +2,14 @@
 #include <stdlib.h>
 #include <string.h>
 
-char *iterate(const char *a, size_t len)
+char *iterate(const char *a, uint32_t len)
 {
-  size_t b_len = 2 * len + 2;
-
+  uint32_t b_len = 2 * len + 2;
   char *result = malloc(b_len * sizeof(char));
   memcpy(result, a, len);
   result[len] = '0';
 
-  for (size_t j = 0; j < len; j++) {
+  for (uint32_t j = 0; j < len; j++) {
     result[b_len - 2 - j] = a[j] == '0' ? '1' : '0';
   }
 
@@ -19,12 +18,12 @@ char *iterate(const char *a, size_t len)
   return result;
 }
 
-void p1(const char *initial, size_t data_len)
+void dragon_curve(const char *initial, uint32_t data_len)
 {
-  char data[1000];
+  char *data = malloc(data_len * 2 * sizeof(char));
   strcpy(data, initial);
   data[data_len] = '\0';
-  size_t len = strlen(data);
+  uint32_t len = strlen(data);
 
   while (len < data_len) {
     char *b = iterate(data, len);
@@ -32,13 +31,12 @@ void p1(const char *initial, size_t data_len)
     strcpy(data, b);
     free(b);
   }
-
   data[data_len] = '\0';
   
-  char checksum[1000];
+  char *checksum = malloc(data_len * 2 * sizeof(char));
   memcpy(checksum, data, data_len);
-  char tmp[1000];
-  size_t checksum_len = data_len;
+  char *tmp = malloc(data_len * 2 * sizeof(char));
+  uint32_t checksum_len = data_len;
   while (checksum_len % 2 == 0) {
     for (int j = 0; j < checksum_len; j += 2) {
       char bit = checksum[j] == checksum[j+1] ? '1' : '0';
@@ -49,10 +47,15 @@ void p1(const char *initial, size_t data_len)
     checksum[checksum_len] = '\0';
   }
   checksum[checksum_len] = '\0';
-  printf("P1: %s\n", checksum);
+  printf("%s\n", checksum);
+
+  free(data);
+  free(tmp);
+  free(checksum);
 }
 
 int main(void)
 {
-  p1("10111011111001111", 272);
+  dragon_curve("10111011111001111", 272);
+  dragon_curve("10111011111001111", 35651584);
 }
