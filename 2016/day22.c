9f387558f427bab33c24074f234aae8e88314722
Dana Larose <ywg.dana@gmail.com>
2024-07-17 13:32:46 -0500

    2016 day 22 part 2. Although part 2 was just printing the map and then calculating the count by hand...

--- 2016/day22.c
+++ 2016/day22.c
@@ -1,4 +1,5 @@
 #include <stdio.h>
+#include <stdint.h>
 #include <stdlib.h>
 #include <string.h>
 #include "utils.h"
@@ -26,7 +27,21 @@ int scan_int(char *s, int *pos)
   return val;
 }
 
-int main(void)
+struct node *node_from_line(char *s)
+{
+  struct node *n = malloc(sizeof(struct node));
+  int pos = 0;    
+  n->x = scan_int(s, &pos);
+  n->y = scan_int(s, &pos);
+  n->size = scan_int(s, &pos);
+  n->used = scan_int(s, &pos);
+  n->available = scan_int(s, &pos);
+  n->used_p = scan_int(s, &pos);
+
+  return n;
+}
+
+void p1(void)
 {
   size_t lc;
   char **lines = read_all_lines("inputs/day22.txt", &lc);
@@ -34,13 +49,14 @@ int main(void)
   struct node *nodes = malloc(lc * sizeof(struct node));
 
   for (int j = 0; j < lc; j++) {
-    int pos = 0;    
-    nodes[j].x = scan_int(lines[j], &pos);
-    nodes[j].y = scan_int(lines[j], &pos);
-    nodes[j].size = scan_int(lines[j], &pos);
-    nodes[j].used = scan_int(lines[j], &pos);
-    nodes[j].available = scan_int(lines[j], &pos);
-    nodes[j].used_p = scan_int(lines[j], &pos);
+    struct node *n = node_from_line(lines[j]);
+    nodes[j].x = n->x;
+    nodes[j].y = n->y;
+    nodes[j].size = n->size;
+    nodes[j].used = n->used;
+    nodes[j].available = n->available;
+    nodes[j].used_p = n->used_p;
+    free(n);
   }
   lines_free(lines, lc);
 
@@ -54,11 +70,76 @@ int main(void)
         continue;
       if (nodes[i].used < nodes[j].available)
         ++viable;
-      //else if (nodes[j].used > 0 && nodes[j].used < nodes[i].available)
-      //  ++viable;
     } 
   }
   printf("P1: %d\n", viable);
 
   free(nodes);
+}
+
+void p2(void)
+{
+  size_t lc;
+  char **lines = read_all_lines("inputs/day22.txt", &lc);
+
+  // First, find the max row and col so I know how much space
+  // to allocate for my 2d array. Also, may as well look for the
+  // start square here
+  struct node **grid = malloc(lc * sizeof(struct node *));
+  uint32_t width = 0;
+  uint32_t height = 0;
+  size_t start_x, start_y;
+  int empty_size = 0;
+  for (int j = 0; j < lc; j++) {
+    struct node *n = node_from_line(lines[j]);
+    if (n->x > width)
+      width = n->x;
+    if (n->y > height)
+      height = n->y;
+    if (n->used_p == 0) {
+      start_x = n->x;
+      start_y = n->y;
+    }
+    grid[j] = n; 
+
+    if (n->used == 0)
+      empty_size = n->size;
+  }
+  ++width;
+  ++height;
+  printf("\nwidth %lu\n", width);
+  printf("start: %lu, %lu\n", start_x, start_y);
+
+  char *map = malloc(width * height * sizeof(char));
+  for (int j = 0; j < lc; j++) {
+    struct node *n = grid[j];
+    size_t idx = n->y * width + n->x;
+    char ch;
+    if (n->used == 0)
+      map[idx] = '_';
+    else if (n->used > empty_size)
+      map[idx] = '#';
+    else
+      map[idx] = '.';
+  }
+  
+  
+  for (int r = 0; r < height; r++) {
+    for (int c = 0; c < width; c++) {
+      printf("%c", map[r*width + c]);
+    }
+    printf("\n");
+  }
+
+  for (int j = 0; j < lc; j++) {
+    free(grid[j]);
+  }
+  free(grid);
+  free(map);
+}
+
+int main(void)
+{
+  p1();
+  p2();
 }
