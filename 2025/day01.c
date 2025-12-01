ee154211e68ec895103ee3f161942c70987bd350
Dana Larose <ywg.dana@gmail.com>
2025-12-01 13:50:23 -0600

    A little more code tidying

--- 2025/day01.c
+++ 2025/day01.c
@@ -9,20 +9,16 @@ void main(void) {
   FILE *fp;
   char s[BUFF_SIZE];
 
-  int zeroes = 0;
-  int wraps = 0;
-  int dial = 50;
+  int zeroes = 0, wraps = 0, dial = 50;
   fp = fopen("data/day01.txt", "r");
   while (fgets(s, BUFF_SIZE, fp)) {
-    char *c = (s+1);
-    int v = atoi(c);
+    int v = atoi(s + 1);
     if (*s == 'L')
       v *= -1;
 
     int dial_pre = dial;
     dial = mod(dial + v);
 
-    int delta_wrap = 0;
     if (v < 0 && dial_pre < abs(v)) {
       wraps += 1 + (abs(v) - dial_pre) / 100;
       if (dial_pre == 0 || dial == 0)
