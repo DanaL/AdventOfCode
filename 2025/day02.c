215f35118985b1ac54dec7d1ba1feea227f0daa2
Dana Larose <ywg.dana@gmail.com>
2025-12-02 14:55:45 -0600

    Tidied code, removed redundant/duplicate code

--- 2025/day02.c
+++ 2025/day02.c
@@ -30,67 +30,45 @@ char* fetch_input(const char* filename) {
 bool repeated_digits(const char *s, size_t seg_len, size_t num_of_segs)
 {
     for (size_t seg = 1; seg < num_of_segs; seg++) {
-        for (size_t p = 0; p < seg_len; p++) {
-            if (s[p] != s[seg * seg_len + p])
-                return false;
+        if (strncmp(s, s + seg * seg_len, seg_len) != 0) {
+           return false;
         }
     }
 
     return true;
 }
 
-unsigned long long p1_check(unsigned long long a, unsigned long long b)
-{
-    unsigned long long total = 0;
-    char buffer[BUFF_LEN];
-
-    for (unsigned long long n = a; n <= b; n++) {
-        snprintf(buffer, sizeof(buffer), "%llu", n);
-        size_t len = strlen(buffer);
-        if (len % 2 != 0 )
-            continue;
-        size_t seg_len = len / 2;
-        if (repeated_digits(buffer, seg_len, 2)) {
-            total += n;
-        }
-    }
-
-    return total;
-}
-
-unsigned long long p2_check(unsigned long long a, unsigned long long b)
-{
-    unsigned long long total = 0;
-    char buffer[BUFF_LEN];
-
-    for (unsigned long long n = a; n <= b; n++) {
-        snprintf(buffer, sizeof(buffer), "%llu", n);
-        size_t len = strlen(buffer);
-        for (size_t seg_len = 1; seg_len < len; seg_len++) {
-            if (len % seg_len != 0)
-                continue;
-            size_t num_of_segs = len / seg_len;
-            if (repeated_digits(buffer, seg_len, num_of_segs)) {
-                total += n;
-                break;
-            }
-        }
-    }
-
-    return total;
-}
-
 int main(void)
 {
+    char buffer[BUFF_LEN];
     char *data = fetch_input("data/day02.txt");
 
     unsigned long long p1 = 0, p2 = 0;
-    
-    char *ptr = data;
     unsigned long long lo, hi;
+    char *ptr = data;
     while (sscanf(ptr, "%llu-%llu", &lo, &hi) == 2) {
-        p1 += p1_check(lo, hi);
-        p2 += p2_check(lo, hi);
+        for (unsigned long long n = lo; n <= hi; n++) {
+            snprintf(buffer, sizeof(buffer), "%llu", n);
+            size_t len = strlen(buffer);
+
+            // part 1
+            if (len % 2 == 0) {
+                size_t seg_len = len / 2;
+                if (strncmp(buffer, buffer + seg_len, seg_len) == 0)
+                    p1 += n;
+            }
+
+            // part 2
+            for (size_t seg_len = 1; seg_len <= len / 2; seg_len++) {
+                if (len % seg_len != 0)
+                    continue;
+                size_t num_of_segs = len / seg_len;
+                if (repeated_digits(buffer, seg_len, num_of_segs)) {
+                    p2 += n;
+                    break;
+                }
+            }
+        }
 
         ptr = strchr(ptr, ',');
         if (!ptr)
