aa44012aca8cdbf0daf89c569eb2422e2b451926
Dana Larose <ywg.dana@gmail.com>
2025-12-02 13:48:53 -0600

    2025 day 2 part 2 done

--- 2025/day02.c
+++ 2025/day02.c
@@ -32,13 +32,12 @@ char* fetch_input(const char* filename) {
     return buffer;
 }
 
-struct range extract_range(size_t *pos, char *s)
+struct range extract_range(size_t *pos, const char *s)
 {
     struct range r;
     size_t start = *pos, dash = 0;
     
-    while (s[*pos] != '\0' && s[*pos] != ',')
-    {
+    while (s[*pos] != '\0' && s[*pos] != ',') {
         if (s[*pos] == '-')
             dash = *pos;
         ++(*pos);
@@ -61,35 +60,55 @@ struct range extract_range(size_t *pos, char *s)
     return r;
 }
 
-bool repeated_digits(unsigned long long n)
+bool repeated_digits(const char *s, size_t seg_len, size_t num_of_segs)
 {
-    char buffer[BUFF_LEN];
-    snprintf(buffer, sizeof(buffer), "%llu", n);
-
-    size_t len = strlen(buffer);
-    if (len % 2 != 0)
-        return false;
-        
-    char *a = buffer;
-    char *b = buffer + len / 2;
-
-    for (size_t j = 0; j < len / 2; j++) {
-        if (*a != *b)
-            return false;
-        ++a;
-        ++b;
+    for (size_t seg = 1; seg < num_of_segs; seg++) {
+        for (size_t p = 0; p < seg_len; p++) {
+            if (s[p] != s[seg * seg_len + p])
+                return false;
+        }
     }
 
     return true;
 }
 
-unsigned long long check_range(struct range r)
+unsigned long long p1_check(struct range r)
 {
     unsigned long long total = 0;
-    for (unsigned long long n = r.lo; n <= r.hi; n++)
-    {
-        if (repeated_digits(n))
+    char buffer[BUFF_LEN];
+
+    for (unsigned long long n = r.lo; n <= r.hi; n++) {
+        snprintf(buffer, sizeof(buffer), "%llu", n);
+        size_t len = strlen(buffer);
+        if (len % 2 != 0 )
+            continue;
+        size_t seg_len = len / 2;
+        if (repeated_digits(buffer, seg_len, 2)) {
             total += n;
+        }
+    }
+
+    return total;
+}
+
+unsigned long long p2_check(struct range r)
+{
+    unsigned long long total = 0;
+    char buffer[BUFF_LEN];
+
+    for (unsigned long long n = r.lo; n <= r.hi; n++) {
+        snprintf(buffer, sizeof(buffer), "%llu", n);
+        size_t len = strlen(buffer);
+        for (size_t seg_len = 1; seg_len < len; seg_len++) {
+            size_t num_of_segs = len / seg_len;
+            if (len % num_of_segs != 0 || len % seg_len != 0)
+                continue;
+            size_t seg_len = len / num_of_segs;
+            if (repeated_digits(buffer, seg_len, num_of_segs)) {
+                total += n;
+                break;
+            }
+        }
     }
 
     return total;
@@ -99,18 +118,17 @@ int main(void)
 {
     char *data = fetch_input("data/day02.txt");
 
-    printf("%s\n", data);
-
-    unsigned long long total = 0;
+    unsigned long long p1 = 0, p2 = 0;
     size_t pos = 0;
     size_t len = strlen(data);
-    while (pos < len)
-    {
+    while (pos < len) {
         struct range r = extract_range(&pos, data);
-        total += check_range(r);
+        p1 += p1_check(r);
+        p2 += p2_check(r);
     }
 
-    printf("P1: %llu\n", total);
+    printf("P1: %llu\n", p1);
+    printf("P2: %llu\n", p2);
 
     free(data);
 
