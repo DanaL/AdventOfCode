b76403523ff7a1e3baea7ee440daeb4d6342155e
Dana Larose <ywg.dana@gmail.com>
2025-12-03 11:40:04 -0600

    2025 day 3, combine logic for parts 1 and 2

--- 2025/day03.c
+++ 2025/day03.c
@@ -3,35 +3,16 @@
 #include <string.h>
 
 #define BUFF_SIZE 128
-#define BATTERY_SIZE 12
+#define MAX_BATTERY_SIZE 12
 
-int calc_p1_joltage(const char *s, size_t len)
+unsigned long long calc_joltage(const char *s, size_t len, size_t battery_size)
 {
-    char a = '\0', b = '\0';
-    for (size_t j = 0; j < len; j++)
-    {
-        if (s[j] > a && j < len - 1)
-        {
-            a = s[j];
-            b = '\0';
-        }
-        else if (s[j] > b)
-        {
-            b = s[j];
-        }
-    }
+    char battery[MAX_BATTERY_SIZE + 1];
+    battery[battery_size] = '\0';
 
-    return (a - '0') * 10 + (b - '0');
-}
-
-unsigned long long calc_p2_joltage(const char *s, size_t len)
-{
-    char battery[BATTERY_SIZE + 1];
-    battery[BATTERY_SIZE] = '\0';
-    
     size_t start_pos = 0;
-    for (size_t digit = 0; digit < BATTERY_SIZE; digit++) {
-        size_t remaining = BATTERY_SIZE - digit - 1;
+    for (size_t digit = 0; digit < battery_size; digit++) {
+        size_t remaining = battery_size - digit - 1;
         size_t search_len = len - remaining;
 
         char biggest = '0';
@@ -58,13 +39,12 @@ int main(void)
     char buffer[BUFF_SIZE];
 
     fp = fopen("data/day03.txt", "r");
-    unsigned int p1_joltage = 0;
-    unsigned long long p2_joltage = 0;
+    unsigned long long p1_joltage = 0, p2_joltage = 0;    
     while (fgets(buffer, BUFF_SIZE, fp))
     {
         size_t n = strlen(buffer) - 1;
-        p1_joltage += calc_p1_joltage(buffer, n);
-        p2_joltage += calc_p2_joltage(buffer, n);
+        p1_joltage += calc_joltage(buffer, n, 2);
+        p2_joltage += calc_joltage(buffer, n, 12);
     }
 
     printf("P1: %u\n", p1_joltage);
