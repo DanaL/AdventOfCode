6aaa5368e93d39f43597c68d11a646e6528b421d
Dana Larose <ywg.dana@gmail.com>
2025-12-09 03:39:07 -0600

    2025 day 9 split p1 code into its own function

--- 2025/day09.c
+++ 2025/day09.c
@@ -1,6 +1,8 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+#define D(a, b) llabs(((a)->x - (b)->x + 1) * ((a)->y - (b)->y + 1))
+
 #define BUFF_LEN 128
 #define LIST_LEN 1024
 
@@ -8,6 +10,20 @@ typedef struct {
   long x;
   long y;
 } Point;
+
+void p1(Point *pts, size_t num_pts)
+{
+  long long largest_area = 0;
+  for (size_t j = 0; j < num_pts; j++) {
+    for (size_t k = j + 1; k < num_pts; k++) {
+      long long area = D(&pts[j], &pts[k]);
+      if (area > largest_area)
+        largest_area = area;
+    }
+  }
+
+  printf("P1: %lld\n", largest_area);
+}
 
 int main(void)
 {
@@ -26,16 +42,7 @@ int main(void)
   }
   fclose(fp);
 
-  long long largest_area = 0;
-  for (size_t j = 0; j < num_pts; j++) {
-    for (size_t k = j + 1; k < num_pts; k++) {
-      long long area = llabs((pts[j].x - pts[k].x + 1) * (pts[j].y - pts[k].y + 1));
-      if (area > largest_area)
-        largest_area = area;
-    }
-  }
-
-  printf("P1: %lld\n", largest_area);
+  p1(pts, num_pts);
 
   return 0;
 }
